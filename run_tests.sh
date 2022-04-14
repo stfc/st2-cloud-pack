8311f1a717e517399b8b90feefe117348fad4b03
David Fairbrother <DavidFair@users.noreply.github.com>
2022-04-14 11:31:16 +0100

    Add script to run tests locally.
    This script incorporates elements of the CI to allow developers to
    easily re-run their tests.

---
+++ run_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -ex
+
+REPO_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+
+export ST2_REPO_PATH="/tmp/st2"
+if [ ! -d "$ST2_REPO_PATH" ]; then
+    git clone --depth=1 https://github.com/stackstorm/st2.git "$ST2_REPO_PATH"
+fi
+
+"$ST2_REPO_PATH/st2common/bin/st2-run-pack-tests" -p "$REPO_DIR" -c
