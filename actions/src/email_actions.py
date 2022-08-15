913431f04f1e92ebc9c41c62e687b941df2cc450
Joel Davies <joel.davies@stfc.ac.uk>
2022-08-15 16:30:32 +0100

    Add email.floating.ip.users action

--- actions/src/email_actions.py
+++ actions/src/email_actions.py
@@ -1,5 +1,6 @@
 from typing import Callable, Dict, List
 from email_api.email_api import EmailApi
+from openstack_api.openstack_floating_ip import OpenstackFloatingIP
 from openstack_api.openstack_query import OpenstackQuery
 from openstack_api.openstack_server import OpenstackServer
 
@@ -12,6 +13,9 @@ class EmailActions(Action):
         self._api: EmailApi = config.get("email_api", EmailApi())
         self._server_api: OpenstackServer = config.get(
             "openstack_server_api", OpenstackServer()
+        )
+        self._floating_ip_api: OpenstackServer = config.get(
+            "openstack_floating_ip_api", OpenstackFloatingIP()
         )
         self._query_api: OpenstackQuery = config.get(
             "openstack_query_api", OpenstackQuery()
@@ -86,7 +90,7 @@ class EmailActions(Action):
         **kwargs,
     ):
         """
-        Finds all servers belonging to a project (or all servers if project is empty)
+        Finds all servers matching a query and then sends emails to their users
         :param: cloud_account: The account from the clouds configuration to use
         :param: project_identifier: The project this applies to (or empty for all servers)
         :param: query_preset: The query to use when searching for servers
@@ -104,13 +108,13 @@ class EmailActions(Action):
         :param: send_as_html (Bool): If true will send in HTML format
         :return:
         """
-        if not "user_email" in properties_to_select:
+        if "user_email" not in properties_to_select:
             raise ValueError("properties_to_select must contain 'user_email'")
 
         # Ensure only a valid query preset is used when there is no project
-        # (try and prevent mistakingly emailing loads of people)
+        # (try and prevent mistakenly emailing loads of people)
         if project_identifier == "":
-            if not query_preset in OpenstackServer.SEARCH_QUERY_PRESETS_NO_PROJECT:
+            if query_preset not in OpenstackServer.SEARCH_QUERY_PRESETS_NO_PROJECT:
                 raise ValueError(
                     f"project_identifier needed for the query type '{query_preset}'"
                 )
@@ -125,6 +129,88 @@ class EmailActions(Action):
             "server",
             properties_to_select,
             "user_email",
+            send_as_html,
+        )
+
+        for key, value in emails.items():
+            separator = "<br><br>" if send_as_html else "\n\n"
+            emails[key] = f"{message}{separator}{value}"
+
+        return self._api.send_emails(
+            smtp_accounts=self.config.get("smtp_accounts", None),
+            emails=emails,
+            subject=subject,
+            email_from=email_from,
+            email_cc=email_cc,
+            header=header,
+            footer=footer,
+            attachment_filepaths=attachment_filepaths,
+            smtp_account=smtp_account,
+            test_override=test_override,
+            test_override_email=test_override_email,
+            send_as_html=send_as_html,
+        )
+
+    # pylint:disable=too-many-arguments,too-many-locals
+    def email_floating_ip_users(
+        self,
+        cloud_account: str,
+        project_identifier: str,
+        query_preset: str,
+        message: str,
+        properties_to_select: List[str],
+        subject: str,
+        email_from: str,
+        email_cc: List[str],
+        header: str,
+        footer: str,
+        attachment_filepaths: List[str],
+        smtp_account: str,
+        test_override: bool,
+        test_override_email: List[str],
+        send_as_html: bool,
+        **kwargs,
+    ):
+        """
+        Finds all floating ips matching a query and then sends emails to their project's contact
+        :param: cloud_account: The account from the clouds configuration to use
+        :param: project_identifier: The project this applies to (or empty for all servers)
+        :param: query_preset: The query to use when searching for servers
+        :param: message: Message to add to the body of emails sent
+        :param: properties_to_select: The list of properties to select and output from the found servers
+        :param: subject (String): Subject of the emails
+        :param: email_from (String): Sender Email, subject (String): Email Subject,
+        :param: email_cc (List[String]): Email addresses to Cc
+        :param: header (String): filepath to header file,
+        :param: footer (String): filepath to footer file,
+        :param: attachment (List): list of attachment filepaths,
+        :param: smtp_account (String): email config to use,
+        :param: test_override (Boolean): send all emails to test emails
+        :param: test_override_email (List[String]): send to this email if test_override enabled
+        :param: send_as_html (Bool): If true will send in HTML format
+        :return:
+        """
+        if "project_email" not in properties_to_select:
+            raise ValueError("properties_to_select must contain 'project_email'")
+
+        # Ensure only a valid query preset is used when there is no project
+        # (try and prevent mistakenly emailing loads of people)
+        if project_identifier == "":
+            if query_preset not in OpenstackFloatingIP.SEARCH_QUERY_PRESETS_NO_PROJECT:
+                raise ValueError(
+                    f"project_identifier needed for the query type '{query_preset}'"
+                )
+
+        servers = self._floating_ip_api[f"search_{query_preset}"](
+            cloud_account, project_identifier, **kwargs
+        )
+
+        emails = self._query_api.parse_and_output_table(
+            cloud_account,
+            servers,
+            "floating_ip",
+            properties_to_select,
+            "project_email",
             send_as_html,
         )
 
--- tests/actions/test_email_actions.py
+++ tests/actions/test_email_actions.py
@@ -1,5 +1,6 @@
 from unittest.mock import create_autospec, NonCallableMock
 from email_api.email_api import EmailApi
+from openstack_api.openstack_floating_ip import OpenstackFloatingIP
 
 from openstack_api.openstack_server import OpenstackServer
 from openstack_api.openstack_query import OpenstackQuery
@@ -29,12 +30,19 @@ class TestServerActions(OpenstackActionTestBase):
         # calls will go to the same mock
         self.server_mock.__getitem__ = OpenstackServer.__getitem__
 
+        self.floating_ip_mock = create_autospec(OpenstackFloatingIP)
+
+        # Want to keep mock of __getitem__ otherwise all f"search_{query_preset}"
+        # calls will go to the same mock
+        self.floating_ip_mock.__getitem__ = OpenstackFloatingIP.__getitem__
+
         self.query_mock = create_autospec(OpenstackQuery)
 
         self.action: EmailActions = self.get_action_instance(
             api_mocks={
-                "openstack_server_api": self.server_mock,
                 "email_api": self.email_mock,
+                "openstack_server_api": self.server_mock,
+                "openstack_floating_ip_api": self.floating_ip_mock,
                 "openstack_query_api": self.query_mock,
             },
         )
@@ -77,7 +85,7 @@ class TestServerActions(OpenstackActionTestBase):
             attachment_filepaths=[],
             smtp_account="",
             test_override=False,
-            test_override_email="",
+            test_override_email=[""],
             send_as_html=False,
             days=60,
             ids=None,
@@ -103,7 +111,7 @@ class TestServerActions(OpenstackActionTestBase):
             attachment_filepaths=[],
             smtp_account="",
             test_override=False,
-            test_override_email="",
+            test_override_email=[""],
             send_as_html=False,
             days=60,
             ids=None,
@@ -113,8 +121,8 @@ class TestServerActions(OpenstackActionTestBase):
 
     def test_email_server_users_no_project(self):
         """
-        Tests the action that sends emails to server does not give a value error when project is
-        required for the query type
+        Tests the action that sends emails to server users does not give a value error when a project
+        is required for the query type
         """
 
         i = 0
@@ -134,7 +142,7 @@ class TestServerActions(OpenstackActionTestBase):
 
     def test_email_server_users_no_project_error(self):
         """
-        Tests the action that sends emails to server users gives a value error when project is
+        Tests the action that sends emails to server users gives a value error when a project is
         required for the query type
         """
 
@@ -147,9 +155,103 @@ class TestServerActions(OpenstackActionTestBase):
         for query_preset in should_not_pass:
             self._check_email_server_users_raises(query_preset)
 
+    @raises(ValueError)
+    def test_email_floating_ip_users_no_email_error(self):
+        """
+        Tests the action that sends emails to floating ip users gives a value error when user_email
+        is not present in the `properties_to_select`
+        """
+        self.action.email_server_users(
+            cloud_account="test_account",
+            project_identifier="",
+            query_preset="fips_older_than",
+            message="Message",
+            properties_to_select=["id"],
+            subject="Subject",
+            email_from="testemail",
+            email_cc=[],
+            header="",
+            footer="",
+            attachment_filepaths=[],
+            smtp_account="",
+            test_override=False,
+            test_override_email=[""],
+            send_as_html=False,
+            days=60,
+            ids=None,
+            names=None,
+            name_snippets=None,
+        )
+
+    def _email_floating_ip_users(self, query_preset: str):
+        """
+        Helper for checking email_floating_ip_users works correctly
+        """
+        return self.action.email_floating_ip_users(
+            cloud_account="test_account",
+            project_identifier="",
+            query_preset=query_preset,
+            message="Message",
+            properties_to_select=["project_email"],
+            subject="Subject",
+            email_from="testemail",
+            email_cc=[],
+            header="",
+            footer="",
+            attachment_filepaths=[],
+            smtp_account="",
+            test_override=False,
+            test_override_email=[""],
+            send_as_html=False,
+            days=60,
+            ids=None,
+            names=None,
+            name_snippets=None,
+        )
+
+    def test_email_floating_ip_users_no_project(self):
+        """
+        Tests the action that sends emails to floating ip users does not give a value error when a project
+        is required for the query type
+        """
+
+        i = 0
+        for query_preset in OpenstackFloatingIP.SEARCH_QUERY_PRESETS_NO_PROJECT:
+            self._email_floating_ip_users(query_preset)
+            i += 1
+            self.assertEqual(self.email_mock.send_emails.call_count, i)
+
+    @raises(ValueError)
+    def _check_email_floating_ip_users_raises(self, query_preset):
+        """
+        Helper for checking email_floating_ip_users raises a ValueError when needed
+        (needed to allow multiple to be checked in the same test otherwise it stops
+         after the first error)
+        """
+        self.assertRaises(ValueError, self._email_floating_ip_users(query_preset))
+
+    def test_email_floating_ip_users_no_project_error(self):
+        """
+        Tests the action that sends emails to floating ip users gives a value error when a project
+        is required for the query type
+        """
+
+        # Should raise an error for all but a few queries
+        should_pass = OpenstackFloatingIP.SEARCH_QUERY_PRESETS_NO_PROJECT
+        should_not_pass = OpenstackFloatingIP.SEARCH_QUERY_PRESETS
+        for x in should_pass:
+            should_not_pass.remove(x)
+
+        for query_preset in should_not_pass:
+            self._check_email_floating_ip_users_raises(query_preset)
+
     def test_run_method(self):
         """
         Tests that run can dispatch to the Stackstorm facing methods
         """
-        expected_methods = ["send_email", "email_server_users"]
+        expected_methods = [
+            "send_email",
+            "email_server_users",
+            "email_floating_ip_users",
+        ]
         self._test_run_dynamic_dispatch(expected_methods)
