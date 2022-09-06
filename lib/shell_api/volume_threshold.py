bb09184c443c1554c41cc2e7ae6887c5ca4eeda8
William Edwards <william.edwards@stfc.ac.uk>
2022-09-06 15:43:37 +0100

    More test changes

--- lib/shell_api/volume_threshold.py
+++ lib/shell_api/volume_threshold.py
@@ -23,7 +23,7 @@ class OpenStackVolumeThreshold(OpenstackWrapperBase):
             volumeUnMetaList = []
             volumeUnMetaList = [volumeList]
             print(volumeUnMetaList)
-            count = volumeList.len()
+            count = volumeUnMetaList.len()
             volumesizes = []
             TCount = 0
             FCount = 0
