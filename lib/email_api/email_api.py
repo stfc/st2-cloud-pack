f6b071c3f7a2fd955bc901a14fed5298e0b8a668
Joel Davies <joel.davies@stfc.ac.uk>
2022-09-07 14:53:32 +0100

    Fix send.email error

--- lib/email_api/email_api.py
+++ lib/email_api/email_api.py
@@ -41,7 +41,6 @@ class EmailApi:
             msg.attach(part)
         return msg
 
-    # pylint:disable=too-many-arguments
     def send_email(
         self,
         smtp_account: SMTPAccount,
@@ -52,22 +51,25 @@ class EmailApi:
         """
         Send an email
         :param: smtp_account (SMTPAccount): SMTP config
-        :param: subject: (String): Subject of the email
+        :param: email_params: See EmailParams
         :param: email_to (List[String]): Email addresses to send the email to
-        :param: email_from (String): Sender Email, subject (String): Email Subject,
-        :param: email_cc (List[String]): Email addresses to Cc
-        :param: header (String): filepath to header file,
-        :param: footer (String): filepath to footer file,
         :param: body (String): body of email,
-        :param: attachment_filepaths (List): list of attachment filepaths,
-        :param: send_as_html (Bool): If true will send in HTML format
         :return: (Status (Bool), Output <*>): tuple of action status (succeeded(T)/failed(F)) and the output
         """
+
+        chosen_email_to = email_params.test_override_email
+
+        if not email_params.test_override_email:
+            chosen_email_to = (
+                (email_to + email_params.email_cc)
+                if email_params.email_cc
+                else email_to
+            )
 
         msg = MIMEMultipart()
         msg["Subject"] = Header(email_params.subject, "utf-8")
         msg["From"] = email_params.email_from
-        msg["To"] = ", ".join(email_to)
+        msg["To"] = ", ".join(chosen_email_to)
 
         msg["Date"] = formatdate(localtime=True)
 
@@ -101,15 +103,10 @@ class EmailApi:
         if smtp_account.smtp_auth:
             smtp.login(smtp_account.username, smtp_account.password)
 
-        email_to = (
-            (email_to + email_params.email_cc) if email_params.email_cc else email_to
-        )
-
         smtp.sendmail(email_params.email_from, email_to, msg.as_string())
         smtp.quit()
         return (True, "Email sent")
 
-    # pylint:disable=too-many-arguments
     def send_emails(
         self,
         smtp_account: SMTPAccount,
@@ -119,29 +116,20 @@ class EmailApi:
         """
         Sends multiple emails
         :param: smtp_account (SMTPAccount): SMTP config
-        :emails (Dict): Keys are email addresses, values are messages to send to those emails
-        :param: subject: (String): Subject of the email
-        :param: email_from (String): Sender Email, subject (String): Email Subject,
-        :param: email_cc (List[String]): Email addresses to Cc
-        :param: header (String): filepath to header file,
-        :param: footer (String): filepath to footer file,
-        :param: attachment (List): list of attachment filepaths,
-        :param: test_override (Boolean): send all emails to test emails
-        :param: test_override_email (List[String]): send to this email if test_override enabled
-        :param: send_as_html (Bool): If true will send in HTML format
+        :param: emails (Dict): Keys are email addresses, values are messages to send to those emails
+        :param: email_params: See EmailParams
         :return: Status (Bool): tuple of action status (succeeded(T)/failed(F))
         """
         if email_params.test_override:
             # Send a maximum of 10 emails
             emails = dict(random.sample(emails.items(), min(len(emails), 10)))
-            override_email = email_params.test_override_email
 
         return all(
             [
                 self.send_email(
                     smtp_account=smtp_account,
                     email_params=email_params,
-                    email_to=override_email if email_params.test_override else [key],
+                    email_to=[key],
                     body=value,
                 )[0]
             ]
