1b10e04d9131f9e60294fa9c13049fbf99ddd1a9
David Symons <david.symons@stfc.ac.uk>
2022-11-10 16:53:44 +0000

    Update method to get config

--- sensors/src/hypervisor_polling_sensor.py
+++ sensors/src/hypervisor_polling_sensor.py
@@ -5,15 +5,16 @@ Polling Sensor to query OpenStack for empty hypervisors.
 from st2reactor.sensor.base import PollingSensor
 from st2reactor.container.sensor_wrapper import SensorService
 from openstack_api.openstack_hypervisor import OpenstackHypervisor
+
 class HypervisorPollingSensor(PollingSensor):
 
     def __init__(self, sensor_service, config=None, poll_interval=10):
         super().__init__(sensor_service, config, poll_interval)
         self.api = OpenstackHypervisor()
-        self.sensor_service: SensorService = sensor_service
+        self._config = self._config.get("cloud_names")
+        self._cloud = {"dev": self._config.get("dev_cloud", None), "prod": self._config.get("prod_cloud", None)}
         self._log = self.sensor_service.get_logger(name=self.__class__.__name__)
-        self._cloud = {"dev": None, "prod": None}
-        self._environment = "dev"
+        self._environment = self._config.get("selected_env")
 
     # pylint: disable=missing-function-docstring
     def add_trigger(self, trigger):
@@ -32,15 +33,6 @@ class HypervisorPollingSensor(PollingSensor):
         """
         Sets up the sensor
         """
-        self._cloud["prod"] = self.sensor_service.get_value(
-            "automated_hypervisor_updater.prod_cloud", local=False, decrypt=False
-        )
-        self._cloud["dev"] = self.sensor_service.get_value(
-            "automated_hypervisor_updater.dev_cloud", local=False, decrypt=False
-        )
-
-        if self._cloud["prod"] != "not_set" and self._cloud["prod"] != None:
-            self._environment = "prod"
         
         self._log.info("Environment: " + str(self._environment))
         self._log.info("Cloud to use: " + str(self._cloud[self._environment]))
