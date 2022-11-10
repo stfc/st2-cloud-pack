b29cb107967a74b743514e4d191c0f85e15154ce
David Symons <david.symons@stfc.ac.uk>
2022-11-10 14:08:28 +0000

    Additional logging

--- sensors/src/hypervisor_polling_sensor.py
+++ sensors/src/hypervisor_polling_sensor.py
@@ -42,7 +42,8 @@ class HypervisorPollingSensor(PollingSensor):
         if self._cloud["prod"] != "not_set":
             self._environment = "prod"
         
-        self._log.info("Cloud to use" + str(self._cloud[self._environment]))
+        self._log.info("Environment: " + str(self._environment))
+        self._log.info("Cloud to use: " + str(self._cloud[self._environment]))
 
     def poll(self):
         """
