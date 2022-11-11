d94ccb490458ca7b6065016144ebb4b089cac110
David Symons <david.symons@stfc.ac.uk>
2022-11-11 15:02:07 +0000

    Fixing pylint errors I missed

--- lib/openstack_api/openstack_hypervisor.py
+++ lib/openstack_api/openstack_hypervisor.py
@@ -100,7 +100,7 @@ class OpenstackHypervisor(OpenstackWrapperBase, OpenstackQueryBase):
         empty_hvs = []
         for hpv in hvs:
             hpv = dict(hpv)
-            if hpv["vcpus_used"] == 0 and hpv["running_vms"] == 0 and hv["status"] == "enabled":
+            if hpv["vcpus_used"] == 0 and hpv["running_vms"] == 0 and hpv["status"] == "enabled":
                 empty_hvs.append(hpv["name"])
 
         return empty_hvs
