b116b293f7bdc84b2f25209a400bf6622e5aecb0
David Symons <david.symons@stfc.ac.uk>
2022-11-18 11:19:27 +0000

    Pylint is a drama queen

--- lib/openstack_api/openstack_hypervisor.py
+++ lib/openstack_api/openstack_hypervisor.py
@@ -100,7 +100,7 @@ class OpenstackHypervisor(OpenstackWrapperBase, OpenstackQueryBase):
         empty_hvs = []
         for hpv in hvs:
             hpv = dict(hpv)
-            if len(hpv["servers"]) == None and hpv["status"] == "enabled":
+            if len(hpv["servers"]) is None and hpv["status"] == "enabled":
                 empty_hvs.append(hpv["name"])
 
         return empty_hvs
