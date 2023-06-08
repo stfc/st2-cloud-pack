aec4ba5ec0cb09ead29c3785ce620a1c2ca0bedb
anish-mudaraddi <anish.mudaraddi@stfc.ac.uk>
2023-06-08 14:48:15 +0100

    put server properties enum in alphabetical order

--- lib/enums/query/server_properties.py
+++ lib/enums/query/server_properties.py
@@ -2,14 +2,14 @@ from enum import Enum, auto
 
 
 class ServerProperties(Enum):
-    USER_ID = auto()
-    HYPERVISOR_ID = auto()
-    SERVER_ID = auto()
-    SERVER_NAME = auto()
-    SERVER_DESCRIPTION = auto()
-    SERVER_STATUS = auto()
-    SERVER_CREATION_DATE = auto()
-    SERVER_LAST_UPDATED_DATE = auto()
     FLAVOR_ID = auto()
+    HYPERVISOR_ID = auto()
     IMAGE_ID = auto()
     PROJECT_ID = auto()
+    SERVER_CREATION_DATE = auto()
+    SERVER_DESCRIPTION = auto()
+    SERVER_ID = auto()
+    SERVER_LAST_UPDATED_DATE = auto()
+    SERVER_NAME = auto()
+    SERVER_STATUS = auto()
+    USER_ID = auto()
