7a5dcf4ab37192fc067b0bad48aed924b384b97c
anish-mudaraddi <anish.mudaraddi@stfc.ac.uk>
2023-07-17 17:13:00 +0100

    fix for tests/actions/server_query_actions

--- tests/actions/test_server_query_actions.py
+++ tests/actions/test_server_query_actions.py
@@ -1,13 +1,8 @@
-from unittest.mock import patch, NonCallableMock, MagicMock
+from unittest.mock import patch, NonCallableMock
 from parameterized import parameterized
 from src.server_query_actions import ServerQueryActions
 from tests.actions.openstack_action_test_base import OpenstackActionTestBase
-
-from enums.query.query_output_types import QueryOutputTypes
 from enums.cloud_domains import CloudDomains
-from enums.query.props.server_properties import ServerProperties
-
-from structs.query.query_output_details import QueryOutputDetails
 
 
 class TestServerQueryActions(OpenstackActionTestBase):
@@ -20,10 +15,10 @@ class TestServerQueryActions(OpenstackActionTestBase):
     def setUp(self):
         super().setUp()
         self.action: ServerQueryActions = self.get_action_instance()
+        self.mock_cloud_account = "dev"
         self.args = {
-            "cloud_account": "dev",
             "properties_to_select": ["server_id", "server_name"],
-            "return_type": "to_str",
+            "output_type": "to_str",
             "kwarg1": NonCallableMock(),
             "kwarg2": NonCallableMock(),
         }
@@ -36,26 +31,15 @@ class TestServerQueryActions(OpenstackActionTestBase):
             "search_by_regex",
         ]
     )
-    def test_run(self, method_name):
+    @patch("src.server_query_actions.ServerManager")
+    def test_run(self, method_name, mock_server_manager):
         """
         Tests that run method can dispatch to ServerManager methods
         """
-        assert hasattr(self.action._server_manager, method_name)
-
-        self.action._server_manager = MagicMock()
-        mock_method = getattr(self.action._server_manager.return_value, method_name)
-
-        self.action.run(submodule=method_name, **self.args)
-
-        self.action._server_manager.assert_called_once_with(CloudDomains.DEV)
-        mock_method.assert_called_once_with(
-            output_details=QueryOutputDetails(
-                properties_to_select=[
-                    ServerProperties.SERVER_ID,
-                    ServerProperties.SERVER_NAME,
-                ],
-                output_type=QueryOutputTypes.TO_STR,
-            ),
-            kwarg1=self.args["kwarg1"],
-            kwarg2=self.args["kwarg2"],
+        assert hasattr(mock_server_manager, method_name)
+        mock_method = getattr(mock_server_manager.return_value, method_name)
+        self.action.run(
+            submodule=method_name, cloud_account=self.mock_cloud_account, **self.args
         )
+        mock_server_manager.assert_called_once_with(cloud_account=CloudDomains.DEV)
+        mock_method.assert_called_once_with(**self.args)
