fe5c11c9e64e677a3cf0646166b68aa4aa4b2840
anish-mudaraddi <anish.mudaraddi@stfc.ac.uk>
2023-07-20 15:34:10 +0100

    allow smtp account dataclass to be created from pack config
    previous file used a helper method to do the same thing - cleaner to include it with the dataclass as another way to instantiate it

--- lib/structs/smtp_account.py
+++ lib/structs/smtp_account.py
@@ -21,3 +21,28 @@ class SMTPAccount:
             key: value for key, value in dictionary.items() if key in field_set
         }
         return SMTPAccount(**filtered_arg_dict)
+
+    @staticmethod
+    def from_pack_config(pack_config: dict, smtp_account_name: str):
+        """
+        Returns instance of this dataclass from StackStorm pack config
+        :param pack_config: The pack config
+        :param smtp_account_name: The account name to get from the config
+        :raises ValueError: When the pack config does not have smtp_accounts defined
+        :raises KeyError: When the account does not appear in the given config
+        :return: (Dictionary) SMTP account names and properties
+        """
+        smtp_accounts_config = pack_config.get("smtp_accounts", None)
+
+        if smtp_accounts_config is None:
+            raise ValueError("Pack config must contain the 'smtp_accounts' field")
+
+        try:
+            key_value = {config["name"]: config for config in smtp_accounts_config}
+            account_data = key_value[smtp_account_name]
+        except KeyError as exc:
+            raise KeyError(
+                f"The account {smtp_account_name} does not appear in the configuration"
+            ) from exc
+
+        return SMTPAccount.from_dict(account_data)
