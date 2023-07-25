7fdd510b043e14d0e1e4892026f2e289f100828a
anish-mudaraddi <anish.mudaraddi@stfc.ac.uk>
2023-07-25 11:57:09 +0100

    Add send test email stackstorm action
    Added stackstorm config to send a test email

---
+++ actions/src/email_actions.py
@@ -0,0 +1,19 @@
+from typing import Callable
+from email_api.email_actions import EmailActions
+from structs.smtp_account import SMTPAccount
+from st2common.runners.base_action import Action
+
+# pylint: disable=too-few-public-methods
+
+
+class ST2EmailActions(Action):
+    def run(self, submodule: str, smtp_account_name: str, **kwargs):
+        """
+        Dynamically dispatches to the method wanted
+        """
+        email_actions = EmailActions()
+        func: Callable = getattr(email_actions, submodule)
+        return func(
+            smtp_account=SMTPAccount.from_pack_config(self.config, smtp_account_name),
+            **kwargs,
+        )
---
+++ tests/actions/test_email_actions.py
@@ -0,0 +1,48 @@
+from unittest.mock import patch, NonCallableMock
+from parameterized import parameterized
+
+from src.email_actions import ST2EmailActions
+from tests.actions.openstack_action_test_base import OpenstackActionTestBase
+
+
+class TestST2EmailActions(OpenstackActionTestBase):
+    """
+    Unit tests for email.* actions
+    """
+
+    action_cls = ST2EmailActions
+    # pylint: disable=invalid-name
+
+    def setUp(self):
+        super().setUp()
+        self.action: ST2EmailActions = self.get_action_instance()
+        self.mock_kwargs = {
+            "kwarg1": NonCallableMock(),
+            "kwarg2": NonCallableMock(),
+        }
+
+    @parameterized.expand(
+        [
+            "send_test_email",
+        ]
+    )
+    @patch("src.email_actions.EmailActions")
+    @patch("src.email_actions.SMTPAccount")
+    def test_run(self, method_name, mock_smtp_account, mock_email_actions):
+        """
+        Tests that run method can dispatch to EmailActions methods
+        """
+        assert hasattr(mock_email_actions, method_name)
+        mock_method = getattr(mock_email_actions.return_value, method_name)
+        self.action.run(
+            submodule=method_name, smtp_account_name="config1", **self.mock_kwargs
+        )
+
+        mock_smtp_account.from_pack_config.assert_called_once_with(
+            self.action.config, "config1"
+        )
+        mock_email_actions.assert_called_once()
+        mock_method.assert_called_once_with(
+            smtp_account=mock_smtp_account.from_pack_config.return_value,
+            **self.mock_kwargs
+        )
