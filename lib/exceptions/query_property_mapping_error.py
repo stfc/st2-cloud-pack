ac672330b161cb6ce52d598fc04b570ae050427d
meoflynn <68336207+meoflynn@users.noreply.github.com>
2023-08-02 11:15:46 +0100

    Add enum mapping error class

--- lib/exceptions/query_property_mapping_error.py
+++ lib/exceptions/query_property_mapping_error.py
@@ -1,4 +1,7 @@
-class QueryPropertyMappingError(RuntimeError):
+from exceptions.enum_mapping_error import EnumMappingError
+
+
+class QueryPropertyMappingError(EnumMappingError):
     """
     Exception which is thrown whenever there is an error finding a property function for a given
     property Enum
