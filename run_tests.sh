70d5a516d1e4ab7b28d05a99894d887187a148c2
David Fairbrother <DavidFair@users.noreply.github.com>
2023-08-31 17:26:17 +0100

    BUG: Patch upstream runner to use pytest
    Patches the upstream runner to use Pytest instead of nose

--- run_tests.sh
+++ run_tests.sh
@@ -8,6 +8,10 @@ if [ ! -d "$ST2_REPO_PATH" ]; then
     git clone --depth=1 https://github.com/stackstorm/st2.git "$ST2_REPO_PATH"
 fi
 
+# Monkey patch nose to pytest until upstream has switched
+echo "PATCH: Replace nosetests with pytest"
+sed -ri 's/nosetests \${NOSE_OPTS\[\@\]}/pytest/g' "$ST2_REPO_PATH/st2common/bin/st2-run-pack-tests"
+
 PYTHON_PATH=$PYTHON_PATH:"$REPO_DIR/lib" "$ST2_REPO_PATH/st2common/bin/st2-run-pack-tests" -p "$REPO_DIR" -c
 
 # Generate coverage to consume locally
