ef1d434b16db97b574989c73046be7c465854531
David Fairbrother <DavidFair@users.noreply.github.com>
2023-09-01 10:53:24 +0100

    BUG: Actually use the script for tests
    Previously we we're replicated the script in our GH action rather than
    use the script directly

--- run_tests.sh
+++ run_tests.sh
@@ -10,9 +10,10 @@ fi
 
 # Monkey patch nose to pytest until upstream has switched
 echo "PATCH: Replace nosetests with pytest"
-sed -ri 's/nosetests-\$\{NOSE_OPTS\[@\]\}/pytest/g' "$ST2_REPO_PATH/st2common/bin/st2-run-pack-tests"
+sed -ri 's/nosetests \$\{NOSE_OPTS\[@\]\}/pytest/g' "$ST2_REPO_PATH/st2common/bin/st2-run-pack-tests"
 
-PYTHON_PATH=$PYTHON_PATH:"$REPO_DIR/lib" "$ST2_REPO_PATH/st2common/bin/st2-run-pack-tests" -p "$REPO_DIR" -c
+export PYTHONPATH="$PYTHONPATH:$REPO_DIR/lib"
+"$ST2_REPO_PATH/st2common/bin/st2-run-pack-tests" -p "$REPO_DIR" -c
 
 # Generate coverage to consume locally
 python -m pip install coverage~=4.4
