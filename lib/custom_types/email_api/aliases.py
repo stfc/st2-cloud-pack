f0a51de1228e0953831c4c6ce46f403cec8777dd
meoflynn <68336207+meoflynn@users.noreply.github.com>
2023-10-05 11:21:13 +0100

    BUG: Make sure email addresses to email are provided as a list
    If an email address has something like an apostrophe, it will split the email address into
    individual characters. Email params has been updated to take email addresses as a list of addresses

--- lib/custom_types/email_api/aliases.py
+++ lib/custom_types/email_api/aliases.py
@@ -1,7 +1,7 @@
-from typing import Dict, Tuple
+from typing import Dict, List
 
 EmailAddress = str
-EmailAddresses = Tuple[EmailAddress]
+EmailAddresses = List[EmailAddress]
 
 # Maps name of a template to a set of kwargs that the template will use to render string output
 TemplateMappings = Dict[str, Dict[str, str]]
