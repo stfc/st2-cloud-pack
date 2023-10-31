afb3cec330f9e2a214dfa2bc980e3f5b77a0539d
meoflynn <megan.o'flynn@stfc.ac.uk>
2023-10-31 14:45:39 +0000

    ENH: Expand the email shutoff workflow and unit tests
    This commit refactors the workflow and adds additional unit tests

--- lib/workflows/email_shutoff.py
+++ lib/workflows/email_shutoff.py
@@ -1,20 +1,18 @@
-from enums.cloud_domains import CloudDomains
+from typing import List, Optional, Dict, Tuple
 from openstack_query.api.query_objects import ServerQuery
-from openstack_query.api.query_objects import UserQuery
 from enums.query.props.server_properties import ServerProperties
 from enums.query.props.user_properties import UserProperties
 from structs.email.email_params import EmailParams
 from enums.query.query_presets import QueryPresetsGeneric
 from email_api.emailer import Emailer
-from tabulate import tabulate
-
 from structs.email.smtp_account import SMTPAccount
 
 
-def query_shutoff_vms(project_id=None):
+def query_shutoff_vms(project_id: Optional[List[str]] = None):
     """
     Sends an email to a user about a shutoff (stopped) VM
-    :param project_id: optional field to provide the ID for a specific project
+    :param project_id: optional field to provide a list of project IDs for specific projects,
+    otherwise query all projects
     :returns: Dictionary with a list of VM names grouped by user ID
     """
 
@@ -25,133 +23,95 @@ def query_shutoff_vms(project_id=None):
     ).where(
         QueryPresetsGeneric.EQUAL_TO,
         ServerProperties.SERVER_STATUS,
-        value="STOPPED"
-    ).group_by(
-        ServerProperties.USER_ID
-    )
+        value="SHUTOFF")
 
     # if project ID provided as parameter
     if project_id:
         query_obj.run(
             "openstack",
+            as_admin=True,
             from_projects=project_id
         )
 
     else:
         query_obj.run(
-            "openstack"
+            "openstack",
+            as_admin=True,
+            all_projects=True
         )
 
-    query_result = query_obj.to_list()
+    user_query = query_obj.then("USER_QUERY", keep_previous_results=True)
 
-    return query_result
-
-
-def query_user_details(user_id):
-    """
-    Queries OpenStack for the username and user email using
-    a user ID
-    :param user_id: ID for a given user
-    :returns: List of dictionaries with matching username and email
-    """
-    query_obj = UserQuery()
-
-    query_obj.select(
-        UserProperties.USER_NAME,
+    user_query.select(
         UserProperties.USER_EMAIL
-    ).where(
-        QueryPresetsGeneric.EQUAL_TO,
-        UserProperties.USER_ID,
-        value=user_id
+    ).group_by(
+        UserProperties.USER_NAME
     )
 
-    query_obj.run(
-        "openstack"
-    )
+    if project_id:
+        user_query.run(
+            "openstack",
+            # from_projects=project_id
+        )
 
-    query_result = query_obj.to_list()
+    else:
+        user_query.run("openstack", as_admin=True, all_projects=True)
 
-    return query_result
+    res = user_query.to_props()
+
+    print(res)
+    return res
 
 
-def extract_user_details(user_list):
+def prepare_user_server_email(server_query_result: List[Dict]) -> Tuple[str, List[str]]:
     """
-    Using a list of user IDs, this method queries for the name and email
-    address that match each user ID
-    :param user_list: A list of user IDs
-    :returns: A tuple of dictionaries containing the username and emails respectively matching the user IDs
-
+    Extracts the user email and list of server names from a
+    query object
+    :param: server_query_object: A list of dictionaries containing results from a query
+    :returns: A tuple containing the user email and list of VM names
     """
-    user_names = dict()
-    user_emails = dict()
-    for user in user_list:
-        # get username and details
-        user_detail = query_user_details(user)
-        user_detail = user_detail[0]
-
-        user_names[user] = user_detail['user_name']
-        user_emails[user] = user_detail['user_email']
-
-        if not user_emails[user]:
-            raise KeyError(
-                f"Missing user email for {user} {user_detail['user_name']}"
-            )
+    details = server_query_result
+    # store user email
+    assert all(details[0]['user_email'] == i['user_email'] for i in details)
+    user_email = details[0]['user_email']
+    # extract list of server names
+    server_names = [d['server_name'] for d in server_query_result]
+    return user_email, server_names
 
 
-    return user_names, user_emails
-
-def send_user_email(user_email, subject_text, email_templates=None):
+def send_user_email(smtp_account: SMTPAccount, user_email: str, subject_text: str, email_templates=None):
     """
     Sends an email to a user
+    :param: smtp_account: SMTP account object with Emailer config
     :param: user_email: Email address for a specific user
     :param: subject_text: Subject for the email
     """
 
-    email_account = SMTPAccount.from_dict({"account_details"})
+    if not user_email:
+        user_email = "cloud-support@stfc.ac.uk"
+
     test_params = EmailParams(
         subject=subject_text,
-        email_from='support@example.com',
-        email_to=user_email,
+        email_from='cloud-support@stfc.ac.uk',
+        email_to=[user_email],
         email_templates=[]
     )
 
-    Emailer(email_account).send_emails([test_params])
+    Emailer(smtp_account).send_emails([test_params])
 
 
-def main():
-    # script for testing within own scratch space project
+def main(smtp_account: SMTPAccount, project_id: Optional[List[str]] = None):
+    """
+    The main method for running the workflow.
+    :param: project_id: (Optional) A list of project IDs to query from
+    """
 
-    project_id = ['project_id']
+    # get VMs shutoff, grouped by user, include user email in results
+    shutoff_vm = query_shutoff_vms(project_id)
 
+    for user in shutoff_vm.values():
+        user_email, server_name_list = prepare_user_server_email(user) # shutoff_vm[user] is list of dictionaries
+        print('output: ', user_email, server_name_list)
+        # prepare and send blank test email
+        send_user_email(smtp_account, user_email, server_name_list)
 
-    user_email_test = ["user@example.com"]  # initially hardcoded for now
-
-    # get all VMs in shutoff state in a specific project, group by user ID
-    instance_list = query_shutoff_vms(project_id)
-
-    # from VM query, get all user IDs
-    user_list = instance_list.keys()
-
-    extract_user_details(instance_list, user_email_test, user_list)
-
-    # for vm in user_vms:
-    #     vm_name = vm["server_name"]
-    #     message = f'Test email - VM in shutoff state: {vm_name}'
-    #     print(message)
-    # print(f'Email message subject preview before sending: {message}')
-    # send_email(user_email_test, message)
-    # break
-
-
-
-
-
-
-#        print(f'Results for user: {user_name} Email: {user_email}')
-
-#        user_vms = (instance_list[user])
-
-
-
-if __name__ == "__main__":
-    main()
