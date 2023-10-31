8f0c270375c09c761d8192d4b8b5825a99bf7902
meoflynn <megan.o'flynn@stfc.ac.uk>
2023-10-31 14:45:39 +0000

    ENH: Expand the email shutoff workflow and unit tests
    This commit refactors the workflow and adds additional unit tests

--- tests/lib/workflows/test_email_shutoff.py
+++ tests/lib/workflows/test_email_shutoff.py
@@ -1,34 +1,9 @@
-from unittest.mock import NonCallableMock, patch
-
-from nose.tools import raises
-
+from unittest.mock import NonCallableMock, patch, call
 from enums.query.props.server_properties import ServerProperties
 from enums.query.props.user_properties import UserProperties
 from enums.query.query_presets import QueryPresetsGeneric
 from structs.email.email_params import EmailParams
-from workflows.email_shutoff import send_user_email, query_shutoff_vms, query_user_details, main, extract_user_details
-
-
-def test_send_user_email():
-    """
-    Test that an email is sent with the send_email method
-    """
-    mock_email = NonCallableMock()
-    mock_subject = NonCallableMock()
-    with patch('workflows.email_shutoff.Emailer') as mock_emailer:
-        send_user_email(mock_email, mock_subject)
-
-    mock_emailer.assert_called_once()
-    email_object = mock_emailer.return_value
-
-    mock_email_params = EmailParams(
-        subject=mock_subject,
-        email_from='cloud-support@stfc.ac.uk',
-        email_to=mock_email,
-        email_templates=[]
-    )
-
-    email_object.send_emails.assert_called_once_with([mock_email_params])
+from workflows.email_shutoff import send_user_email, query_shutoff_vms, main, prepare_user_server_email
 
 
 def test_query_shutoff_vms_no_project_id():
@@ -46,15 +21,12 @@ def test_query_shutoff_vms_no_project_id():
     server_object.select.assert_called_once_with(ServerProperties.SERVER_NAME)
 
     select_object = server_object.select.return_value
-    select_object.where.assert_called_once_with(QueryPresetsGeneric.EQUAL_TO, ServerProperties.SERVER_STATUS,value="STOPPED")
+    select_object.where.assert_called_once_with(QueryPresetsGeneric.EQUAL_TO, ServerProperties.SERVER_STATUS,value="SHUTOFF")
 
     where_object = select_object.where.return_value
     where_object.group_by.assert_called_once_with(ServerProperties.USER_ID)
 
-    server_object.run.assert_called_once_with("openstack")
-    server_object.to_list.assert_called_once()
-
-    assert res == server_object.to_list.return_value
+    server_object.run.assert_called_once_with("openstack", as_admin=True, all_projects=True)
 
 
 def test_query_shutoff_vms_project_id():
@@ -70,68 +42,140 @@ def test_query_shutoff_vms_project_id():
 
     server_object = mock_server_query.return_value
 
-    server_object.run.assert_called_once_with("openstack", from_projects=mock_project_id)
+    server_object.run.assert_called_once_with("openstack", as_admin=True, from_projects=mock_project_id)
+
+    user_query = server_object.then.return_value
+
+    user_query.run.assert_called_once_with("openstack")
 
 
-def test_query_user_details():
+def test_query_server_get_user_details():
     """
-    Test that we query to get username and email from user id
+    Tests that we get the details of the user from a server query
+    This tests the user query section of the query chaining used in the method
     """
-    mock_user_id = NonCallableMock()
+    mock_project_id = None
 
-    with patch('workflows.email_shutoff.UserQuery') as mock_user_query:
-        res = query_user_details(mock_user_id)
+    with patch('workflows.email_shutoff.ServerQuery') as mock_server_query:
+        res = query_shutoff_vms(mock_project_id)
 
-    user_object = mock_user_query.return_value
-    user_object.select.assert_called_once_with(UserProperties.USER_NAME, UserProperties.USER_EMAIL)
+    server_object = mock_server_query.return_value
+    server_object.then.assert_called_once_with("USER_QUERY", keep_previous_results=True)
 
-    select_object = user_object.select.return_value
-    select_object.where.assert_called_once_with(QueryPresetsGeneric.EQUAL_TO, UserProperties.USER_ID, value=mock_user_id)
+    user_query = server_object.then.return_value
+    user_query.select.assert_called_once_with(UserProperties.USER_EMAIL)
+    select_object = user_query.select.return_value
 
-    user_object.run.assert_called_once_with("openstack")
-    user_object.to_list.assert_called_once()
+    select_object.group_by.assert_called_once_with(UserProperties.USER_NAME)
 
-    assert res == user_object.to_list.return_value
+    user_query.run.assert_called_once()
 
+    user_query.to_string.assert_called_once()
 
-def test_extract_user_details():
-    mock_user_list = [NonCallableMock()]
-    mock_user_name = NonCallableMock()
-    mock_user_email = NonCallableMock()
-    mock_return_value = [{'user_name': mock_user_name,
-                         'user_email': mock_user_email}]
-
-    with patch('workflows.email_shutoff.query_user_details') as mock_query_user:
-        mock_query_user.return_value = mock_return_value
-        names, emails = extract_user_details(mock_user_list)
-
-    assert names[mock_user_list[0]] == mock_user_name
-    assert emails[mock_user_list[0]] == mock_user_email
-
-# TODO - Add test to check KeyError is raised when a user has no email address
+    assert res == user_query.to_string.return_value
 
 
-@raises(KeyError)
-def test_extract_user_details_no_email():
+def test_prepare_user_server_email():
     """
-    Test that KeyError is raised if a user has no email address
+    Tests that we extract the user_email and names of servers
+    from the server_query_result parameter
     """
-    # patch extract_user_details method?
-    # when method called, mock query_user_detail.return_value
+    fake_server_a = {'server_name': NonCallableMock(), 'user_email': 'example@example.com'}
+    fake_server_b = {'server_name': NonCallableMock(), 'user_email': 'example@example.com'}
 
-    # mock query_user_details method returning a value for user detail
-    # mock user_detail dictionary where user_detail['user_email'] is None
+    fake_query_result = [fake_server_a, fake_server_b]
 
-    # assert extract_user_details raises a KeyError?
+    expected_email = fake_server_a['user_email']
+    expected_server_names = [fake_server_a['server_name'], fake_server_b['server_name']]
+
+    result_email, result_server_names = prepare_user_server_email(fake_query_result)
+
+    assert result_email == expected_email
+    assert result_server_names == expected_server_names
 
 
-# TODO - Add test for main method
+def test_send_user_email():
+    """
+    Test that an email is sent with the send_email method
+    """
+    mock_smtp_account = NonCallableMock()
+    mock_email = NonCallableMock()
+    mock_subject = NonCallableMock()
+    with patch('workflows.email_shutoff.Emailer') as mock_emailer:
+        send_user_email(mock_smtp_account, mock_email, mock_subject)
+
+    mock_emailer.assert_called_once()
+    email_object = mock_emailer.return_value
+
+    expected_email_params = EmailParams(
+        subject=mock_subject,
+        email_from='cloud-support@stfc.ac.uk',
+        email_to=[mock_email],
+        email_templates=[]
+    )
+
+    email_object.send_emails.assert_called_once_with([expected_email_params])
 
 
-def test_main():
+@patch('workflows.email_shutoff.Emailer')
+def test_send_user_email_no_email_given(mock_emailer):
+    """
+    Tests that if no email address was associated with a user, then
+    the Cloud Support email address is used by default.
+    """
+    mock_smtp_account = NonCallableMock()
+    mock_email = None
+    mock_subject = NonCallableMock()
+
+    send_user_email(mock_smtp_account, mock_email, mock_subject)
+
+    expected_email_params = EmailParams(
+        subject=mock_subject,
+        email_from='cloud-support@stfc.ac.uk',
+        email_to=['cloud-support@stfc.ac.uk'],
+        email_templates=[]
+    )
+
+    mock_emailer.assert_called_once()
+    email_object = mock_emailer.return_value
+
+    email_object.send_emails.assert_called_once_with([expected_email_params])
+
+
+@patch('workflows.email_shutoff.query_shutoff_vms')
+@patch('workflows.email_shutoff.prepare_user_server_email')
+@patch('workflows.email_shutoff.send_user_email')
+def test_main(send_user_email, prepare_user_server_email, query_shutoff_vms):
     """
     Test that we can query for a shutoff machine,
     get the user_id and query to get details, then send email
     """
+    mock_project_id = NonCallableMock()
+    mock_smtp_account = NonCallableMock()
+    mock_data = {
+                'a': [{'server_name': NonCallableMock(), 'user_email': NonCallableMock()}],
+                'b': [{'server_name': NonCallableMock(), 'user_email': NonCallableMock()}]
+            }
 
-    pass
+    mocked_user_a = (NonCallableMock(), NonCallableMock())
+    mocked_user_b = (NonCallableMock(), NonCallableMock())
+
+    prepare_user_server_email.side_effect = [mocked_user_a, mocked_user_b]
+    query_shutoff_vms.return_value = mock_data
+
+    main(mock_smtp_account, mock_project_id)
+
+    query_shutoff_vms.assert_called_once_with(mock_project_id)
+
+    assert prepare_user_server_email.call_count == 2
+
+    prepare_user_server_email.assert_has_calls([call(mock_data["a"]), call(mock_data["b"])])
+
+    assert send_user_email.call_count == 2
+    expected_email_user_a = call(mock_smtp_account, mocked_user_a[0], mocked_user_a[1])
+    expected_email_user_b = call(mock_smtp_account, mocked_user_b[0], mocked_user_b[1])
+    expected_email_args = [expected_email_user_a, expected_email_user_b]
+    send_user_email.assert_has_calls(expected_email_args)
+
+
+
