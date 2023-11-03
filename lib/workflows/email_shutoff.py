96c8ec5f68e425a802f35a3148d03b039d9f31e7
meoflynn <megan.o'flynn@stfc.ac.uk>
2023-11-03 13:58:09 +0000

    ENH: Add extra params to methods
    Added extra parameters for querying shutoff servers and sending emails.
    Fixed formatting and broken unit tests as well.

--- lib/workflows/email_shutoff.py
+++ lib/workflows/email_shutoff.py
@@ -3,59 +3,47 @@ from openstack_query.api.query_objects import ServerQuery
 from enums.query.props.server_properties import ServerProperties
 from enums.query.props.user_properties import UserProperties
 from structs.email.email_params import EmailParams
-from enums.query.query_presets import QueryPresetsGeneric
+from enums.query.query_presets import QueryPresetsGeneric, QueryPresetsDateTime
 from email_api.emailer import Emailer
 from structs.email.smtp_account import SMTPAccount
 
 
-def query_shutoff_vms(project_id: Optional[List[str]] = None):
+def query_shutoff_vms(
+    cloud_account: str,
+    project_id: Optional[List[str]] = None,
+    days_threshold: int = None,
+):
     """
     Sends an email to a user about a shutoff (stopped) VM
+    :param cloud_account: Cloud account to use for running query
     :param project_id: optional field to provide a list of project IDs for specific projects,
     otherwise query all projects
+    :param days_threshold: param to query servers updated to be in shutoff state the last 30 days by default
     :returns: Dictionary with a list of VM names grouped by user ID
     """
 
     query_obj = ServerQuery()
 
-    query_obj.select(
-        ServerProperties.SERVER_NAME
+    query_obj.select(ServerProperties.SERVER_NAME).where(
+        QueryPresetsGeneric.EQUAL_TO, ServerProperties.SERVER_STATUS, value="SHUTOFF"
     ).where(
-        QueryPresetsGeneric.EQUAL_TO,
-        ServerProperties.SERVER_STATUS,
-        value="SHUTOFF")
+        QueryPresetsDateTime.OLDER_THAN,
+        ServerProperties.SERVER_LAST_UPDATED_DATE,
+        days=days_threshold,
+    )
 
     # if project ID provided as parameter
     if project_id:
-        query_obj.run(
-            "openstack",
-            as_admin=True,
-            from_projects=project_id
-        )
+        query_obj.run(cloud_account, as_admin=True, from_projects=project_id)
 
     else:
-        query_obj.run(
-            "openstack",
-            as_admin=True,
-            all_projects=True
-        )
+        query_obj.run(cloud_account, as_admin=True, all_projects=True)
 
     user_query = query_obj.then("USER_QUERY", keep_previous_results=True)
 
-    user_query.select(
-        UserProperties.USER_EMAIL
-    ).group_by(
-        UserProperties.USER_NAME
-    )
+    user_query.select(UserProperties.USER_EMAIL).group_by(UserProperties.USER_NAME)
 
-    if project_id:
-        user_query.run(
-            "openstack",
-            # from_projects=project_id
-        )
-
-    else:
-        user_query.run("openstack", as_admin=True, all_projects=True)
+    user_query.run(cloud_account)
 
     res = user_query.to_props()
 
@@ -66,49 +54,71 @@ def prepare_user_server_email(server_query_result: List[Dict]) -> Tuple[str, Lis
     """
     Extracts the user email and list of server names from a
     query object
-    :param: server_query_object: A list of dictionaries containing results from a query
+    :param server_query_result: A list of dictionaries containing results from a query
     :returns: A tuple containing the user email and list of VM names
     """
     details = server_query_result
     # store user email
-    assert all(details[0]['user_email'] == i['user_email'] for i in details)
-    user_email = details[0]['user_email']
+    assert all(details[0]["user_email"] == i["user_email"] for i in details)
+    user_email = details[0]["user_email"]
     # extract list of server names
-    server_names = [d['server_name'] for d in server_query_result]
+    server_names = [d["server_name"] for d in server_query_result]
     return user_email, server_names
 
 
-def send_user_email(smtp_account: SMTPAccount, user_email: str, subject_text: str, email_templates=None):
+def send_user_email(
+    smtp_account: SMTPAccount,
+    email_from: str,
+    user_email: str,
+    server_list: List[str],
+    email_templates=None,
+):
     """
     Sends an email to a user
-    :param: smtp_account: SMTP account object with Emailer config
-    :param: user_email: Email address for a specific user
-    :param: subject_text: Subject for the email
+    :param smtp_account: SMTP account object with Emailer config
+    :param email_from: Email address to email from
+    :param user_email: Email address for a specific user
+    :param server_list: List of servers from a query
+    :param email_templates: Template to use for emails
     """
+    # For now, subject is the list of VMs in shutoff state
 
     if not user_email:
         user_email = "cloud-support@stfc.ac.uk"
 
-    test_params = EmailParams(
-        subject=subject_text,
-        email_from='cloud-support@stfc.ac.uk',
+    subject = f"VM Shutoff {server_list}"
+    email_params = EmailParams(
+        subject=subject,
+        email_from=email_from,
         email_to=[user_email],
-        email_templates=[]
+        email_templates=[],
     )
 
-    Emailer(smtp_account).send_emails([test_params])
+    Emailer(smtp_account).send_emails([email_params])
 
 
-def main(smtp_account: SMTPAccount, project_id: Optional[List[str]] = None):
+def main(
+    smtp_account: SMTPAccount,
+    cloud_account: str = "openstack",
+    email_from: str = "cloud-support@stfc.ac.uk",
+    limit_by_project: Optional[List[str]] = None,
+    days_threshold: int = 30,
+):
     """
-    The main method for running the workflow.
-    :param: project_id: (Optional) A list of project IDs to query from
+    The main method for running the workflow
+    :param smtp_account: SMTPAccount object to use for emailing
+    :param cloud_account: Name of cloud account to use e.g. "openstack"
+    :param email_from: Email to send from
+    :param limit_by_project: (Optional) A list of project IDs to query from
+    :param days_threshold: Check for VMs updated in last 30 days in SHUTOFF state
     """
 
     # get VMs shutoff, grouped by user, include user email in results
-    shutoff_vm = query_shutoff_vms(project_id)
+    shutoff_vm = query_shutoff_vms(cloud_account, limit_by_project, days_threshold)
 
     for user in shutoff_vm.values():
-        user_email, server_name_list = prepare_user_server_email(user) # shutoff_vm[user] is list of dictionaries
-        send_user_email(smtp_account, user_email, server_name_list)
+        user_email, server_name_list = prepare_user_server_email(
+            user
+        )  # shutoff_vm[user] is list of dictionaries
 
+        send_user_email(smtp_account, email_from, user_email, server_name_list)
--- tests/lib/workflows/test_email_shutoff.py
+++ tests/lib/workflows/test_email_shutoff.py
@@ -1,9 +1,14 @@
 from unittest.mock import NonCallableMock, patch, call
 from enums.query.props.server_properties import ServerProperties
 from enums.query.props.user_properties import UserProperties
-from enums.query.query_presets import QueryPresetsGeneric
+from enums.query.query_presets import QueryPresetsGeneric, QueryPresetsDateTime
 from structs.email.email_params import EmailParams
-from workflows.email_shutoff import send_user_email, query_shutoff_vms, main, prepare_user_server_email
+from workflows.email_shutoff import (
+    send_user_email,
+    query_shutoff_vms,
+    main,
+    prepare_user_server_email,
+)
 
 
 def test_query_shutoff_vms_no_project_id():
@@ -12,21 +17,33 @@ def test_query_shutoff_vms_no_project_id():
     state
     Testing the case where no project id is given (query all projects)
     """
+    mock_cloud_account = NonCallableMock()
     mock_project_id = None
+    mock_days_threshold = NonCallableMock()
 
-    with patch('workflows.email_shutoff.ServerQuery') as mock_server_query:
-        res = query_shutoff_vms(mock_project_id)
+    with patch("workflows.email_shutoff.ServerQuery") as mock_server_query:
+        query_shutoff_vms(mock_cloud_account, mock_project_id, mock_days_threshold)
 
     server_object = mock_server_query.return_value
     server_object.select.assert_called_once_with(ServerProperties.SERVER_NAME)
 
     select_object = server_object.select.return_value
-    select_object.where.assert_called_once_with(QueryPresetsGeneric.EQUAL_TO, ServerProperties.SERVER_STATUS,value="SHUTOFF")
 
-    where_object = select_object.where.return_value
-    where_object.group_by.assert_called_once_with(ServerProperties.USER_ID)
+    select_object.where.assert_called_once_with(
+        QueryPresetsGeneric.EQUAL_TO, ServerProperties.SERVER_STATUS, value="SHUTOFF"
+    )
 
-    server_object.run.assert_called_once_with("openstack", as_admin=True, all_projects=True)
+    where_status_object = select_object.where.return_value
+
+    where_status_object.where.assert_called_once_with(
+        QueryPresetsDateTime.OLDER_THAN,
+        ServerProperties.SERVER_LAST_UPDATED_DATE,
+        days=mock_days_threshold,
+    )
+
+    server_object.run.assert_called_once_with(
+        mock_cloud_account, as_admin=True, all_projects=True
+    )
 
 
 def test_query_shutoff_vms_project_id():
@@ -34,19 +51,21 @@ def test_query_shutoff_vms_project_id():
     Test that we can query machines that are in shutoff (stopped) state
     Tests the case where a project id has been given
     """
-
+    mock_cloud_account = NonCallableMock()
     mock_project_id = NonCallableMock()
 
-    with patch('workflows.email_shutoff.ServerQuery') as mock_server_query:
-        query_shutoff_vms(mock_project_id)
+    with patch("workflows.email_shutoff.ServerQuery") as mock_server_query:
+        query_shutoff_vms(mock_cloud_account, mock_project_id)
 
     server_object = mock_server_query.return_value
 
-    server_object.run.assert_called_once_with("openstack", as_admin=True, from_projects=mock_project_id)
+    server_object.run.assert_called_once_with(
+        mock_cloud_account, as_admin=True, from_projects=mock_project_id
+    )
 
     user_query = server_object.then.return_value
 
-    user_query.run.assert_called_once_with("openstack")
+    user_query.run.assert_called_once_with(mock_cloud_account)
 
 
 def test_query_server_get_user_details():
@@ -54,10 +73,11 @@ def test_query_server_get_user_details():
     Tests that we get the details of the user from a server query
     This tests the user query section of the query chaining used in the method
     """
+    mock_cloud_account = NonCallableMock()
     mock_project_id = None
 
-    with patch('workflows.email_shutoff.ServerQuery') as mock_server_query:
-        res = query_shutoff_vms(mock_project_id)
+    with patch("workflows.email_shutoff.ServerQuery") as mock_server_query:
+        res = query_shutoff_vms(mock_cloud_account, mock_project_id)
 
     server_object = mock_server_query.return_value
     server_object.then.assert_called_once_with("USER_QUERY", keep_previous_results=True)
@@ -68,11 +88,11 @@ def test_query_server_get_user_details():
 
     select_object.group_by.assert_called_once_with(UserProperties.USER_NAME)
 
-    user_query.run.assert_called_once()
+    user_query.run.assert_called_once_with(mock_cloud_account)
 
-    user_query.to_string.assert_called_once()
+    user_query.to_props.assert_called_once()
 
-    assert res == user_query.to_string.return_value
+    assert res == user_query.to_props.return_value
 
 
 def test_prepare_user_server_email():
@@ -80,13 +100,19 @@ def test_prepare_user_server_email():
     Tests that we extract the user_email and names of servers
     from the server_query_result parameter
     """
-    fake_server_a = {'server_name': NonCallableMock(), 'user_email': 'example@example.com'}
-    fake_server_b = {'server_name': NonCallableMock(), 'user_email': 'example@example.com'}
+    fake_server_a = {
+        "server_name": NonCallableMock(),
+        "user_email": "example@example.com",
+    }
+    fake_server_b = {
+        "server_name": NonCallableMock(),
+        "user_email": "example@example.com",
+    }
 
     fake_query_result = [fake_server_a, fake_server_b]
 
-    expected_email = fake_server_a['user_email']
-    expected_server_names = [fake_server_a['server_name'], fake_server_b['server_name']]
+    expected_email = fake_server_a["user_email"]
+    expected_server_names = [fake_server_a["server_name"], fake_server_b["server_name"]]
 
     result_email, result_server_names = prepare_user_server_email(fake_query_result)
 
@@ -99,19 +125,23 @@ def test_send_user_email():
     Test that an email is sent with the send_email method
     """
     mock_smtp_account = NonCallableMock()
-    mock_email = NonCallableMock()
-    mock_subject = NonCallableMock()
-    with patch('workflows.email_shutoff.Emailer') as mock_emailer:
-        send_user_email(mock_smtp_account, mock_email, mock_subject)
+    mock_email_from = NonCallableMock()
+    mock_user_email = NonCallableMock()
+    mock_server_list = NonCallableMock()
+
+    with patch("workflows.email_shutoff.Emailer") as mock_emailer:
+        send_user_email(
+            mock_smtp_account, mock_email_from, mock_user_email, mock_server_list
+        )
 
     mock_emailer.assert_called_once()
     email_object = mock_emailer.return_value
 
     expected_email_params = EmailParams(
-        subject=mock_subject,
-        email_from='cloud-support@stfc.ac.uk',
-        email_to=[mock_email],
-        email_templates=[]
+        subject=f"VM Shutoff {mock_server_list}",
+        email_from=mock_email_from,
+        email_to=[mock_user_email],
+        email_templates=[],
     )
 
     email_object.send_emails.assert_called_once_with([expected_email_params])
@@ -123,16 +153,19 @@ def test_send_user_email_no_email_given():
     the Cloud Support email address is used by default.
     """
     mock_smtp_account = NonCallableMock()
-    mock_email = None
-    mock_subject = NonCallableMock()
-    with patch('workflows.email_shutoff.Emailer') as mock_emailer:
-        send_user_email(mock_smtp_account, mock_email, mock_subject)
+    mock_email_from = NonCallableMock()
+    mock_user_email = None
+    mock_server_list = NonCallableMock()
+    with patch("workflows.email_shutoff.Emailer") as mock_emailer:
+        send_user_email(
+            mock_smtp_account, mock_email_from, mock_user_email, mock_server_list
+        )
 
     expected_email_params = EmailParams(
-        subject=mock_subject,
-        email_from='cloud-support@stfc.ac.uk',
-        email_to=['cloud-support@stfc.ac.uk'],
-        email_templates=[]
+        subject=f"VM Shutoff {mock_server_list}",
+        email_from=mock_email_from,
+        email_to=["cloud-support@stfc.ac.uk"],
+        email_templates=[],
     )
 
     mock_emailer.assert_called_once()
@@ -141,20 +174,25 @@ def test_send_user_email_no_email_given():
     email_object.send_emails.assert_called_once_with([expected_email_params])
 
 
-@patch('workflows.email_shutoff.query_shutoff_vms')
-@patch('workflows.email_shutoff.prepare_user_server_email')
-@patch('workflows.email_shutoff.send_user_email')
+@patch("workflows.email_shutoff.query_shutoff_vms")
+@patch("workflows.email_shutoff.prepare_user_server_email")
+@patch("workflows.email_shutoff.send_user_email")
 def test_main(send_user_email, prepare_user_server_email, query_shutoff_vms):
     """
     Test that we can query for a shutoff machine,
     get the user_id and query to get details, then send email
     """
-    mock_project_id = NonCallableMock()
+
     mock_smtp_account = NonCallableMock()
+    mock_cloud_account = NonCallableMock()
+    mock_email_from = NonCallableMock()
+    mock_project_id = NonCallableMock()
+    mock_days_threshold = NonCallableMock()
+
     mock_data = {
-                'a': [{'server_name': NonCallableMock(), 'user_email': NonCallableMock()}],
-                'b': [{'server_name': NonCallableMock(), 'user_email': NonCallableMock()}]
-            }
+        "a": [{"server_name": NonCallableMock(), "user_email": NonCallableMock()}],
+        "b": [{"server_name": NonCallableMock(), "user_email": NonCallableMock()}],
+    }
 
     mocked_user_a = (NonCallableMock(), NonCallableMock())
     mocked_user_b = (NonCallableMock(), NonCallableMock())
@@ -162,19 +200,31 @@ def test_main(send_user_email, prepare_user_server_email, query_shutoff_vms):
     prepare_user_server_email.side_effect = [mocked_user_a, mocked_user_b]
     query_shutoff_vms.return_value = mock_data
 
-    main(mock_smtp_account, mock_project_id)
+    main(
+        mock_smtp_account,
+        mock_cloud_account,
+        mock_email_from,
+        mock_project_id,
+        mock_days_threshold,
+    )
 
-    query_shutoff_vms.assert_called_once_with(mock_project_id)
+    query_shutoff_vms.assert_called_once_with(
+        mock_cloud_account, mock_project_id, mock_days_threshold
+    )
 
     assert prepare_user_server_email.call_count == 2
 
-    prepare_user_server_email.assert_has_calls([call(mock_data["a"]), call(mock_data["b"])])
+    prepare_user_server_email.assert_has_calls(
+        [call(mock_data["a"]), call(mock_data["b"])]
+    )
 
     assert send_user_email.call_count == 2
-    expected_email_user_a = call(mock_smtp_account, mocked_user_a[0], mocked_user_a[1])
-    expected_email_user_b = call(mock_smtp_account, mocked_user_b[0], mocked_user_b[1])
+
+    expected_email_user_a = call(
+        mock_smtp_account, mock_email_from, mocked_user_a[0], mocked_user_a[1]
+    )
+    expected_email_user_b = call(
+        mock_smtp_account, mock_email_from, mocked_user_b[0], mocked_user_b[1]
+    )
     expected_email_args = [expected_email_user_a, expected_email_user_b]
     send_user_email.assert_has_calls(expected_email_args)
-
-
-
