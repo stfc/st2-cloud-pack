16716b094115d51f2ff54a1313002e97b33e7835
anish-mudaraddi <anish.mudaraddi@stfc.ac.uk>
2023-12-04 11:59:16 +0000

    MAINT: code review changes
    removed parametrize and changed fixture name

--- tests/lib/openstack_query/query_blocks/test_query_builder.py
+++ tests/lib/openstack_query/query_blocks/test_query_builder.py
@@ -67,19 +67,19 @@ def instance_fixture(
     )
 
 
-@pytest.fixture(name="run_parse_where_test")
-def run_parse_where_test_fixture(
+@pytest.fixture(name="parse_where_runner")
+def parse_where_runner_fixture(
     instance, mock_server_side_handler, mock_valid_client_handler
 ):
     """
-    This fixture runs test cases of parse_where, where we expect it to complete successfully,
+    This is a helper function that runs test cases of parse_where, where we expect it to complete successfully,
     In each test case we change what server_side_handler.get_filters and client_side_handler.get_filter_func returns
     This will change what is set in the client_side_filters and server_side_filters properties at the end
     """
     mock_kwargs = {"arg1": "val1", "arg2": "val2"}
 
     # pylint:disable=too-many-arguments
-    def _run_parse_where_test_case(
+    def _parse_where_runner(
         mock_get_filter_return,
         mock_get_filter_func_return,
         expected_server_side_filters,
@@ -123,15 +123,15 @@ def run_parse_where_test_fixture(
         assert test_instance.server_side_filters == expected_server_side_filters
         assert test_instance.server_filter_fallback == expected_fallback_filters
 
-    return _run_parse_where_test_case
+    return _parse_where_runner
 
 
-def test_parse_where_with_single_filter(run_parse_where_test):
+def test_parse_where_with_single_filter(parse_where_runner):
     """
-    Tests that parse_where where server-side-handler get_filters returns a single filter set
+    Tests parse_where where server-side-handler get_filters returns a single filter set
     """
     mock_client_filter = NonCallableMock()
-    run_parse_where_test(
+    parse_where_runner(
         mock_get_filter_return=[{"filter1": "val1", "filter2": "val2"}],
         mock_get_filter_func_return=mock_client_filter,
         expected_server_side_filters=[{"filter1": "val1", "filter2": "val2"}],
@@ -140,13 +140,13 @@ def test_parse_where_with_single_filter(run_parse_where_test):
     )
 
 
-def test_parse_where_with_filter_list(run_parse_where_test):
+def test_parse_where_with_filter_list(parse_where_runner):
     """
-    Tests that parse_where where server-side-handler get_filters returns a list of filters
+    Tests parse_where where server-side-handler get_filters returns a list of filters
     """
     mock_client_filter = NonCallableMock()
     mock_server_filter = [{"filter1": "val1"}, {"filter2": "val2"}]
-    run_parse_where_test(
+    parse_where_runner(
         mock_get_filter_return=mock_server_filter,
         mock_get_filter_func_return=mock_client_filter,
         expected_server_side_filters=mock_server_filter,
@@ -155,13 +155,13 @@ def test_parse_where_with_filter_list(run_parse_where_test):
     )
 
 
-def test_parse_where_with_no_filters(run_parse_where_test):
+def test_parse_where_with_no_filters(parse_where_runner):
     """
-    Tests that parse_where where server-side-handler get_filters returns no filters
+    Tests parse_where where server-side-handler get_filters returns no filters
     """
     mock_client_filter = NonCallableMock()
     mock_server_filter = None
-    run_parse_where_test(
+    parse_where_runner(
         mock_get_filter_return=mock_server_filter,
         mock_get_filter_func_return=mock_client_filter,
         expected_server_side_filters=[],
@@ -170,110 +170,161 @@ def test_parse_where_with_no_filters(run_parse_where_test):
     )
 
 
-@pytest.mark.parametrize(
-    "existing_filters, new_filters, expected_out",
-    [
-        # one (non-overlapping) server-side filter set
-        # one server-side filter set to add
-        (
-            [{"filter1": "val1"}],
-            [{"filter2": "val2"}],
-            [{"filter1": "val1", "filter2": "val2"}],
-        ),
-        # multiple (non-overlapping) server-side filters set, one
-        # one server-side filter set to add
-        (
-            [{"filter1": "val1"}, {"filter2": "val2"}],
-            [{"filter3": "val3"}],
-            [
-                {"filter1": "val1", "filter3": "val3"},
-                {"filter2": "val2", "filter3": "val3"},
-            ],
-        ),
-        # multiple (non-overlapping) server-side filters set
-        # multiple server-side filter sets to add
-        (
-            [{"filter1": "val1"}, {"filter2": "val2"}],
-            [{"filter3": "val3"}, {"filter4": "val4"}],
-            [
-                {"filter1": "val1", "filter3": "val3"},
-                {"filter1": "val1", "filter4": "val4"},
-                {"filter2": "val2", "filter3": "val3"},
-                {"filter2": "val2", "filter4": "val4"},
-            ],
-        ),
-    ],
-)
-def test_parse_where_when_filter_exists(
-    existing_filters, new_filters, expected_out, instance, run_parse_where_test
+@pytest.fixture(name="parse_where_runner_with_server_side_filter_set")
+def parse_where_runner_with_server_side_filter_set_fixture(
+    instance, parse_where_runner
 ):
     """
-    Tests parse_where, for various cases where adding new preset requires a new
-    server-side filter to be added, to an already populated server_side_filters property from previous presets
-    Tests that the two filters are concatenated properly
+    A fixture to setup and test different test-cases for parse_where when server side filter is already set
     """
-    instance.server_side_filters = existing_filters
 
-    filter1_fallback_func = NonCallableMock()
-    instance.server_filter_fallback = [filter1_fallback_func]
+    def _server_side_filter_set_runner(
+        expected_server_side_filters,
+        current_server_side_filters,
+        new_server_side_filters,
+        expect_conflict=False,
+    ):
+        """
+        function to setup and test different test-cases for parse_where when server side filter is already set.
 
-    mock_client_filter = NonCallableMock()
-    run_parse_where_test(
-        mock_get_filter_return=new_filters,
-        mock_get_filter_func_return=mock_client_filter,
-        expected_server_side_filters=expected_out,
-        expected_client_side_filters=[],
-        expected_fallback_filters=[filter1_fallback_func, mock_client_filter],
+        :param expected_server_side_filters: a list of server-side-filters we expect the parse_where method to set
+        after calling it
+        :param current_server_side_filters: a list of filters that have already been set
+        :param new_server_side_filters: a list of filters to add
+        :param expect_conflict: whether we expect server-side filter conflict to be detected
+            - (If we expect a conflict, we expect that client-side filter will be set
+            and the new server-side filters to be ignored)
+
+        """
+        instance.server_side_filters = current_server_side_filters
+
+        filter1_fallback_func = NonCallableMock()
+        instance.server_filter_fallback = [filter1_fallback_func]
+
+        mock_client_filter = NonCallableMock()
+        if expect_conflict:
+            parse_where_runner(
+                mock_get_filter_return=new_server_side_filters,
+                mock_get_filter_func_return=mock_client_filter,
+                expected_server_side_filters=expected_server_side_filters,
+                expected_client_side_filters=[mock_client_filter],
+                expected_fallback_filters=[filter1_fallback_func],
+            )
+        else:
+            # conflict occurred - don't add server-side filter, instead add client_side filter
+            parse_where_runner(
+                mock_get_filter_return=new_server_side_filters,
+                mock_get_filter_func_return=mock_client_filter,
+                expected_server_side_filters=expected_server_side_filters,
+                expected_client_side_filters=[],
+                expected_fallback_filters=[filter1_fallback_func, mock_client_filter],
+            )
+
+    return _server_side_filter_set_runner
+
+
+def test_parse_where_single_filter_set_non_conflicting(
+    parse_where_runner_with_server_side_filter_set,
+):
+    """
+    Tests parse_where where server-side-handler get_filters returns a single server-side filter, when another
+    (non-conflicting) filter is already set - should concatenate the two filter sets into one set
+    """
+    parse_where_runner_with_server_side_filter_set(
+        expected_server_side_filters=[{"filter1": "val1", "filter2": "val2"}],
+        current_server_side_filters=[{"filter1": "val1"}],
+        new_server_side_filters=[{"filter2": "val2"}],
     )
 
 
-@pytest.mark.parametrize(
-    "existing_filters, new_filters",
-    [
-        # one server-side filter set
-        # same filter to add again - conflicting
-        (
-            [{"filter1": "val1"}],
-            [{"filter1": "val2"}],
-        ),
-        # multiple server-side filters set
-        # one to add which conflicts with existing filter in set
-        (
-            [{"filter1": "val1"}, {"filter2": "val2"}],
-            [{"filter2": "val3"}],
-        ),
-        # multiple server-side filters with multiple items in them
-        # one to add which conflicts with one filter of one item in set
-        (
-            [
-                {"filter1": "val1", "filter3": "val3"},
-                {"filter1": "val1", "filter4": "val4"},
-                {"filter2": "val2", "filter3": "val3"},
-                {"filter2": "val2", "filter4": "val4"},
-            ],
-            [{"filter3": "val4"}],
-        ),
-    ],
-)
-def test_add_filter_conflicting_presets(
-    existing_filters, new_filters, instance, run_parse_where_test
+def test_parse_where_multi_filter_set_non_conflicting_add_single(
+    parse_where_runner_with_server_side_filter_set,
 ):
     """
-    Tests parse_where - when a conflict occurs between server-side filter params
-    In this case, we add the new client_side filter to client_side_filters attribute instead
+    Tests parse_where where server-side-handler get_filters returns a single server-side filter, when
+    multiple (non-conflicting) filters already set - should concatenate new filter set onto each currently existing set
     """
-    instance.server_side_filters = existing_filters
 
-    filter1_fallback_func = NonCallableMock()
-    instance.server_filter_fallback = [filter1_fallback_func]
+    parse_where_runner_with_server_side_filter_set(
+        expected_server_side_filters=[
+            {"filter1": "val1", "filter3": "val3"},
+            {"filter2": "val2", "filter3": "val3"},
+        ],
+        current_server_side_filters=[{"filter1": "val1"}, {"filter2": "val2"}],
+        new_server_side_filters=[{"filter3": "val3"}],
+    )
 
-    mock_client_filter = NonCallableMock()
-    run_parse_where_test(
-        mock_get_filter_return=new_filters,
-        mock_get_filter_func_return=mock_client_filter,
-        expected_server_side_filters=existing_filters,
-        expected_client_side_filters=[mock_client_filter],
-        expected_fallback_filters=[filter1_fallback_func],
+
+def test_parse_where_multi_filter_set_non_conflicting_add_multi(
+    parse_where_runner_with_server_side_filter_set,
+):
+    """
+    Tests parse_where where server-side-handler get_filters returns multiple server-side filters, when
+    multiple (non-conflicting) filters already set - should create a list with all unique combinations of filters
+    between the two lists
+    """
+    parse_where_runner_with_server_side_filter_set(
+        expected_server_side_filters=[
+            {"filter1": "val1", "filter3": "val3"},
+            {"filter1": "val1", "filter4": "val4"},
+            {"filter2": "val2", "filter3": "val3"},
+            {"filter2": "val2", "filter4": "val4"},
+        ],
+        current_server_side_filters=[{"filter1": "val1"}, {"filter2": "val2"}],
+        new_server_side_filters=[{"filter3": "val3"}, {"filter4": "val4"}],
+    )
+
+
+def test_parse_where_single_filter_set_conflicting(
+    parse_where_runner_with_server_side_filter_set,
+):
+    """
+    Tests parse_where where server-side-handler get_filters returns a single server-side filter, when another
+    (conflicting) filter is already set - should add the new filter as a client-side filter
+    """
+    current_filters = [{"filter1": "val1"}]
+    parse_where_runner_with_server_side_filter_set(
+        expected_server_side_filters=current_filters,
+        current_server_side_filters=current_filters,
+        new_server_side_filters=[{"filter1": "val2"}],
+        expect_conflict=True,
+    )
+
+
+def test_parse_where_multi_filter_set_conflicting_add_single(
+    parse_where_runner_with_server_side_filter_set,
+):
+    """
+    Tests parse_where where server-side-handler get_filters returns a single server-side filter, when
+    multiple filters already set which are conflicting - should add a the new filter as a client-side filter
+    """
+    current_filters = [{"filter1": "val1"}, {"filter2": "val2"}]
+    parse_where_runner_with_server_side_filter_set(
+        expected_server_side_filters=current_filters,
+        current_server_side_filters=current_filters,
+        new_server_side_filters=[{"filter2": "val3"}],
+        expect_conflict=True,
+    )
+
+
+def test_parse_where_multi_filter_set_conflicting_add_multi(
+    parse_where_runner_with_server_side_filter_set,
+):
+    """
+    Tests parse_where where server-side-handler get_filters returns multiple server-side filters, when
+    multiple filters already set which conflict - should add the new filter as a client-side filter
+    """
+    current_filters = [
+        {"filter1": "val1", "filter3": "val3"},
+        {"filter1": "val1", "filter4": "val4"},
+        {"filter2": "val2", "filter3": "val3"},
+        {"filter2": "val2", "filter4": "val4"},
+    ]
+    parse_where_runner_with_server_side_filter_set(
+        expected_server_side_filters=current_filters,
+        current_server_side_filters=current_filters,
+        new_server_side_filters=[{"filter3": "val4"}],
+        expect_conflict=True,
     )
 
 
