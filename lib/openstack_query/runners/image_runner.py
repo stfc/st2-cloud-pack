ad85808c20fcb2d02364b5424203a137391ccd7d
anish-mudaraddi <anish.mudaraddi@stfc.ac.uk>
2024-01-02 17:13:36 +0000

    ENH: Add Image Runner

--- lib/openstack_query/runners/image_runner.py
+++ lib/openstack_query/runners/image_runner.py
@@ -1,28 +1,25 @@
 from typing import Optional, List, Dict
 import logging
 
-from openstack.compute.v2.server import Server
+from openstack.compute.v2.image import Image
 
 from openstack_api.openstack_connection import OpenstackConnection
 from openstack_query.runners.runner_utils import RunnerUtils
 from openstack_query.runners.runner_wrapper import RunnerWrapper
 
 from exceptions.parse_query_error import ParseQueryError
-from custom_types.openstack_query.aliases import (
-    ProjectIdentifier,
-    ServerSideFilters,
-)
+from custom_types.openstack_query.aliases import ServerSideFilters, ProjectIdentifier
 
 logger = logging.getLogger(__name__)
 
 
-class ServerRunner(RunnerWrapper):
+class ImageRunner(RunnerWrapper):
     """
-    Runner class for openstack Server resource.
-    ServerRunner encapsulates running any openstacksdk Server commands
+    Runner class for openstack image resource
+    ImageRunner encapsulated running any openstacksdk Image commands
     """
 
-    RESOURCE_TYPE = Server
+    RESOURCE_TYPE = Image
 
     def parse_meta_params(
         self,
@@ -32,13 +29,13 @@ class ServerRunner(RunnerWrapper):
         as_admin: bool = False,
     ) -> Dict:
         """
-        This method is a helper function that will parse a set of query meta params related to openstack server queries.
+        This method is a helper function that will parse a set of meta params specific to the resource and
+        return a set of parsed meta-params to pass to _run_query
         :param conn: An OpenstackConnection object - used to connect to openstack and parse meta params
         :param from_projects: A list of projects to search in
         :param all_projects: A boolean which, if true - will run query on all available projects to the user
         :param as_admin: A boolean which, if true - will run query as an admin
         """
-
         # raise error if ambiguous query
         if from_projects and all_projects:
             raise ParseQueryError(
@@ -56,67 +53,48 @@ class ServerRunner(RunnerWrapper):
 
         # don't provide any projects to scope the query so it runs on all projects
         if all_projects:
-            return {"all_tenants": True}
-
-        # all_tenants only works if admin
-        if not as_admin:
-            return {"projects": projects}
-        return {"all_tenants": True, "projects": projects}
+            return {}
+        return {"owners": projects}
 
     def run_query(
         self,
         conn: OpenstackConnection,
         filter_kwargs: Optional[ServerSideFilters] = None,
         **meta_params,
-    ) -> List[Server]:
+    ) -> List[Image]:
         """
         This method runs the query by running openstacksdk commands
 
-        For ServerQuery, this command gets all projects available and iteratively finds servers that belong to that
-        project
+        For ImageQuery, this command finds all images that match a given set of filter_kwargs.
+        If meta-param from_projects passed, it will limit search to images that belong to those projects only
         :param conn: An OpenstackConnection object - used to connect to openstacksdk
-        :param filter_kwargs: An Optional list of filter kwargs to pass to conn.compute.servers()
-            to limit the servers being returned. - see https://docs.openstack.org/api-ref/compute/#list-servers
+        :param filter_kwargs: An Optional list of filter kwargs to pass to conn.compute.images()
+            to limit the images being returned.
+            see https://docs.openstack.org/api-ref/image/v2/index.html#list-images
         :param meta_params: a set of meta parameters that dictates how the query is run
         """
         if not filter_kwargs:
             filter_kwargs = {}
 
-        if "project_id" in filter_kwargs.keys() and "projects" in meta_params:
-            raise ParseQueryError(
-                "This query uses a preset that requires searching on project_ids "
-                "- but you've provided projects to search in using from_projects meta param"
-                "- please use one or the other, not both"
-                "- or you are not running as admin"
-            )
-
-        if "all_tenants" in meta_params:
-            filter_kwargs["all_tenants"] = meta_params["all_tenants"]
-
-        if "projects" not in meta_params:
-            logger.debug(
-                "running openstacksdk command conn.compute.servers (%s)",
-                ", ".join(f"{key}={value}" for key, value in filter_kwargs.items()),
-            )
+        if not meta_params:
             return RunnerUtils.run_paginated_query(
-                conn.compute.servers, self._page_marker_prop_func, dict(filter_kwargs)
+                conn.compute.images, self._page_marker_prop_func, dict(filter_kwargs)
             )
-
         query_res = []
-        project_num = len(meta_params["projects"])
+        project_num = len(meta_params["owners"])
         logger.debug("running query on %s projects", project_num)
-        for i, project_id in enumerate(meta_params["projects"], 1):
-            filter_kwargs.update({"project_id": project_id})
+        for i, project_id in enumerate(meta_params["owners"], 1):
+            filter_kwargs.update({"owner": project_id})
             logger.debug(
                 "running query on project %s / %s (id: %s)", i, project_num, project_id
             )
             logger.debug(
-                "running openstacksdk command conn.compute.servers (%s)",
+                "running openstacksdk command conn.compute.images (%s)",
                 ", ".join(f"{key}={value}" for key, value in filter_kwargs.items()),
             )
             query_res.extend(
                 RunnerUtils.run_paginated_query(
-                    conn.compute.servers,
+                    conn.compute.images,
                     self._page_marker_prop_func,
                     dict(filter_kwargs),
                 )
