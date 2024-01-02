8347fb6c59dde8ab4da570e08836ef10a483b585
anish-mudaraddi <anish.mudaraddi@stfc.ac.uk>
2024-01-02 14:54:05 +0000

    ENH: add Image Properties Enum

---
+++ lib/enums/query/props/image_properties.py
@@ -0,0 +1,70 @@
+from enum import auto
+from enums.query.props.prop_enum import PropEnum
+from exceptions.parse_query_error import ParseQueryError
+from exceptions.query_property_mapping_error import QueryPropertyMappingError
+
+
+class ImageProperties(PropEnum):
+    """
+    An enum class for all image properties
+    """
+
+    IMAGE_CREATION_DATE = auto()
+    IMAGE_CREATION_PROGRESS = auto()
+    IMAGE_ID = auto()
+    IMAGE_LAST_UPDATED_DATE = auto()
+    IMAGE_MINIMUM_DISK = auto()
+    IMAGE_MINIMUM_RAM = auto()
+    IMAGE_NAME = auto()
+    IMAGE_SIZE = auto()
+    IMAGE_STATUS = auto()
+
+    @staticmethod
+    def _get_aliases():
+        """
+        A method that returns all valid string alias mappings
+        """
+        return {
+            ImageProperties.IMAGE_CREATION_DATE: ["created_at"],
+            ImageProperties.IMAGE_CREATION_PROGRESS: ["progress"],
+            ImageProperties.IMAGE_ID: ["id", "uuid"],
+            ImageProperties.IMAGE_LAST_UPDATED_DATE: ["updated_at"],
+            ImageProperties.IMAGE_MINIMUM_RAM: ["min_ram", "ram"],
+            ImageProperties.IMAGE_MINIMUM_DISK: ["min_disk", "disk"],
+            ImageProperties.IMAGE_NAME: ["name"],
+            ImageProperties.IMAGE_SIZE: ["size"],
+            ImageProperties.IMAGE_STATUS: ["status"],
+        }
+
+    @staticmethod
+    def get_prop_mapping(prop):
+        """
+        Method that returns the property function if function mapping exists for a given FlavorProperty Enum
+        how to get specified property from an openstacksdk Flavor object is documented here:
+        https://docs.openstack.org/openstacksdk/latest/user/resources/compute/v2/image.html#openstack.compute.v2.image.Image
+        :param prop: A FlavorProperty Enum for which a function may exist for
+        """
+        mapping = {
+            ImageProperties.IMAGE_CREATION_DATE: lambda a: a["created_at"],
+            ImageProperties.IMAGE_CREATION_PROGRESS: lambda a: a["progress"],
+            ImageProperties.IMAGE_ID: lambda a: a["id"],
+            ImageProperties.IMAGE_LAST_UPDATED_DATE: lambda a: a["updated_at"],
+            ImageProperties.IMAGE_MINIMUM_DISK: lambda a: a["min_disk"],
+            ImageProperties.IMAGE_MINIMUM_RAM: lambda a: a["min_ram"],
+            ImageProperties.IMAGE_NAME: lambda a: a["name"],
+            ImageProperties.IMAGE_SIZE: lambda a: a["size"],
+            ImageProperties.IMAGE_STATUS: lambda a: a["status"],
+        }
+        try:
+            return mapping[prop]
+        except KeyError as exp:
+            raise QueryPropertyMappingError(
+                f"Error: failed to get property mapping, property {prop.name} is not supported in ImageProperties"
+            ) from exp
+
+    @staticmethod
+    def get_marker_prop_func():
+        """
+        A getter method to return marker property function for pagination
+        """
+        return ImageProperties.get_prop_mapping(ImageProperties.IMAGE_ID)
---
+++ tests/enums/props/test_image_properties.py
@@ -0,0 +1,138 @@
+from unittest.mock import patch
+
+from enums.query.props.image_properties import ImageProperties
+import pytest
+
+from exceptions.query_property_mapping_error import QueryPropertyMappingError
+from tests.lib.openstack_query.mocks.mocked_props import MockProperties
+
+
+@pytest.mark.parametrize("prop", list(ImageProperties))
+def test_get_prop_mapping(prop):
+    """
+    Tests that all image properties have a property function mapping
+    """
+    ImageProperties.get_prop_mapping(prop)
+
+
+def test_get_prop_mapping_invalid():
+    """
+    Tests that get_prop_mapping returns Error if property not supported
+    """
+    with pytest.raises(QueryPropertyMappingError):
+        ImageProperties.get_prop_mapping(MockProperties.PROP_1)
+
+
+@patch("enums.query.props.image_properties.ImageProperties.get_prop_mapping")
+def test_get_marker_prop_func(mock_get_prop_mapping):
+    """
+    Tests that marker_prop_func returns get_prop_mapping called with IMAGE_ID
+    """
+    val = ImageProperties.get_marker_prop_func()
+    mock_get_prop_mapping.assert_called_once_with(ImageProperties.IMAGE_ID)
+    assert val == mock_get_prop_mapping.return_value
+
+
+@pytest.mark.parametrize(
+    "val",
+    ["image_creation_date", "Image_Creation", "ImAgE_CrEaTiOn_DaTe", "created_at"],
+)
+def test_image_creation_date_serialization(val):
+    """
+    Tests that variants of IMAGE_CREATION_DATE can be serialized
+    """
+    assert ImageProperties.from_string(val) is ImageProperties.IMAGE_CREATION_DATE
+
+
+@pytest.mark.parametrize(
+    "val",
+    [
+        "image_creation_progress",
+        "Image_creation_Progress",
+        "ImAgE_CrEaTiOn_PrOgReSs",
+        "progress",
+    ],
+)
+def test_image_creation_progress_serialization(val):
+    """
+    Tests that variants of IMAGE_CREATION_PROGRESS can be serialized
+    """
+    assert ImageProperties.from_string(val) is ImageProperties.IMAGE_CREATION_PROGRESS
+
+
+@pytest.mark.parametrize("val", ["image_id", "Image_Id", "ImAgE_Id", "id", "uuid"])
+def test_image_creation_progress_serialization(val):
+    """
+    Tests that variants of IMAGE_ID can be serialized
+    """
+    assert ImageProperties.from_string(val) is ImageProperties.IMAGE_ID
+
+
+@pytest.mark.parametrize(
+    "val",
+    [
+        "image_last_updated_date",
+        "Image_Last_Updated_Date",
+        "ImAgE_LaSt_UpDaTeD_DaTe",
+        "updated_at",
+    ],
+)
+def test_image_last_updated_date_serialization(val):
+    """
+    Tests that variants of IMAGE_LAST_UPDATED_DATE can be serialized
+    """
+    assert ImageProperties.from_string(val) is ImageProperties.IMAGE_LAST_UPDATED_DATE
+
+
+@pytest.mark.parametrize(
+    "val",
+    ["image_minimum_ram", "Image_Minimum_RAM", "ImAgE_MiNiMuM_RaM", "min_ram", "ram"],
+)
+def test_image_minimum_ram_serialization(val):
+    """
+    Tests that variants of IMAGE_MINIMUM_RAM can be serialized
+    """
+    assert ImageProperties.from_string(val) is ImageProperties.IMAGE_MINIMUM_RAM
+
+
+@pytest.mark.parametrize(
+    "val",
+    [
+        "image_minimum_disk",
+        "Image_Minimum_Disk",
+        "ImAgE_MiNiMuM_DiSk",
+        "min_disk",
+        "disk",
+    ],
+)
+def test_image_minimum_disk_serialization(val):
+    """
+    Tests that variants of IMAGE_MINIMUM_DISK can be serialized
+    """
+    assert ImageProperties.from_string(val) is ImageProperties.IMAGE_MINIMUM_DISK
+
+
+@pytest.mark.parametrize("val", ["image_name", "Image_Name", "ImAgE_NaMe", "name"])
+def test_image_name_serialization(val):
+    """
+    Tests that variants of IMAGE_NAME can be serialized
+    """
+    assert ImageProperties.from_string(val) is ImageProperties.IMAGE_NAME
+
+
+@pytest.mark.parametrize("val", ["image_size", "Image_Size", "ImAgE_SiZe", "size"])
+def test_image_size_serialization(val):
+    """
+    Tests that variants of IMAGE_SIZE can be serialized
+    """
+    assert ImageProperties.from_string(val) is ImageProperties.IMAGE_SIZE
+
+
+@pytest.mark.parametrize(
+    "val", ["image_status", "Image_Status", "ImAgE_StAtUs", "status"]
+)
+def test_image_status_serialization(val):
+    """
+    Tests that variants of IMAGE_STATUS can be serialized
+    """
+    assert ImageProperties.from_string(val) is ImageProperties.IMAGE_STATUS
