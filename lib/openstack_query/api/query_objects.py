85f8bb7cf96f0e35ef2861b6f04a742f35019828
anish-mudaraddi <anish.mudaraddi@stfc.ac.uk>
2024-03-14 16:37:06 +0000

    BUG: QueryTypes/QueryChainer circular imports
    there is an annoying circular import issue with how chaining works - this is a hotfix to get the tests working but a redesign of chaining is required

--- lib/openstack_query/api/query_objects.py
+++ lib/openstack_query/api/query_objects.py
@@ -1,7 +1,5 @@
 from typing import Type
-from openstack_query.api.query_api import QueryAPI
 from openstack_query.mappings.mapping_interface import MappingInterface
-from openstack_query.query_factory import QueryFactory
 from openstack_query.mappings.user_mapping import UserMapping
 from openstack_query.mappings.server_mapping import ServerMapping
 from openstack_query.mappings.flavor_mapping import FlavorMapping
@@ -9,12 +7,15 @@ from openstack_query.mappings.project_mapping import ProjectMapping
 from openstack_query.mappings.image_mapping import ImageMapping
 
 
-def get_common(query_mapping: Type[MappingInterface]) -> QueryAPI:
+def get_common(query_mapping: Type[MappingInterface]) -> "QueryAPI":
     """
     helper function to create query object from given mapping class
     using QueryFactory
     :param query_mapping: a mapping class that defines property, runner and handler mappings
     """
+    from openstack_query.api.query_api import QueryAPI
+    from openstack_query.query_factory import QueryFactory
+
     return QueryAPI(QueryFactory.build_query_deps(query_mapping))
 
 
@@ -22,35 +23,35 @@ def get_common(query_mapping: Type[MappingInterface]) -> QueryAPI:
 # pylint:disable=invalid-name
 
 
-def ServerQuery() -> QueryAPI:
+def ServerQuery() -> "QueryAPI":
     """
     Simple helper function to setup a query using a factory
     """
     return get_common(ServerMapping)
 
 
-def UserQuery() -> QueryAPI:
+def UserQuery() -> "QueryAPI":
     """
     Simple helper function to setup a query using a factory
     """
     return get_common(UserMapping)
 
 
-def FlavorQuery() -> QueryAPI:
+def FlavorQuery() -> "QueryAPI":
     """
     Simple helper function to setup a query using a factory
     """
     return get_common(FlavorMapping)
 
 
-def ProjectQuery() -> QueryAPI:
+def ProjectQuery() -> "QueryAPI":
     """
     Simple helper function to setup a query using a factory
     """
     return get_common(ProjectMapping)
 
 
-def ImageQuery() -> QueryAPI:
+def ImageQuery() -> "QueryAPI":
     """
     Simple helper function to setup a query using a factory
     """
