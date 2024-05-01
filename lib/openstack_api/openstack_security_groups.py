cd988d9aa58747a845602b6295d845970d2aa327
anish-mudaraddi <anish.mudaraddi@stfc.ac.uk>
2024-05-01 15:05:09 +0100

    BUG: fix not applying external security groups
    previously applying internal security groups to external projects - added a method and altered orquesta workflow to use new method

--- lib/openstack_api/openstack_security_groups.py
+++ lib/openstack_api/openstack_security_groups.py
@@ -3,6 +3,8 @@ from typing import Optional, List
 from openstack.network.v2.security_group import SecurityGroup
 from openstack.network.v2.security_group_rule import SecurityGroupRule
 
+from enums.ip_version import IPVersion
+from enums.network_direction import NetworkDirection
 from enums.protocol import Protocol
 from exceptions.item_not_found_error import ItemNotFoundError
 from exceptions.missing_mandatory_param_error import MissingMandatoryParamError
@@ -16,6 +18,220 @@ class OpenstackSecurityGroups(OpenstackWrapperBase):
     def __init__(self, connection_cls=OpenstackConnection):
         super().__init__(connection_cls)
         self._identity_api = OpenstackIdentity(self._connection_cls)
+
+    def create_external_security_group_rules(
+        self, cloud_account, project_identifier, security_group_identifier
+    ):
+        """
+        Sets external security group rules
+        :param cloud_account: The associated clouds.yaml account
+        :param project_identifier: the name or the Openstack ID of the associated project
+        :param security_group_identifier: The name or the Openstack ID of the associated security group
+        """
+        # default network external rules
+        default_external_rules = [
+            {
+                "direction": NetworkDirection.INGRESS,
+                "ip_version": IPVersion.IPV4,
+                "protocol": Protocol.ICMP,
+                "remote_ip_cidr": "0.0.0.0/0",
+                "port_range": ("*", "*"),
+            },
+            {
+                "direction": NetworkDirection.INGRESS,
+                "ip_version": IPVersion.IPV4,
+                "protocol": Protocol.TCP,
+                "remote_ip_cidr": "0.0.0.0/0",
+                "port_range": ("22", "22"),
+            },
+            {
+                "direction": NetworkDirection.EGRESS,
+                "ip_version": IPVersion.IPV4,
+                "protocol": Protocol.TCP,
+                "remote_ip_cidr": "130.246.0.0/16",
+                "port_range": ("53", "53"),
+            },
+            {
+                "direction": NetworkDirection.INGRESS,
+                "ip_version": IPVersion.IPV4,
+                "protocol": Protocol.TCP,
+                "remote_ip_cidr": "130.246.0.0/16",
+                "port_range": ("53", "53"),
+            },
+            {
+                "direction": NetworkDirection.EGRESS,
+                "ip_version": IPVersion.IPV4,
+                "protocol": Protocol.TCP,
+                "remote_ip_cidr": "130.246.180.101/32",
+                "port_range": ("80", "80"),
+            },
+            {
+                "direction": NetworkDirection.EGRESS,
+                "ip_version": IPVersion.IPV4,
+                "protocol": Protocol.TCP,
+                "remote_ip_cidr": "130.246.180.163/32",
+                "port_range": ("80", "80"),
+            },
+            {
+                "direction": NetworkDirection.INGRESS,
+                "ip_version": IPVersion.IPV4,
+                "protocol": Protocol.TCP,
+                "remote_ip_cidr": "130.246.180.13/32",
+                "port_range": ("80", "80"),
+            },
+            {
+                "direction": NetworkDirection.EGRESS,
+                "ip_version": IPVersion.IPV4,
+                "protocol": Protocol.TCP,
+                "remote_ip_cidr": "130.246.180.13/32",
+                "port_range": ("80", "80"),
+            },
+            {
+                "direction": NetworkDirection.INGRESS,
+                "ip_version": IPVersion.IPV4,
+                "protocol": Protocol.TCP,
+                "remote_ip_cidr": "130.246.223.126/32",
+                "port_range": ("80", "9999"),
+            },
+            {
+                "direction": NetworkDirection.INGRESS,
+                "ip_version": IPVersion.IPV4,
+                "protocol": Protocol.TCP,
+                "remote_ip_cidr": "130.246.180.101/32",
+                "port_range": ("80", "80"),
+            },
+            {
+                "direction": NetworkDirection.INGRESS,
+                "ip_version": IPVersion.IPV4,
+                "protocol": Protocol.TCP,
+                "remote_ip_cidr": "130.246.186.163/32",
+                "port_range": ("80", "80"),
+            },
+            {
+                "direction": NetworkDirection.EGRESS,
+                "ip_version": IPVersion.IPV4,
+                "protocol": Protocol.TCP,
+                "remote_ip_cidr": "130.246.180.101/32",
+                "port_range": ("443", "443"),
+            },
+            {
+                "direction": NetworkDirection.EGRESS,
+                "ip_version": IPVersion.IPV4,
+                "protocol": Protocol.TCP,
+                "remote_ip_cidr": "130.246.223.126/32",
+                "port_range": ("9999", "9999"),
+            },
+            {
+                "direction": NetworkDirection.EGRESS,
+                "ip_version": IPVersion.IPV4,
+                "protocol": Protocol.UDP,
+                "remote_ip_cidr": "130.246.0.0/16",
+                "port_range": ("53", "53"),
+            },
+            {
+                "direction": NetworkDirection.INGRESS,
+                "ip_version": IPVersion.IPV4,
+                "protocol": Protocol.UDP,
+                "remote_ip_cidr": "130.246.0.0/16",
+                "port_range": ("53", "53"),
+            },
+            {
+                "direction": NetworkDirection.EGRESS,
+                "ip_version": IPVersion.IPV4,
+                "protocol": Protocol.TCP,
+                "remote_ip_cidr": "130.246.176.0/22",
+                "port_range": ("443", "443"),
+            },
+        ]
+
+        # tcp/udp egress external cidr
+        tcp_udp_egress_external_cidr = [
+            "8.0.0.0/7",
+            "192.170.0.0/15",
+            "172.64.0.0/10",
+            "192.160.0.0/13",
+            "192.169.0.0/16",
+            "192.176.0.0/12",
+            "192.128.0.0/11",
+            "173.0.0.0/8",
+            "172.0.0.0/12",
+            "130.248.0.0/12",
+            "193.0.0.0/8",
+            "130.247.0.0/16",
+            "32.0.0.0/3",
+            "131.0.0.0/8",
+            "196.0.0.0/6",
+            "176.0.0.0/4",
+            "128.0.0.0/7",
+            "174.0.0.0/7",
+            "144.0.0.0/4",
+            "172.128.0.0/9",
+            "192.172.0.0/14",
+            "192.192.0.0/10",
+            "208.0.0.0/4",
+            "194.0.0.0/7",
+            "168.0.0.0/6",
+            "132.0.0.0/6",
+            "192.0.0.0/9",
+            "160.0.0.0/5",
+            "172.32.0.0/11",
+            "12.0.0.0/6",
+            "16.0.0.0/4",
+            "130.128.0.0/10",
+            "130.224.0.0/12",
+            "130.0.0.0/9",
+            "64.0.0.0/2",
+            "130.244.0.0/15",
+            "200.0.0.0/5",
+            "130.240.0.0/14",
+            "11.0.0.0/8",
+            "130.192.0.0/11",
+            "136.0.0.0/5",
+            "0.0.0.0/5",
+        ]
+
+        results = []
+        for rule in default_external_rules:
+            results.append(
+                self.create_security_group_rule(
+                    cloud_account,
+                    SecurityGroupRuleDetails(
+                        security_group_identifier=security_group_identifier,
+                        project_identifier=project_identifier,
+                        **rule,
+                    ),
+                )
+            )
+        for cidr in tcp_udp_egress_external_cidr:
+            results.append(
+                self.create_security_group_rule(
+                    cloud_account,
+                    SecurityGroupRuleDetails(
+                        security_group_identifier=security_group_identifier,
+                        project_identifier=project_identifier,
+                        direction=NetworkDirection.EGRESS,
+                        ip_version=IPVersion.IPV4,
+                        protocol=Protocol.TCP,
+                        remote_ip_cidr=cidr,
+                        port_range=("1", "65535"),
+                    ),
+                )
+            )
+            results.append(
+                self.create_security_group_rule(
+                    cloud_account,
+                    SecurityGroupRuleDetails(
+                        security_group_identifier=security_group_identifier,
+                        project_identifier=project_identifier,
+                        direction=NetworkDirection.EGRESS,
+                        ip_version=IPVersion.IPV4,
+                        protocol=Protocol.UDP,
+                        remote_ip_cidr=cidr,
+                        port_range=("1", "65535"),
+                    ),
+                )
+            )
+        return results
 
     def find_security_group(
         self,
