e3a7b91c76cb7fab9451807f66bebf85eeba62b9
anish-mudaraddi <anish.mudaraddi@stfc.ac.uk>
2024-05-09 17:16:24 +0100

    MAINT: clean up floating ip searching actions
    theses aren't necessary - if they are we'll rewrite using query library

--- actions/src/floating_ip_actions.py
+++ actions/src/floating_ip_actions.py
@@ -3,10 +3,7 @@ from typing import Tuple, Union, Dict, List, Callable
 from openstack.network.v2.floating_ip import FloatingIP
 
 from openstack_action import OpenstackAction
-from openstack_api.dataclasses import QueryParams
-from openstack_api.openstack_floating_ip import OpenstackFloatingIP
 from openstack_api.openstack_network import OpenstackNetwork
-from openstack_api.openstack_query import OpenstackQuery
 
 
 class FloatingIPActions(OpenstackAction):
@@ -16,12 +13,6 @@ class FloatingIPActions(OpenstackAction):
         self._network_api: OpenstackNetwork = config.get(
             "openstack_network_api", OpenstackNetwork()
         )
-        self._fip_api: OpenstackFloatingIP = config.get(
-            "openstack_floating_ip_api", OpenstackFloatingIP()
-        )
-        self._query_api: OpenstackQuery = config.get(
-            "openstack_query_api", OpenstackQuery()
-        )
 
     def run(self, submodule: str, **kwargs):
         """
@@ -29,19 +20,6 @@ class FloatingIPActions(OpenstackAction):
         """
         func: Callable = getattr(self, submodule)
         return func(**kwargs)
-
-    def floating_ip_get(
-        self, cloud_account: str, ip_addr: str
-    ) -> Tuple[bool, Union[FloatingIP, str]]:
-        """
-        Show floating ip information
-        :param cloud_account: The account from the clouds configuration to use
-        :param ip_addr: IP address to lookup
-        :return: status, result if found else error message
-        """
-        found = self._network_api.get_floating_ip(cloud_account, ip_addr)
-        to_return = found if found else "The requested floating IP could not be found"
-        return bool(found), to_return
 
     def floating_ip_delete(self, ip_addr):
         """
@@ -73,57 +51,3 @@ class FloatingIPActions(OpenstackAction):
             number_to_create=number_to_create,
         )
         return bool(created), created
-
-    # pylint:disable=too-many-arguments
-    def floating_ip_list(
-        self,
-        cloud_account: str,
-        query_preset: str,
-        properties_to_select: List[str],
-        group_by: str,
-        return_html: bool,
-        **kwargs,
-    ) -> str:
-        """
-        Finds all floating ips belonging to a project (or all floating ips if project is empty)
-        :param cloud_account: The account from the clouds configuration to use
-        :param query_preset: The query to use when searching for floating ips
-        :param properties_to_select: The list of properties to select and output from the found floating ips
-        :param group_by: Property to group returned results - can be empty for no grouping
-        :param return_html: When True tables returned are in html format
-        :return: (String or Dictionary of strings) Table(s) of results grouped by the 'group_by' parameter
-        """
-
-        return self._fip_api.search(
-            cloud_account=cloud_account,
-            query_params=QueryParams(
-                query_preset=query_preset,
-                properties_to_select=properties_to_select,
-                group_by=group_by,
-                return_html=return_html,
-            ),
-            **kwargs,
-        )
-
-    def find_non_existent_floating_ips(
-        self, cloud_account: str, project_identifier: str
-    ):
-        """
-        Returns a dictionary containing the ids of projects along with a list of non-existent floating ips found
-        within them
-        :param cloud_account: The associated clouds.yaml account
-        :param project_identifier: The project to get all associated servers with, can be empty for all projects
-        :return: A dictionary containing the non-existent server ids and their projects
-        """
-        return self._fip_api.find_non_existent_fips(
-            cloud_account=cloud_account, project_identifier=project_identifier
-        )
-
-    def find_non_existent_projects(self, cloud_account: str):
-        """
-        Returns a dictionary containing the ids of non-existent projects along with a list of floating ips that
-        refer to them
-        :param cloud_account: The associated clouds.yaml account
-        :return: A dictionary containing the non-existent server ids and their projects
-        """
-        return self._fip_api.find_non_existent_projects(cloud_account=cloud_account)
