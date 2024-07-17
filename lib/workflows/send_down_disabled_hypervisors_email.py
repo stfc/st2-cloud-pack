bd58f0b16d256294720a29c28134373f3e09630a
Akhil Maganti <akhil.maganti@stfc.ac.uk>
2024-07-17 14:49:36 +0100

    Added HV down and disabled action and function

--- lib/workflows/send_down_disabled_hypervisors_email.py
+++ lib/workflows/send_down_disabled_hypervisors_email.py
@@ -1,5 +1,7 @@
 from typing import List, Optional, Union
 
+import openstack
+
 from enums.query.props import ServerProperties
 from enums.query.query_presets import QueryPresetsGeneric
 from enums.query.props.hypervisor_properties import HypervisorProperties
@@ -8,24 +10,58 @@ from openstack_query import HypervisorQuery, ServerQuery
 
 
 def find_down_hypervisors(
-        cloud_account: str, from_projects: Optional[List[str]] = None
+        cloud_account: str
 ):
     """
     :param cloud_account: string represents cloud account to use
-    :param from_projects: A list of project identifiers to limit search in
     """
 
-    hypervisor_query = HypervisorQuery()
-    hypervisor_query.where(
-        QueryPresetsGeneric.ANY_IN, HypervisorProperties.HYPERVISOR_STATE, values="DOWN"
+    hypervisor_query_down = HypervisorQuery()
+    hypervisor_query_down.where(
+        QueryPresetsGeneric.ANY_IN, HypervisorProperties.HYPERVISOR_STATE, values=["down"]
     )
-    hypervisor_query.run(
+    hypervisor_query_down.run(
         cloud_account,
-        from_projects=from_projects if from_projects else None,
+    )
+    hypervisor_query_down.select(
+        HypervisorProperties.HYPERVISOR_ID,
+        HypervisorProperties.HYPERVISOR_NAME,
+        HypervisorProperties.HYPERVISOR_STATE,
+        HypervisorProperties.HYPERVISOR_STATUS,
     )
 
-    print(hypervisor_query.to_string())
+    return hypervisor_query_down
+
+
+def find_disabled_hypervisors(
+        cloud_account: str
+):
+    """
+    :param cloud_account: string represents cloud account to use
+    """
+
+    hypervisor_query_disabled = HypervisorQuery()
+    hypervisor_query_disabled.where(
+        QueryPresetsGeneric.ANY_IN, HypervisorProperties.HYPERVISOR_STATUS, values=["disabled"]
+    )
+    hypervisor_query_disabled.run(
+        cloud_account,
+    )
+    hypervisor_query_disabled.select(
+        HypervisorProperties.HYPERVISOR_ID,
+        HypervisorProperties.HYPERVISOR_NAME,
+        HypervisorProperties.HYPERVISOR_STATE,
+        HypervisorProperties.HYPERVISOR_STATUS,
+    )
+
+    return hypervisor_query_disabled
 
 
 if __name__ == "__main__":
-    find_down_hypervisors("prod")
+    find_down_hypervisors("dev")
+    find_disabled_hypervisors("dev")
+    """
+    conn = openstack.connect("dev")
+    for hv in conn.compute.hypervisors(details=True):
+        print(hv)
+"""
