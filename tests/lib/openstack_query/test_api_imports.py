3ebe9478eca09e8d6d71d20f94a6d4f6154a1fad
Akhil Maganti <akhil.maganti@stfc.ac.uk>
2024-08-08 09:24:36 +0100

    Fixed linting issues

--- tests/lib/openstack_query/test_api_imports.py
+++ tests/lib/openstack_query/test_api_imports.py
@@ -5,7 +5,14 @@ import openstack_query
 
 @pytest.mark.parametrize(
     "test_module_name",
-    ["ServerQuery", "UserQuery", "FlavorQuery", "ProjectQuery", "ImageQuery", "HypervisorQuery"],
+    [
+        "ServerQuery",
+        "UserQuery",
+        "FlavorQuery",
+        "ProjectQuery",
+        "ImageQuery",
+        "HypervisorQuery",
+    ],
 )
 def test_query_server_import(test_module_name):
     """Tests that query object imports can be done at root level"""
