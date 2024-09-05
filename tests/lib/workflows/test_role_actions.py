7c1b8c3a989d8ff99287ba5112036b8995495e5f
anish-mudaraddi <anish.mudaraddi@stfc.ac.uk>
2024-09-05 14:21:08 +0100

    WIP: moving useful actions into workflow folder
    This is so we can remove all the unnecessary code under actions.src.*. We're only keeping useful actions here - other ones can be easily done using openstack cli so we shouldn't re-invent the wheel here

---
+++ tests/lib/workflows/test_role_actions.py
@@ -0,0 +1,75 @@
+from unittest.mock import patch, MagicMock
+
+from enums.user_domains import UserDomains
+from structs.role_details import RoleDetails
+from workflows.role_actions import role_add, role_remove
+
+
+@patch("workflows.role_actions.openstack_roles.assign_role_to_user")
+def test_role_add(mock_api_assign_role_to_user):
+    """
+    Tests that role_add function forwards on request to openstack API role_add function
+    and returns the correct status (True)
+    """
+
+    # setup input values
+    mock_conn = MagicMock()
+    mock_user_identifier = "user-id"
+    mock_project_identifier = "project-id"
+    mock_role = "user"
+    mock_user_domain = "stfc"
+
+    status = role_add(
+        mock_conn,
+        mock_user_identifier,
+        mock_project_identifier,
+        mock_role,
+        mock_user_domain,
+    )
+
+    mock_api_assign_role_to_user.assert_called_once_with(
+        mock_conn,
+        details=RoleDetails(
+            user_identifier=mock_user_identifier,
+            project_identifier=mock_project_identifier,
+            role_identifier=mock_role,
+            user_domain=UserDomains.STFC,
+        ),
+    )
+
+    assert status
+
+
+@patch("workflows.role_actions.openstack_roles.remove_role_from_user")
+def test_role_remove(mock_api_remove_role_from_user):
+    """
+    Tests that role_add function forwards on request to openstack API role_add function
+    and returns the correct status (True)
+    """
+
+    # setup input values
+    mock_conn = MagicMock()
+    mock_user_identifier = "user-id"
+    mock_project_identifier = "project-id"
+    mock_role = "user"
+    mock_user_domain = "stfc"
+
+    status = role_remove(
+        mock_conn,
+        mock_user_identifier,
+        mock_project_identifier,
+        mock_role,
+        mock_user_domain,
+    )
+
+    mock_api_remove_role_from_user.assert_called_once_with(
+        mock_conn,
+        details=RoleDetails(
+            user_identifier=mock_user_identifier,
+            project_identifier=mock_project_identifier,
+            role_identifier=mock_role,
+            user_domain=UserDomains.STFC,
+        ),
+    )
+
+    assert status
