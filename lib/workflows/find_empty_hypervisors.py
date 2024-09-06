0f4fb04a04c30c3c2c2911a52a0993ba2a107ef0
David Fairbrother <DavidFair@users.noreply.github.com>
2024-09-06 12:01:17 +0100

    Use query library to pre-sort output
    Use the query library to sort the output for the list of hypervisors,
    rather than doing it in the lib layer

--- lib/workflows/find_empty_hypervisors.py
+++ lib/workflows/find_empty_hypervisors.py
@@ -1,12 +1,10 @@
 from enums.query.props.hypervisor_properties import HypervisorProperties
 from enums.query.query_presets import QueryPresetsGeneric
+from enums.query.sort_order import SortOrder
 from openstack_query import HypervisorQuery
 
 
 def find_empty_hypervisors(cloud_account: str):
-    """
-    Runs a search for hypervisors based on the search type
-    """
     """
     Finds all hypervisors that have no instances
     cloud_account: str: the cloud account to run the query on
@@ -19,5 +17,9 @@ def find_empty_hypervisors(cloud_account: str):
         value=0,
     )
     hv_query.select(HypervisorProperties.HYPERVISOR_NAME)
-    results = hv_query.run(cloud_account=cloud_account).to_props(flatten=True)
-    return sorted(results["hypervisor_name"])
+    results = (
+        hv_query.run(cloud_account=cloud_account)
+        .sort_by([HypervisorProperties.HYPERVISOR_NAME, SortOrder.ASC])
+        .to_props(flatten=True)
+    )
+    return results["hypervisor_name"]
--- tests/lib/workflows/test_find_empty_hypervisors.py
+++ tests/lib/workflows/test_find_empty_hypervisors.py
@@ -2,6 +2,7 @@ from unittest.mock import patch
 
 from enums.query.props.hypervisor_properties import HypervisorProperties
 from enums.query.query_presets import QueryPresetsGeneric
+from enums.query.sort_order import SortOrder
 from workflows.find_empty_hypervisors import find_empty_hypervisors
 
 
@@ -13,7 +14,9 @@ def test_with_empty_hvs(mocked_query_factory):
     """
     query_obj = mocked_query_factory.return_value
     fake_query_output = {"hypervisor_name": ["hypervisor3", "hypervisor1"]}
-    query_obj.run.return_value.to_props.return_value = fake_query_output
+    query_obj.run.return_value.sort_by.return_value.to_props.return_value = (
+        fake_query_output
+    )
 
     result = find_empty_hypervisors(cloud_account="test")
     mocked_query_factory.assert_called_once_with()
@@ -26,6 +29,10 @@ def test_with_empty_hvs(mocked_query_factory):
     )
     query_obj.run.assert_called_once_with(cloud_account="test")
     query_results = query_obj.run.return_value
-    query_results.to_props.assert_called_once_with(flatten=True)
+    query_results.sort_by.assert_called_once_with(
+        [HypervisorProperties.HYPERVISOR_NAME, SortOrder.ASC]
+    )
+    sort_by_chain = query_results.sort_by.return_value
+    sort_by_chain.to_props.assert_called_once_with(flatten=True)
 
-    assert result == sorted(query_results.to_props.return_value["hypervisor_name"])
+    assert result == fake_query_output["hypervisor_name"]
