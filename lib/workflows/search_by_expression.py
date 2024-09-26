f84659df1b70c7fb5a10240bc0927814eae5e184
anish-mudaraddi <anish.mudaraddi@stfc.ac.uk>
2024-09-26 12:31:30 +0100

    ENH: add search_by_expression workflow
    this workflow action allows querying generic openstack resources for numerical properties based on an expression - e.g "less-than" or "greater-than" a given threshold
    
    Future stackstorm actions will make use of it

---
+++ lib/workflows/search_by_expression.py
@@ -0,0 +1,64 @@
+from typing import List, Optional
+from enums.query.query_presets import QueryPresetsInteger
+from enums.query.sort_order import SortOrder
+import openstack_query
+
+# pylint:disable=too-many-arguments
+
+
+def search_by_expression(
+    cloud_account: str,
+    query_type: str,
+    search_mode: str,
+    property_to_search_by: str,
+    value: int,
+    properties_to_select: Optional[List[str]] = None,
+    output_type: Optional[str] = None,
+    group_by: Optional[str] = None,
+    sort_by: Optional[List[str]] = None,
+    **kwargs
+):
+    """
+    Method that builds and runs a query to find generic numerical resource based on a expression
+    (less-than, greater-than etc)
+    :param cloud_account: A string representing the cloud account to use - set in clouds.ymal
+    :param query_type: what Query object to use to run query with ServerQuery, UserQuery etc
+    :param search_mode: A string representing a preset Enum
+        LESS_THAN,
+        LESS_THAN_OR_EQUAL_TO,
+        GREATE_THAN,
+        GREATER_THAN_OR_EQUAL_TO
+    which dictates what query to perform
+    :param property_to_search_by: A string representing a datetime property Enum that the preset will be used on
+    :param value: A number to compare property against
+    :param properties_to_select: list of strings representing which properties to select
+    :param output_type: string representing how to output the query
+    :param group_by: an optional string representing a property to group results by
+    :param sort_by: an optional set of tuples representing way which properties to sort results by
+    :param kwargs: A set of optional meta params to pass to the query
+    """
+
+    query = getattr(openstack_query, query_type)()
+    if not properties_to_select:
+        query.select_all()
+    else:
+        query.select(*properties_to_select)
+
+    query.where(
+        preset=QueryPresetsInteger.from_string(search_mode),
+        prop=property_to_search_by,
+        value=value,
+    )
+
+    if sort_by:
+        query.sort_by(*[(p, SortOrder.DESC) for p in sort_by])
+    if group_by:
+        query.group_by(group_by)
+
+    query.run(cloud_account, **kwargs)
+    return {
+        "to_html": query.to_html(),
+        "to_string": query.to_string(),
+        "to_objects": query.to_objects(),
+        "to_props": query.to_props(),
+    }[output_type]
