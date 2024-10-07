c066aa2fa1200068dab3d08820834fedbd19eaa4
George Matthews <george.matthews@stfc.ac.uk>
2024-10-07 09:56:10 +0100

    Add Search Icinga Actions
    Add actions to search by hosts/services state or name
    Add search tests

---
+++ lib/workflows/icinga_search.py
@@ -0,0 +1,173 @@
+import json
+from functools import partial
+from typing import Callable, Dict, List, Optional
+
+from enums.icinga.icinga_states import HostState, ServiceState
+from icinga_api.query_objects import IcingaQuery, query_object
+from structs.icinga.icinga_account import IcingaAccount
+from tabulate import tabulate
+
+
+def create_join_filter_functions(user_joins: List[str]) -> List[Callable[[Dict], str]]:
+    """
+    Parses list of user defined joins and returns a list of filter
+    functions to return respective value for each join.
+    :param user_joins: List of user defined joins
+    """
+
+    def get_value(a: Dict, filter1: str, filter2: str, states=None) -> str:
+        value = a["joins"][filter1][filter2]
+        return states(value).name if states else value
+
+    filter_functions = []
+    for join in user_joins:
+        filter1, filter2 = join.split(".")
+        states = (
+            (ServiceState if filter1 == "service" else HostState)
+            if filter2 == "state"
+            else None
+        )
+        filter_functions.append(
+            partial(get_value, filter1=filter1, filter2=filter2, states=states)
+        )
+
+    return filter_functions
+
+
+def create_property_filter_functions(
+    object_type: str, propeties: List[str]
+) -> List[Callable[[Dict], str]]:
+    """
+    Parses list of user defined propeties and returns a list of filter
+    functions to return respective value for each join.
+    :param object_type: Type of object in Icinga
+    :param propeties: List of user defined propeties
+    """
+
+    def get_value(a: Dict, filter1: str, states=None) -> str:
+        value = a["attrs"][filter1]
+        return states(value).name if states else value
+
+    filter_functions = []
+    for prop in propeties:
+        states = (
+            (ServiceState if object_type == "Service" else HostState)
+            if prop == "state"
+            else None
+        )
+        filter_functions.append(partial(get_value, filter1=prop, states=states))
+
+    return filter_functions
+
+
+def generate_table(
+    data: Dict, object_type: str, properties_to_select: List[str], user_joins: List[str]
+) -> str:
+    """
+    Generate a table of properties from the query results
+    :param data: results of the query in a dict
+    :param object_type: Type of iginga object
+    :param properties_to_select: User defined properties returned by the query
+    :param user_joins: User defined query joins
+    :return: tabulate table string
+    """
+    results = []
+    prop_filter_functions = create_property_filter_functions(
+        object_type, properties_to_select
+    )
+    join_filter_functions = (
+        create_join_filter_functions(user_joins) if user_joins is not None else []
+    )
+
+    for host in data["results"]:
+        props = [fn(host) for fn in prop_filter_functions]
+        joins = [fn(host) for fn in join_filter_functions]
+
+        props += joins
+        results.append(props)
+
+    headers = []
+    for prop in properties_to_select:
+        headers.append((f"{object_type} {prop}").title())
+    for join in user_joins if user_joins is not None else []:
+        join = join.replace(".", " ")
+        headers.append(join.title())
+
+    return tabulate(tabular_data=results, headers=headers, tablefmt="grid")
+
+
+def search_by_state(
+    icinga_account: IcingaAccount,
+    object_type: str,
+    state: str,
+    output_type: str,
+    properties_to_select: List[str],
+    joins: Optional[List[str]] = None,
+):
+    """
+    Query iginga for objects in a given state
+    :param icinga_account: Account used to interact with icinga
+    :param object_type: Type of iginga object
+    :param state: Host/Service state to filter by
+    :param output_type: how to output the query
+    :param properties_to_select: User defined properties returned by the query
+    :param user_joins: User defined query joins
+    """
+
+    state = (
+        HostState[state.upper()]
+        if object_type == "Host"
+        else ServiceState[state.upper()]
+    )
+
+    query = IcingaQuery(
+        object_type=object_type,
+        filter=f"host.vars.team==team && {object_type.lower()}.state==state",
+        filter_vars={"team": "cloud", "state": state.value},
+        properties_to_select=properties_to_select,
+        joins=joins,
+    )
+
+    _, data = query_object(icinga_account, query)
+
+    data = json.loads(data)
+
+    return {
+        "dict": data,
+        "table": generate_table(data, object_type, properties_to_select, joins),
+    }[output_type]
+
+
+def search_by_name(
+    icinga_account: IcingaAccount,
+    object_type: str,
+    name: str,
+    output_type: str,
+    properties_to_select: List[str],
+    joins: Optional[List[str]] = None,
+):
+    """
+    Query iginga for objects in a given state
+    :param icinga_account: Account used to interact with icinga
+    :param object_type: Type of iginga object
+    :param name: name of iginga object, can be regex pattern
+    :param output_type: how to output the query
+    :param properties_to_select: User defined properties returned by the query
+    :param user_joins: User defined query joins
+    """
+    query = IcingaQuery(
+        object_type=object_type,
+        filter=f"host.vars.team==team && match(pattern, {object_type.lower()}.name)",
+        filter_vars={"team": "cloud", "pattern": name},
+        properties_to_select=properties_to_select,
+        joins=joins,
+    )
+
+    _, data = query_object(icinga_account, query)
+
+    data = json.loads(data)
+
+    return {
+        "dict": data,
+        "table": generate_table(data, object_type, properties_to_select, joins),
+    }[output_type]
---
+++ tests/lib/workflows/test_incinga_search.py
@@ -0,0 +1,175 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+
+from workflows.icinga_search import (
+    create_join_filter_functions,
+    create_property_filter_functions,
+    generate_table,
+    search_by_name,
+    search_by_state,
+)
+
+
+@pytest.fixture(name="mock_data")
+def mock_data_fixture():
+    return {
+        "results": [
+            {
+                "attrs": {"handled": False, "name": "ping6", "state": 3},
+                "joins": {"host": {"name": "Host1", "state": 1}},
+                "meta": {},
+                "name": "icinga2!ping6",
+                "type": "Service",
+            },
+            {
+                "attrs": {"handled": False, "name": "disk /", "state": 1},
+                "joins": {"host": {"name": "Host2", "state": 0}},
+                "meta": {},
+                "name": "icinga2!disk /",
+                "type": "Service",
+            },
+        ]
+    }
+
+
+Expected = {
+    "tabular_data": [["ping6", "UNKOWN"], ["disk /", "WARNING"]],
+    "headers": ["Service Name", "Service State"],
+}
+
+Expected_with_joins = {
+    "tabular_data": [
+        ["ping6", "UNKOWN", "Host1", "DOWN"],
+        ["disk /", "WARNING", "Host2", "UP"],
+    ],
+    "headers": ["Service Name", "Service State", "Host Name", "Host State"],
+}
+
+
+@patch("workflows.icinga_search.tabulate")
+@patch("workflows.icinga_search.json.loads")
+@patch("workflows.icinga_search.query_object")
+@pytest.mark.parametrize(
+    "joins, expected",
+    [
+        ([], Expected),
+        (["host.name", "host.state"], Expected_with_joins),
+    ],
+)
+def test_search_by_state_success(
+    mock_query_object,
+    mock_json_loads,
+    mock_tabulate,
+    mock_data,
+    joins,
+    expected,
+):
+    icinga_account = MagicMock()
+
+    mock_query_object.return_value = [200, MagicMock()]
+    mock_json_loads.return_value = mock_data
+
+    search_by_state(
+        icinga_account,
+        object_type="Service",
+        state="Critical",
+        properties_to_select=["name", "state"],
+        output_type="table",
+        joins=joins,
+    )
+
+    mock_query_object.assert_called_once()
+
+    mock_tabulate.assert_called_once_with(
+        tabular_data=expected["tabular_data"],
+        headers=expected["headers"],
+        tablefmt="grid",
+    )
+
+
+@patch("workflows.icinga_search.tabulate")
+@patch("workflows.icinga_search.json.loads")
+@patch("workflows.icinga_search.query_object")
+@pytest.mark.parametrize(
+    "joins, expected",
+    [
+        ([], Expected),
+        (["host.name", "host.state"], Expected_with_joins),
+    ],
+)
+def test_search_by_name_success(
+    mock_query_object,
+    mock_json_loads,
+    mock_tabulate,
+    mock_data,
+    joins,
+    expected,
+):
+    icinga_account = MagicMock()
+
+    mock_query_object.return_value = [200, MagicMock()]
+    mock_json_loads.return_value = mock_data
+
+    search_by_name(
+        icinga_account,
+        object_type="Service",
+        name="ping*",
+        properties_to_select=["name", "state"],
+        output_type="table",
+        joins=joins,
+    )
+
+    mock_query_object.assert_called_once()
+
+    mock_tabulate.assert_called_once_with(
+        tabular_data=expected["tabular_data"],
+        headers=expected["headers"],
+        tablefmt="grid",
+    )
+
+
+def test_create_join_filter_functions():
+    mock_user_joins = ["host.name", "host.state"]
+    mock_data = {"joins": {"host": {"name": "Host2", "state": 0}}}
+
+    filter_functions = create_join_filter_functions(mock_user_joins)
+
+    expected = ["Host2", "UP"]
+
+    for idx, fn in enumerate(filter_functions):
+        assert fn(mock_data) == expected[idx]
+
+
+def test_create_properties_filter_functions():
+    mock_user_properties = ["name", "state"]
+    mock_data = {"attrs": {"handled": False, "name": "ping6", "state": 3}}
+
+    filter_functions = create_property_filter_functions("Service", mock_user_properties)
+
+    expected = ["ping6", "UNKOWN"]
+
+    for idx, fn in enumerate(filter_functions):
+        assert fn(mock_data) == expected[idx]
+
+
+@patch("workflows.icinga_search.tabulate")
+@patch("workflows.icinga_search.create_join_filter_functions")
+@patch("workflows.icinga_search.create_property_filter_functions")
+def test_generate_table(
+    mock_create_property_filter_functions,
+    mock_create_joins_filter_functions,
+    mock_tabulate,
+    mock_data,
+):
+    mock_user_properties = ["name", "state"]
+    mock_user_joins = ["host.name", "host.state"]
+
+    generate_table(mock_data, "Service", mock_user_properties, mock_user_joins)
+
+    mock_create_property_filter_functions.assert_called_once_with(
+        "Service", mock_user_properties
+    )
+    mock_create_joins_filter_functions.assert_called_once_with(mock_user_joins)
+
+    mock_tabulate.assert_called_once()
