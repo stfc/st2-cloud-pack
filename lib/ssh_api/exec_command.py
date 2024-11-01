77e1e938049d29ec702a21ed607f692643a98940
George Matthews <george.matthews@stfc.ac.uk>
2024-11-01 11:33:47 +0000

    SSH action docstrings
    Add docstrings and return types to ssh functions

--- lib/ssh_api/exec_command.py
+++ lib/ssh_api/exec_command.py
@@ -10,12 +10,18 @@ class SSHConnection:
     """
 
     def __init__(self, conn: SSHDetails):
+        """
+        Create SSH client and load private key
+        """
         self.host = conn.host
         self.username = conn.username
         self.private_key = paramiko.RSAKey.from_private_key_file(conn.private_key_path)
         self.client = paramiko.SSHClient()
 
     def __enter__(self):
+        """
+        Connect host via SSH
+        """
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
         self.client.connect(self.host, username=self.username, pkey=self.private_key)
@@ -23,9 +29,17 @@ class SSHConnection:
         return self.client
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        Close ssh client
+        """
         self.client.close()
 
-    def run_command_on_host(self, command: str):
+    def run_command_on_host(self, command: str) -> bytes:
+        """
+        Run command on a host
+        :param command: Command to run over SSH
+        :return: Output bytes
+        """
         with self as client:
             _stdin, stdout, stderr = client.exec_command(command=command)
 
--- lib/workflows/ssh_remote_command.py
+++ lib/workflows/ssh_remote_command.py
@@ -2,7 +2,17 @@ from ssh_api.exec_command import SSHConnection
 from structs.ssh.ssh_connection_details import SSHDetails
 
 
-def ssh_remote_command(host: str, username: str, private_key_path: str, command: str):
+def ssh_remote_command(
+    host: str, username: str, private_key_path: str, command: str
+) -> str:
+    """
+    Run command on host over SSH
+    :param host: Host to run command on
+    :param username: Username to authenticate with
+    :param private_key_path: Path to private key to authenticate with
+    :param command: Command to run on host
+    :return: Command output
+    """
     connection_details = SSHDetails(
         host=host, username=username, private_key_path=private_key_path
     )
