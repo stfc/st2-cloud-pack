7939ec5ef2e356d5b557aa77427d19f4f213667a
Lizzie Salmon <lizzie.salmon@stfc.ac.uk>
2024-11-08 11:59:01 +0000

    Added in first test - successful

--- lib/workflows/hv_patch_and_reboot.py
+++ lib/workflows/hv_patch_and_reboot.py
@@ -1,6 +1,10 @@
-from workflows.icinga_downtime import schedule_downtime
+from exceptions.missing_mandatory_param_error import MissingMandatoryParamError
+from workflows.icinga_downtime import schedule_downtime, remove_downtime
+from workflows.ssh_remote_command import ssh_remote_command
 
 from structs.icinga.icinga_account import IcingaAccount
+
+from paramiko.ssh_exception import SSHException
 
 
 def patch_and_reboot(
@@ -8,14 +12,43 @@ def patch_and_reboot(
     name: str,
     start_time: str,
     end_time: str,
+    host: str,
+    private_key_path: str,
 ):
     comment = f"starting downtime to patch and reboot host: {name}"
-    schedule_downtime(
-        icinga_account=icinga_account,
-        object_type="Host",
-        name=name,
-        start_time=start_time,
-        end_time=end_time,
-        is_fixed=True,
-        comment=comment,
-    )
+    downtime_scheduled = False
+    try:
+        schedule_downtime(
+            icinga_account=icinga_account,
+            object_type="Host",
+            name=name,
+            start_time=start_time,
+            end_time=end_time,
+            is_fixed=True,
+            comment=comment,
+        )
+        downtime_scheduled = True
+        patch_out = ssh_remote_command(
+            host=host,
+            username="stackstorm",
+            private_key_path=private_key_path,
+            command=f"{host} patch",
+        )
+        reboot_out = ssh_remote_command(
+            host=host,
+            username="stackstorm",
+            private_key_path=private_key_path,
+            command=f"{host} reboot",
+        )
+    except SSHException as exc:
+        raise SSHException("SSH command failed during patch or reboot") from exc
+    except MissingMandatoryParamError as exc:
+        raise MissingMandatoryParamError("A required parameter is missing") from exc
+    finally:
+        if downtime_scheduled:
+            remove_downtime(
+                icinga_account=icinga_account,
+                object_type="Host",
+                name=name,
+            )
+    return {"patch_output": patch_out, "reboot_output": reboot_out}
--- tests/lib/workflows/test_hv_patch_and_reboot.py
+++ tests/lib/workflows/test_hv_patch_and_reboot.py
@@ -1,30 +1,62 @@
 from unittest.mock import MagicMock, patch
-import pytest
 from workflows.hv_patch_and_reboot import patch_and_reboot
 
 
 @patch("workflows.hv_patch_and_reboot.schedule_downtime")
-@pytest.mark.parametrize(
-    "start_time, end_time",
-    [
-        ("01/12/24 12:00:00", "01/12/24 17:00:00"),
-    ],
-)
-def test_hv_patch_and_reboot(mock_schedule_downtime, start_time, end_time):
+@patch("workflows.hv_patch_and_reboot.ssh_remote_command")
+@patch("workflows.hv_patch_and_reboot.remove_downtime")
+def test_successful_patch_and_reboot(
+    mock_remove_downtime, mock_ssh_remote_command, mock_schedule_downtime
+):
     """
-    Test the patch and reboot action on a hypervisor
+    Test successful running of patch and reboot workflow
     """
     icinga_account = MagicMock()
+    # patch for first run and reboot for second call of the mock function
+    mock_ssh_remote_command.side_effect = [
+        "Patch command output",
+        "Reboot command output",
+    ]
     name = "example_host"
     comment = f"starting downtime to patch and reboot host: {name}"
+    mock_host = "test_host"
+    mock_private_key_path = "/home/stackstorm/.ssh/id_rsa"
 
-    patch_and_reboot(icinga_account, name, start_time, end_time)
+    result = patch_and_reboot(
+        icinga_account,
+        "example_host",
+        "01/12/24 12:00:00",
+        "01/12/24 17:00:00",
+        host=mock_host,
+        private_key_path=mock_private_key_path,
+    )
+    # test that schedule downtime is called once with expected parameters
     mock_schedule_downtime.assert_called_once_with(
         icinga_account=icinga_account,
         object_type="Host",
         name=name,
-        start_time=start_time,
-        end_time=end_time,
+        start_time="01/12/24 12:00:00",
+        end_time="01/12/24 17:00:00",
         is_fixed=True,
         comment=comment,
+    )
+    # test that the two ssh remote commands are run and that their results are as expected
+    mock_ssh_remote_command.assert_any_call(
+        host=mock_host,
+        username="stackstorm",
+        private_key_path=mock_private_key_path,
+        command=f"{mock_host} patch",
+    )
+    mock_ssh_remote_command.assert_any_call(
+        host=mock_host,
+        username="stackstorm",
+        private_key_path=mock_private_key_path,
+        command=f"{mock_host} reboot",
+    )
+    assert result["patch_output"] == "Patch command output"
+    assert result["reboot_output"] == "Reboot command output"
+
+    # test that the remove downtime is called
+    mock_remove_downtime.assert_called_once_with(
+        icinga_account=icinga_account, object_type="Host", name=name
     )
