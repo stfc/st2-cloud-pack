561706e35f5f64b05427b33d2fdb54ebf1353cae
Lizzie Salmon <lizzie.salmon@stfc.ac.uk>
2024-11-14 12:29:09 +0000

    Responding to comments.  Remove name.

--- lib/workflows/hv_patch_and_reboot.py
+++ lib/workflows/hv_patch_and_reboot.py
@@ -9,36 +9,42 @@ from ssh_api.exec_command import SSHConnection
 
 def patch_and_reboot(
     icinga_account: IcingaAccount,
-    name: str,
-    host: str,
+    hypervisor_name: str,
     private_key_path: str,
-):
+) -> dict:
+    """
+    Takes the selected hypervisor, schedules a downtime on it starting immediately then runs
+    the patch and reboot scripts on the machine, before ending the downtime.
+    icinga_account: IcingaAccount: The icinga account object to use to schedule and remove the downtimes
+    hypervisor_name: the name of the hypervisor - should also be the host name on icinga
+    return: Dict[str] a dictionary containing the remote commands output
+    """
     connection_details = SSHDetails(
-        host=host, username="stackstorm", private_key_path=private_key_path
+        host=hypervisor_name, username="stackstorm", private_key_path=private_key_path
     )
     ssh_client = SSHConnection(connection_details)
-    start_time = datetime.datetime.now()
+    start_time = datetime.datetime.utcnow()
     end_time = start_time + datetime.timedelta(hours=6)
     start_timestamp = int(start_time.timestamp())
     end_timestamp = int(end_time.timestamp())
     downtime_details = DowntimeDetails(
         object_type="Host",
-        object_name=name,
+        object_name=hypervisor_name,
         start_time=start_timestamp,
         end_time=end_timestamp,
-        comment=f"starting downtime to patch and reboot host: {name}",
+        comment=f"starting downtime to patch and reboot host: {hypervisor_name}",
         is_fixed=True,
         duration=end_timestamp - start_timestamp,
     )
     schedule_downtime(icinga_account=icinga_account, details=downtime_details)
     try:
-        patch_out = ssh_client.run_command_on_host("patch").decode()
-        reboot_out = ssh_client.run_command_on_host("reboot").decode()
+        patch_out = ssh_client.run_command_on_host("patch")
+        reboot_out = ssh_client.run_command_on_host("reboot")
 
     finally:
         remove_downtime(
             icinga_account=icinga_account,
             object_type="Host",
-            object_name=name,
+            object_name=hypervisor_name,
         )
-    return {"patch_output": patch_out, "reboot_output": reboot_out}
+    return {"patch_output": patch_out.decode(), "reboot_output": reboot_out.decode()}
--- tests/lib/workflows/test_hv_patch_and_reboot.py
+++ tests/lib/workflows/test_hv_patch_and_reboot.py
@@ -20,25 +20,23 @@ def test_successful_patch_and_reboot(
         str.encode("Patch command output"),
         str.encode("Reboot command output"),
     ]
-    name = "example_host"
-    comment = f"starting downtime to patch and reboot host: {name}"
-    mock_host = "test_host"
+    mock_hypervisor_name = "test_host"
+    comment = f"starting downtime to patch and reboot host: {mock_hypervisor_name}"
     mock_private_key_path = "/home/stackstorm/.ssh/id_rsa"
-    mock_start_time = datetime.datetime.now()
+    mock_start_time = datetime.datetime.utcnow()
     mock_end_time = mock_start_time + datetime.timedelta(hours=6)
     mock_start_timestamp = int(mock_start_time.timestamp())
     mock_end_timestamp = int(mock_end_time.timestamp())
     result = patch_and_reboot(
         icinga_account,
-        name="example_host",
-        host=mock_host,
+        hypervisor_name=mock_hypervisor_name,
         private_key_path=mock_private_key_path,
     )
     mock_schedule_downtime.assert_called_once_with(
         icinga_account=icinga_account,
         details=DowntimeDetails(
             object_type="Host",
-            object_name=name,
+            object_name=mock_hypervisor_name,
             start_time=mock_start_timestamp,
             end_time=mock_end_timestamp,
             comment=comment,
@@ -48,7 +46,7 @@ def test_successful_patch_and_reboot(
     )
     mock_ssh_conn.assert_called_once_with(
         SSHDetails(
-            host=mock_host,
+            host=mock_hypervisor_name,
             username="stackstorm",
             private_key_path=mock_private_key_path,
         )
@@ -59,37 +57,39 @@ def test_successful_patch_and_reboot(
     assert result["reboot_output"] == "Reboot command output"
 
     mock_remove_downtime.assert_called_once_with(
-        icinga_account=icinga_account, object_type="Host", object_name=name
+        icinga_account=icinga_account,
+        object_type="Host",
+        object_name=mock_hypervisor_name,
     )
 
 
 @patch("workflows.hv_patch_and_reboot.schedule_downtime")
 @patch("workflows.hv_patch_and_reboot.SSHConnection")
 @patch("workflows.hv_patch_and_reboot.remove_downtime")
-def test_failed_schedule(mock_remove_downtime, mock_ssh_conn, mock_schedule_downtime):
+def test_failed_schedule_downtime(
+    mock_remove_downtime, mock_ssh_conn, mock_schedule_downtime
+):
     """
     Test unsuccessful running of patch and reboot workflow - where the schedule
     downtime raises an exception.
     """
     icinga_account = MagicMock()
-    mock_host = "test_host"
     mock_private_key_path = "/home/stackstorm/.ssh/id_rsa"
-
+    mock_hypervisor_name = "test_host"
     mock_schedule_downtime.side_effect = Exception
 
     # test that schedule downtime is called once with expected parameters
     with pytest.raises(Exception):
         patch_and_reboot(
             icinga_account,
-            name="example_host",
-            host=mock_host,
+            hypervisor_name=mock_hypervisor_name,
             private_key_path=mock_private_key_path,
         )
 
     # check that the ssh connection is still made
     mock_ssh_conn.assert_called_once_with(
         SSHDetails(
-            host=mock_host,
+            host=mock_hypervisor_name,
             username="stackstorm",
             private_key_path=mock_private_key_path,
         )
@@ -108,18 +108,42 @@ def test_failed_ssh(mock_remove_downtime, mock_ssh_conn, mock_schedule_downtime)
     fails
     """
     icinga_account = MagicMock()
-    mock_host = "test_host"
+    mock_hypervisor_name = "test_host"
     mock_private_key_path = "/home/stackstorm/.ssh/id_rsa"
     mock_ssh_conn.return_value.run_command_on_host.side_effect = Exception
 
     with pytest.raises(Exception):
         patch_and_reboot(
             icinga_account,
-            "example_host",
-            host=mock_host,
+            hypervisor_name=mock_hypervisor_name,
             private_key_path=mock_private_key_path,
         )
 
-    mock_ssh_conn.assert_called_once()
-    mock_schedule_downtime.assert_called_once()
-    mock_remove_downtime.assert_called_once()
+    mock_ssh_conn.assert_called_once_with(
+        SSHDetails(
+            host=mock_hypervisor_name,
+            username="stackstorm",
+            private_key_path=mock_private_key_path,
+        )
+    )
+    mock_start_time = datetime.datetime.utcnow()
+    mock_end_time = mock_start_time + datetime.timedelta(hours=6)
+    mock_start_timestamp = int(mock_start_time.timestamp())
+    mock_end_timestamp = int(mock_end_time.timestamp())
+    mock_schedule_downtime.assert_called_once_with(
+        icinga_account=icinga_account,
+        details=DowntimeDetails(
+            object_type="Host",
+            object_name=mock_hypervisor_name,
+            start_time=mock_start_timestamp,
+            end_time=mock_end_timestamp,
+            comment=f"starting downtime to patch and reboot host: {mock_hypervisor_name}",
+            is_fixed=True,
+            duration=mock_end_timestamp - mock_start_timestamp,
+        ),
+    )
+    mock_remove_downtime.assert_called_once_with(
+        icinga_account=icinga_account,
+        object_type="Host",
+        object_name=mock_hypervisor_name,
+    )
