995fb44cf7b971718c948754ed21c41a7eddfbb6
Dmitry Popovichev <dmitry.popovichev@stfc.ac.uk>
2024-12-02 17:30:22 +0000

    refactor: Refactored the code and renamed the workflow
    Cleaned up the type hints, print statements and renamed the workflow to follow a more generic name for reusibility. Renamed all of the references accordingly.

--- lib/openstack_api/openstack_service.py
+++ lib/openstack_api/openstack_service.py
@@ -1,22 +1,7 @@
 from typing import Dict
 from openstack.connection import Connection
 from openstack.compute.v2.service import Service
-from openstack.compute.v2._proxy import Proxy
-
-'''
-Find hypervisor, using user input // Hypervisor not found
-Check to see if Nova-Compute service exists using the service ID
-If it does, check if it's already enbabled/disabled
-Enable/disable the service
-Let user know that's the action is complete (print output)
-
-
-Side effects:
-Authenticate the connection (already done)
-Api call to endpoint to find service (hypervisor not found, service not found)
-Authentication failed to hypervisor and/or service (handled by )
-Networking issues, talking to openstack api, too slow, no response (ahndled by openstack api)
-'''    
+from openstack.compute.v2._proxy import Proxy 
 
 
 def disable_service(
@@ -29,16 +14,14 @@ def disable_service(
     """
     Disables an Openstack service
     :param conn: Openstack connection
-    :param service_identifier: The service ID or instance.
+    :param service: The instance of the service class.
     :param hypervisor_name (str): The name or ID of the hypervisor.
     :param service_binary: The name of the service.
-    :param disable_reason: The reason for disabling the service.
+    :param disabled_reason: The reason for disabling the service.
     """
 
-    print(service)
     if service.status == "enabled":
         conn.compute.disable_service(service, host=hypervisor_name, binary=service_binary, disabled_reason=disabled_reason)
-        print("Disabled the status")
         print(f"The {service_binary} service has been disabled.")
     else:
         print("Hypervisor is currently disabled - aborting.")
@@ -54,7 +37,7 @@ def enable_service(
     """
     Enables an Openstack service
     :param conn: Openstack connection
-    :param service_identifier: The service ID or instance.
+    :param service: The instance of the service class.
     :param hypervisor_name (str): The name or ID of the hypervisor.
     :param service_binary: The name of the service.
     """
--- lib/workflows/hv_service_actions.py
+++ lib/workflows/hv_service_actions.py
@@ -4,7 +4,7 @@ from openstack.connection import Connection
 from openstack_api.openstack_service import disable_service, enable_service
 
 
-def hv_compute_service_disable(
+def hv_service_disable(
     conn: Connection, 
     hypervisor_name: str,
     service_binary:str,
@@ -13,20 +13,18 @@ def hv_compute_service_disable(
     """
     Disables an Openstack service
     :param conn: Openstack connection
-    :param service_identifier: The service ID or instance.
     :param hypervisor_name (str): The name or ID of the hypervisor.
     :param service_binary: The name of the service.
     :param disable_reason: The reason for disabling the service.
     """
 
     service = conn.compute.find_service(service_binary, ignore_missing=False, host=hypervisor_name)
-    print(service)
 
     disable_service(conn, service, hypervisor_name, service_binary, disabled_reason)
 
-    
 
-def hv_compute_service_enable(
+
+def hv_service_enable(
     conn: Connection, 
     hypervisor_name: str,
     service_binary:str,
@@ -34,7 +32,6 @@ def hv_compute_service_enable(
     """
     Enables an Openstack service
     :param conn: Openstack connection
-    :param service_identifier: The service ID or instance.
     :param hypervisor_name (str): The name or ID of the hypervisor.
     :param service_binary: The name of the service.
     """
