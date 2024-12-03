2572b7617de2baa6b7709e19c40b603eacdd1e10
George Matthews <george.matthews@stfc.ac.uk>
2024-12-03 14:03:13 +0000

    Update tests
    Update unit tests for icinga to test setting all_services

--- tests/lib/icinga_api/test_downtime.py
+++ tests/lib/icinga_api/test_downtime.py
@@ -4,6 +4,7 @@ from unittest.mock import MagicMock, patch
 import pytest
 from requests import HTTPError
 
+from enums.icinga.icinga_objects import IcingaObject
 from exceptions.missing_mandatory_param_error import MissingMandatoryParamError
 from icinga_api.downtime import remove_downtime, schedule_downtime
 from structs.icinga.downtime_details import DowntimeDetails
@@ -11,10 +12,10 @@ from structs.icinga.downtime_details import DowntimeDetails
 
 class TestScheduleDowntime:
     @patch("icinga_api.downtime.requests.post")
-    def test_schedule_fixed_downtime_success(self, mock_post):
+    def test_schedule_host_fixed_downtime_success(self, mock_post):
         icinga_account = MagicMock()
         details = DowntimeDetails(
-            object_type="Host",
+            object_type=IcingaObject.HOST,
             object_name="example_host",
             start_time=1725955200,
             end_time=1725958800,
@@ -43,6 +44,45 @@ class TestScheduleDowntime:
             "end_time": 1725958800,
             "fixed": True,
             "duration": None,
+            "all_services": True,
+        }
+
+        assert payload == expected_payload
+
+    @patch("icinga_api.downtime.requests.post")
+    def test_schedule_service_fixed_downtime_success(self, mock_post):
+        icinga_account = MagicMock()
+        details = DowntimeDetails(
+            object_type=IcingaObject.SERVICE,
+            object_name="example_service",
+            start_time=1725955200,
+            end_time=1725958800,
+            comment="Scheduled maintenance",
+            is_fixed=True,
+            duration=3600,
+        )
+        mock_response = MagicMock()
+        mock_response.status_code = 201
+        mock_post.return_value = mock_response
+
+        schedule_downtime(icinga_account, details)
+
+        mock_post.assert_called_once()
+        mock_response.raise_for_status.assert_called_once()
+
+        _, kwargs = mock_post.call_args
+        payload = json.loads(kwargs["data"])
+
+        expected_payload = {
+            "type": "Service",
+            "filter": 'service.name=="example_service"',
+            "author": "StackStorm",
+            "comment": "Scheduled maintenance",
+            "start_time": 1725955200,
+            "end_time": 1725958800,
+            "fixed": True,
+            "duration": None,
+            "all_services": False,
         }
 
         assert payload == expected_payload
@@ -51,7 +91,7 @@ class TestScheduleDowntime:
     def test_schedule_downtime_request_fail(self, mock_post):
         icinga_account = MagicMock()
         details = DowntimeDetails(
-            object_type="Host",
+            object_type=IcingaObject.HOST,
             object_name="missing_host",
             start_time=1725955200,
             end_time=1725958800,
@@ -74,7 +114,7 @@ class TestScheduleDowntime:
     def test_schedule_flexible_downtime_success(self, mock_post):
         icinga_account = MagicMock()
         details = DowntimeDetails(
-            object_type="Host",
+            object_type=IcingaObject.HOST,
             object_name="example_host",
             start_time=1725955200,
             end_time=1725958800,
@@ -103,6 +143,7 @@ class TestScheduleDowntime:
             "end_time": 1725958800,
             "fixed": False,
             "duration": 3600,
+            "all_services": True,
         }
 
         assert payload == expected_payload
@@ -110,7 +151,7 @@ class TestScheduleDowntime:
     def test_missing_schedule_downtime_object_name(self):
         icinga_account = MagicMock()
         details = DowntimeDetails(
-            object_type="Host",
+            object_type=IcingaObject.HOST,
             object_name="",
             start_time=1725955200,
             end_time=1725958800,
@@ -125,7 +166,7 @@ class TestScheduleDowntime:
     def test_missing_schedule_downtime_start_time(self):
         icinga_account = MagicMock()
         details = DowntimeDetails(
-            object_type="Host",
+            object_type=IcingaObject.HOST,
             object_name="example_host",
             start_time=None,
             end_time=1725958800,
@@ -140,7 +181,7 @@ class TestScheduleDowntime:
     def test_missing_schedule_downtime_end_time(self):
         icinga_account = MagicMock()
         details = DowntimeDetails(
-            object_type="Host",
+            object_type=IcingaObject.HOST,
             object_name="example_host",
             start_time=1725958800,
             end_time=None,
@@ -155,7 +196,7 @@ class TestScheduleDowntime:
     def test_missing_schedule_downtime_comment(self):
         icinga_account = MagicMock()
         details = DowntimeDetails(
-            object_type="Host",
+            object_type=IcingaObject.HOST,
             object_name="example_host",
             start_time=1725955200,
             end_time=1725958800,
@@ -179,7 +220,7 @@ class TestRemoveDowntime:
 
         remove_downtime(
             icinga_account,
-            object_type="Host",
+            object_type=IcingaObject.HOST,
             object_name="example_host",
         )
 
@@ -209,7 +250,7 @@ class TestRemoveDowntime:
         with pytest.raises(HTTPError):
             remove_downtime(
                 icinga_account,
-                object_type="Host",
+                object_type=IcingaObject.HOST,
                 object_name="missing_host",
             )
 
@@ -221,6 +262,6 @@ class TestRemoveDowntime:
         with pytest.raises(MissingMandatoryParamError):
             remove_downtime(
                 icinga_account,
-                object_type="Host",
+                object_type=IcingaObject.HOST,
                 object_name="",
             )
