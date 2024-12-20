b415560f5267d72aa7a35868458b2e204074929f
Lizzie Salmon <lizzie.salmon@stfc.ac.uk>
2024-12-20 12:31:32 +0000

    Added in tests for openstack_server.py

--- tests/lib/openstack_api/test_openstack_server.py
+++ tests/lib/openstack_api/test_openstack_server.py
@@ -1,115 +1,103 @@
 from datetime import datetime
-import unittest
-from unittest.mock import MagicMock
-from openstack_api.openstack_server import migrate_server, snapshot_server
+from unittest.mock import MagicMock, patch
+import pytest
+from openstack_api.openstack_server import snapshot_and_migrate_server, snapshot_server
 
 
-class TestLiveMigration(unittest.TestCase):
+@pytest.mark.parametrize("dest_host", [None, "hv01"])
+@patch("openstack_api.openstack_server.snapshot_server")
+def test_active_migration(mock_snapshot_server, dest_host):
     """
-    Test Live migrations
+    Test migration when the status of the server is ACTIVE and the destination host is supplied
     """
+    mock_connection = MagicMock()
+    mock_server_id = "server1"
+    mock_server_status = "ACTIVE"
+    snapshot_and_migrate_server(
+        conn=mock_connection,
+        server_id=mock_server_id,
+        server_status=mock_server_status,
+        dest_host=dest_host,
+    )
+    mock_snapshot_server.assert_called_once_with(
+        conn=mock_connection, server_id=mock_server_id
+    )
+    mock_connection.compute.live_migrate_server.assert_called_once_with(
+        server=mock_server_id, host=dest_host, block_migration=True
+    )
+    mock_connection.compute.migrate_server.assert_not_called()
 
-    def test_live_migrate_server_without_dest_host(self):
-        """
-        Test live migration of server, without defined destination host
-        """
-        mock_connection = MagicMock()
-        mock_server_id = "server1"
-        migrate_server(conn=mock_connection, server_id=mock_server_id, live=True)
 
-        mock_connection.compute.live_migrate_server.assert_called_once_with(
-            server=mock_server_id, host=None, block_migration=True
-        )
-        mock_connection.compute.migrate_server.assert_not_called()
+@pytest.mark.parametrize("dest_host", [None, "hv01"])
+@patch("openstack_api.openstack_server.snapshot_server")
+def test_shutoff_migration(mock_snapshot_server, dest_host):
+    """
+    Test migration when the status of the server is SHUTOFF and the destination host is supplied
+    """
+    mock_connection = MagicMock()
+    mock_server_id = "server1"
+    mock_server_status = "SHUTOFF"
+    snapshot_and_migrate_server(
+        conn=mock_connection,
+        server_id=mock_server_id,
+        server_status=mock_server_status,
+        dest_host=dest_host,
+    )
+    mock_snapshot_server.assert_called_once_with(
+        conn=mock_connection, server_id=mock_server_id
+    )
+    mock_connection.compute.live_migrate_server.assert_not_called()
+    mock_connection.compute.migrate_server.assert_called_once_with(
+        server=mock_server_id, host=dest_host
+    )
 
-    def test_live_migrate_server_with_dest_host(self):
-        """
-        Test live migration of server, without defined destination host
-        """
-        mock_connection = MagicMock()
-        mock_server_id = "server1"
-        mock_dest_host = "hv01"
-        migrate_server(
+
+@patch("openstack_api.openstack_server.snapshot_server")
+def test_migration_fail(mock_snapshot_server):
+    mock_connection = MagicMock()
+    mock_server_id = "server1"
+    mock_server_status = "TEST"
+    with pytest.raises(
+        ValueError,
+        match="Server status: TEST. The server must be ACTIVE or SHUTOFF to be migrated",
+    ):
+        snapshot_and_migrate_server(
             conn=mock_connection,
             server_id=mock_server_id,
-            dest_host=mock_dest_host,
-            live=True,
+            server_status=mock_server_status,
         )
+    mock_snapshot_server.assert_called_once_with(
+        conn=mock_connection, server_id=mock_server_id
+    )
+    mock_connection.compute.live_migrate_server.assert_not_called()
+    mock_connection.compute.migrate_server.assert_not_called()
 
-        mock_connection.compute.live_migrate_server.assert_called_once_with(
-            server=mock_server_id, host=mock_dest_host, block_migration=True
-        )
-        mock_connection.compute.migrate_server.assert_not_called()
 
-
-class TestColdMigration(unittest.TestCase):
+def test_snapshot_server():
     """
-    Test cold migrations
+    Test server snapshot
     """
+    mock_connection = MagicMock()
+    mock_server_id = "server1"
+    current_time = datetime.now().strftime("%d-%m-%Y-%H%M")
 
-    def test_migrate_server_without_dest_host(self):
-        """
-        Test migration of server, without defined destination host
-        """
-        mock_connection = MagicMock()
-        mock_server_id = "server1"
-        migrate_server(conn=mock_connection, server_id=mock_server_id, live=False)
+    mock_project_id = "project1"
+    mock_connection.compute.find_server.return_value.project_id = mock_project_id
 
-        mock_connection.compute.migrate_server.assert_called_once_with(
-            server=mock_server_id, host=None
-        )
-        mock_connection.compute.live_migrate_server.assert_not_called()
+    mock_image = MagicMock()
+    mock_connection.compute.create_server_image.return_value = mock_image
 
-    def test_migrate_server_with_dest_host(self):
-        """
-        Test live migration of server, without defined destination host
-        """
-        mock_connection = MagicMock()
-        mock_server_id = "server1"
-        mock_dest_host = "hv01"
-        migrate_server(
-            conn=mock_connection,
-            server_id=mock_server_id,
-            dest_host=mock_dest_host,
-            live=False,
-        )
+    snapshot_server(conn=mock_connection, server_id=mock_server_id)
 
-        mock_connection.compute.migrate_server.assert_called_once_with(
-            server=mock_server_id, host=mock_dest_host
-        )
-        mock_connection.compute.live_migrate_server.assert_not_called()
-
-
-class TestServerSnapshot(unittest.TestCase):
-    """
-    Test cases for server snapshots
-    """
-
-    def test_snapshot_server(self):
-        """
-        Test server snapshot
-        """
-        mock_connection = MagicMock()
-        mock_server_id = "server1"
-        current_time = datetime.now().strftime("%d-%m-%Y-%H%M")
-
-        mock_project_id = "project1"
-        mock_connection.compute.find_server.return_value.project_id = mock_project_id
-
-        mock_image = MagicMock()
-        mock_connection.compute.create_server_image.return_value = mock_image
-
-        snapshot_server(conn=mock_connection, server_id=mock_server_id)
-
-        mock_connection.compute.find_server.assert_called_once_with(
-            mock_server_id, all_projects=True
-        )
-        mock_connection.compute.create_server_image.assert_called_once_with(
-            server=mock_server_id,
-            name=f"{mock_server_id}-{current_time}",
-            wait=True,
-            timeout=300,
-        )
-        mock_connection.image.update_image.assert_called_once_with(
-            mock_image, owner=mock_project_id
-        )
+    mock_connection.compute.find_server.assert_called_once_with(
+        mock_server_id, all_projects=True
+    )
+    mock_connection.compute.create_server_image.assert_called_once_with(
+        server=mock_server_id,
+        name=f"{mock_server_id}-{current_time}",
+        wait=True,
+        timeout=300,
+    )
+    mock_connection.image.update_image.assert_called_once_with(
+        mock_image, owner=mock_project_id
+    )
