540e4905e581f0bf3baa2f4026851d664fe73668
George Matthews <george.matthews@stfc.ac.uk>
2025-01-13 17:04:51 +0000

    Refactor hypervisor states
    Refactor logic for determining the state of a hypervisor
    Add more states for hypervisors

--- lib/apis/openstack_api/enums/hypervisor_states.py
+++ lib/apis/openstack_api/enums/hypervisor_states.py
@@ -6,40 +6,14 @@ class HypervisorState(Enum):
     Class of states used for automating maintenace of hypervisors
     """
 
-    RUNNING = {
-        "uptime": True,
-        "enabled": True,
-        "state": True,
-        "servers": True,
-    }
-    DRAINING = {
-        "uptime": False,
-        "enabled": False,
-        "state": True,
-        "servers": True,
-    }
-    DRAINED = {
-        "uptime": False,
-        "enabled": False,
-        "state": True,
-        "servers": False,
-    }
-    REBOOTED = {
-        "uptime": True,
-        "enabled": False,
-        "state": True,
-        "servers": False,
-    }
-    EMPTY = {
-        "uptime": True,
-        "enabled": True,
-        "state": True,
-        "servers": False,
-    }
+    RUNNING = auto()
     PENDING_MAINTENANCE = auto()
-    DOWN = auto()
-    DISABLED = auto()
+    DRAINING = auto()
+    DRAINED = auto()
     UNKNOWN = auto()
+    REBOOTED = auto()
+    EMPTY = auto()
+    DOWN = auto()
 
     @classmethod
     def _missing_(cls, value):
