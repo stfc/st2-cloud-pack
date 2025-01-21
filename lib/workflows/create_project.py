e7da7592e3b93a3466f61c73e0a26f6e75d10190
George Matthews <george.matthews@stfc.ac.uk>
2025-01-21 13:11:46 +0000

    Create generalised project creation action
    Create new action which can create both internal and external projects and can also take an domain in which to create the project
    Placeholder action which new project creation action calls

---
+++ lib/workflows/create_project.py
@@ -0,0 +1,50 @@
+from typing import List, Optional
+from openstack.connection import Connection
+
+import workflows
+import workflows.create_external_project
+import workflows.create_internal_project
+
+
+def create_project(
+    conn: Connection,
+    project_name: str,
+    project_email: str,
+    project_description: str,
+    project_domain: str,
+    networking_type: str,
+    number_of_floating_ips: int = 1,
+    number_of_security_group_rules: int = 200,
+    project_immutable: Optional[bool] = None,
+    parent_id: Optional[str] = None,
+    admin_user_list: Optional[List[str]] = None,
+    user_list: Optional[List[str]] = None,
+):
+    return (
+        workflows.create_external_project.create_external_project(
+            conn=conn,
+            project_name=f"{project_name}-{project_domain}",
+            project_email=project_email,
+            project_description=project_description,
+            network_name=f"{project_name}-network",
+            subnet_name=f"{project_name}-subnet",
+            router_name=f"{project_name}-router",
+            number_of_floating_ips=number_of_floating_ips,
+            number_of_security_group_rules=number_of_security_group_rules,
+            project_immutable=project_immutable,
+            parent_id=parent_id,
+            admin_user_list=admin_user_list,
+            stfc_user_list=user_list,
+        )
+        if networking_type == "external"
+        else workflows.create_internal_project.create_internal_project(
+            conn=conn,
+            project_name=f"{project_name}_{project_domain}",
+            project_email=project_email,
+            project_description=project_description,
+            project_immutable=project_immutable,
+            parent_id=parent_id,
+            admin_user_list=admin_user_list,
+            stfc_user_list=user_list,
+        )
+    )
