b4e7b3f567b29caa2ca48367d78785d7773796c0
Lizzie Salmon <lizzie.salmon@stfc.ac.uk>
2025-01-27 16:08:41 +0000

    Added in enable in openstack.

--- lib/workflows/hv_post_reboot.py
+++ lib/workflows/hv_post_reboot.py
@@ -1,14 +1,14 @@
-from enums.icinga.icinga_objects import IcingaObject
-from structs.alertmanager.alertmanager_account import AlertManagerAccount
-
-from structs.icinga.icinga_account import IcingaAccount
+from openstack.connection import Connection
 from alertmanager_api.silence import AlertManagerAPI
 
-# from alertmanager_api.silence_details import SilenceDetails
+from enums.icinga.icinga_objects import IcingaObject
+from structs.alertmanager.alertmanager_account import AlertManagerAccount
+from structs.icinga.icinga_account import IcingaAccount
+from hv_service_actions import hv_service_enable
 from icinga_api import downtime
 
 
-def post_reboot(icinga_account: IcingaAccount, name: str):
+def post_reboot(icinga_account: IcingaAccount, name: str, conn=Connection):
     """
     Action to run after a successful reboot
     :param icinga_account: Icinga account to use
@@ -20,18 +20,15 @@ def post_reboot(icinga_account: IcingaAccount, name: str):
         object_type=IcingaObject.HOST,
         object_name=name,
     )
-    # get silence to be removed
+
+    # get silence to be removed and remove the alertmanager silence
     alertmanager_account = AlertManagerAccount(
         username="stackstorm", password="password", alertmanager_endpoint="endpoint"
     )
     alert_manager = AlertManagerAPI(alertmanager_account)
-    silence = alert_manager.get_silences()
-    try:
-        alert_manager.remove_silence(silence)
-        print(f"Silence with ID {silence.silence_id} has been removed successfully.")
-    except Exception as e:
-        print(f"Failed to remove silence: {e}")
-
-    # Remove alertmanager silence
+    silences = alert_manager.get_silences().get_by_name(name)
+    alert_manager.remove_silences(silences)
     # test vm creation
+
     # enable in openstack
+    hv_service_enable(conn=conn, hypervisor_name=name, service_binary="nova-compute")
