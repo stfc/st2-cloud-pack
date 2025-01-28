63b98ac88c496e152d49ae4cb11207f2b80efdcc
Lizzie Salmon <lizzie.salmon@stfc.ac.uk>
2025-01-28 17:19:27 +0000

    Added in unittest for successful post_reboot action.

--- lib/workflows/hv_post_reboot.py
+++ lib/workflows/hv_post_reboot.py
@@ -1,10 +1,10 @@
 from openstack.connection import Connection
 
-# from alertmanager_api.silence import AlertManagerAPI
+from alertmanager_api.silence import AlertManagerAPI
 
 from enums.icinga.icinga_objects import IcingaObject
 
-# from structs.alertmanager.alertmanager_account import AlertManagerAccount
+from structs.alertmanager.alertmanager_account import AlertManagerAccount
 from structs.icinga.icinga_account import IcingaAccount
 from workflows.hv_service_actions import hv_service_enable
 from workflows.hv_create_test_server import create_test_server
@@ -23,14 +23,13 @@ def post_reboot(icinga_account: IcingaAccount, name: str, conn=Connection):
         object_type=IcingaObject.HOST,
         object_name=name,
     )
-
-    # get silence to be removed and remove the alertmanager silence
-    # alertmanager_account = AlertManagerAccount(
-    #     username="stackstorm", password="password", alertmanager_endpoint="endpoint"
-    # )
-    # alert_manager = AlertManagerAPI(alertmanager_account)
-    # silences = alert_manager.get_silences().get_by_name(name)
-    # alert_manager.remove_silences(silences)
+    # get silence to be removed and remove all the alertmanager silences on that host.
+    alertmanager_account = AlertManagerAccount(
+        username="stackstorm", password="password", alertmanager_endpoint="endpoint"
+    )
+    alert_manager = AlertManagerAPI(alertmanager_account)
+    silences = alert_manager.get_silences().get_by_name(name)
+    alert_manager.remove_silences(silences)
     # test vm creation
     create_test_server(conn=conn, hypervisor_name=name, test_all_flavors=False)
     # enable in openstack
