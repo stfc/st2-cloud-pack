71993d04bbef8b3e1e172984a6c735ed697a2ea9
anish-mudaraddi <anish.mudaraddi@stfc.ac.uk>
2025-02-06 10:49:45 +0000

    Added workflow actions for post reboot

--- lib/workflows/hv_patch_and_reboot.py
+++ lib/workflows/hv_patch_and_reboot.py
@@ -1,15 +1,21 @@
 import datetime
 
+from alertmanager_api.silence import AlertManagerAPI
+from alertmanager_api.silence_details import SilenceDetails
+
 from paramiko import SSHException
 from enums.icinga.icinga_objects import IcingaObject
 from icinga_api.downtime import schedule_downtime, remove_downtime
+from structs.alertmanager.alertmanager_account import AlertManagerAccount
 from structs.icinga.downtime_details import DowntimeDetails
 from structs.icinga.icinga_account import IcingaAccount
 from structs.ssh.ssh_connection_details import SSHDetails
 from ssh_api.exec_command import SSHConnection
 
 
+# pylint:disable=too-many-locals
 def patch_and_reboot(
+    alertmanager_account: AlertManagerAccount,
     icinga_account: IcingaAccount,
     hypervisor_name: str,
     private_key_path: str,
@@ -39,6 +45,16 @@ def patch_and_reboot(
         duration=end_timestamp - start_timestamp,
     )
     schedule_downtime(icinga_account=icinga_account, details=downtime_details)
+    alertmanager = AlertManagerAPI(alertmanager_account)
+    silence_details = SilenceDetails(
+        instance_name=hypervisor_name,
+        start_time_dt=start_time,
+        end_time_dt=end_time,
+        author="stackstorm",
+        comment="Stackstorm HV maintenance",
+    )
+    scheduled_silence = alertmanager.schedule_silence(silence_details)
+
     try:
         patch_out = ssh_client.run_command_on_host("patch")
         reboot_out = ssh_client.run_command_on_host("reboot")
@@ -52,4 +68,5 @@ def patch_and_reboot(
             object_type=IcingaObject.HOST,
             object_name=hypervisor_name,
         )
+        alertmanager.remove_silence(scheduled_silence)
         raise exc
---
+++ lib/workflows/hv_post_reboot.py
@@ -0,0 +1,38 @@
+from openstack.connection import Connection
+
+from alertmanager_api.silence import AlertManagerAPI
+
+from enums.icinga.icinga_objects import IcingaObject
+
+from structs.alertmanager.alertmanager_account import AlertManagerAccount
+from structs.icinga.icinga_account import IcingaAccount
+from workflows.hv_service_actions import hv_service_enable
+from workflows.hv_create_test_server import create_test_server
+from icinga_api import downtime
+
+
+def post_reboot(
+    alertmanager_account: AlertManagerAccount,
+    icinga_account: IcingaAccount,
+    name: str,
+    conn=Connection,
+):
+    """
+    Action to run after a successful reboot
+    :param icinga_account: Icinga account to use
+    :param name: Name of host in icinga
+    """
+    # Remove downtime
+    downtime.remove_downtime(
+        icinga_account=icinga_account,
+        object_type=IcingaObject.HOST,
+        object_name=name,
+    )
+    # get silence to be removed and remove all the alertmanager silences on that host.
+    alertmanager = AlertManagerAPI(alertmanager_account)
+    silences = alertmanager.get_silences().get_by_name(name)
+    alertmanager.remove_silences(silences)
+    # test vm creation
+    create_test_server(conn=conn, hypervisor_name=name, test_all_flavors=False)
+    # enable in openstack
+    hv_service_enable(conn=conn, hypervisor_name=name, service_binary="nova-compute")
