85719de56cd36f52cf79c7064bbe0a25b09723f2
anish-mudaraddi <anish.mudaraddi@stfc.ac.uk>
2025-02-10 15:37:43 +0000

    update run_tests.sh
    Latest commit to main use pytest so the monkey-patch isn't necessary
    
    Removes the need to run another "run tests" task.
    
    Now coverage will work for "actions" and "sensors" tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 set -e
 
-REPO_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+# set repo directory as st2-cloud-pack location
+REPO_DIR=$(cd -- "$(dirname -- "$0")" && pwd)
 
 export ST2_REPO_PATH="/tmp/st2"
 if [ ! -d "$ST2_REPO_PATH" ]; then
-    git clone --depth=1 https://github.com/stackstorm/st2.git --branch v3.8 "$ST2_REPO_PATH"
+    git clone --depth=1 https://github.com/stackstorm/st2.git "$ST2_REPO_PATH"
 fi
-
-# Monkey patch nose to pytest until upstream has switched
-echo "PATCH: Replace nosetests with pytest"
-sed -ri 's/nosetests \$\{NOSE_OPTS\[@\]\}/pytest/g' "$ST2_REPO_PATH/st2common/bin/st2-run-pack-tests"
 
 export PYTHONPATH="$PYTHONPATH:$REPO_DIR/lib"
 "$ST2_REPO_PATH/st2common/bin/st2-run-pack-tests" -p "$REPO_DIR" -c
