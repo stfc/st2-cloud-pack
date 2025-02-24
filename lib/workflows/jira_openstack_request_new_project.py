e674666b98cf20c2fa3b3bce37c8a1deed30bfa7
Jose Caballero Bejar <jcaballero.hep@gmail.com>
2025-02-24 14:25:14 +0000

    Write a workflow
    The automation involves more than 1 step. In the case that everything
    goes fine, 3 steps are needed:
    
    1. set the JIRA ticket as "In Progress" (or similar state)
    2. implement the change in OpenStack
    3. reply to the JIRA ticket
    4. set the JIRA ticket as "Done" (or similar state)
    
    Therefore, we cannot implement everything in a single stackstorm Action.
    We need a "workflow".

---
+++ lib/workflows/jira_openstack_request_new_project.py
@@ -0,0 +1,32 @@
+from structs.jira.jira_account import JiraAccount
+
+from jira_api.connection import JiraConnection
+from jira_api.jira_issue import change_state, add_comment
+from workflows.create_project import create_project
+from openstack.connection import Connection
+
+def request_new_project(
+        jira_account: JiraAccount,
+        project_name: str,
+        users,
+        cpus,
+        memory,
+        shared_storage,
+        object_storage,
+        gpus,
+        contact_email,
+        tos_agreement,
+    ):
+    """
+    Implement the workflow to create a new Project in OpenStack
+    1. set JIRA ticket as "In Progress"
+    2. create the new Project in OpenStack
+    3. reply to the JIRA ticket
+    """
+    try:
+        #change_state(jira_account, issue_key, "In Progress")
+        out = create_project()
+        #add_comment(jira_account, issue_key, out)
+        #change_state(jira_account, issue_key, "Done")
+    except Exception as ex:
+        raise ex
