af788a1c4801a52067021dfef0805012cb7a195e
Jose Caballero Bejar <jcaballero.hep@gmail.com>
2025-02-27 16:57:54 +0000

    Add function to change status of a JIRA Issue
    As part of potential Automations, we may need to be able to transition
    JIRA Issues to a given status. There are several reasons for that, for
    example:
    1. to let people know the Automation has transition the Issue
    2. to prevent the Automations from picking up the same Issue twice. Once
       the Issue is in a new state, the Automations should ignore it next
       time it queries JIRA for Issues to be processed.
    
    When the transition we want to perform is not allowed for some reason,
    a proper Exception should be raised.

--- lib/jira_api/jira_issue.py
+++ lib/jira_api/jira_issue.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from exceptions.missing_mandatory_param_error import MissingMandatoryParamError
+from exceptions.jira import MismatchedState, ForbiddenTransition
 from structs.jira.jira_account import JiraAccount
 from structs.jira.jira_issue_details import IssueDetails
 from jira_api.connection import JiraConnection
@@ -59,3 +60,56 @@ def add_comment(
         raise ValueError("Comment text cannot be empty")
     with JiraConnection(account) as conn:
         conn.add_comment(issue_key, text, internal)
+
+
+def change_state(
+    account: JiraAccount,
+    issue_key: str,
+    to_state: str,
+    from_state: Optional[bool] = None,
+):
+    """
+    If possible, transition a given JIRA Issue to a new state
+
+    :param account: credentials to create a connection with the JIRA server
+    :type account: JiraAccount
+    :param issue_key: the unique key to identify the Issue to transition
+    :type issue_key: str
+    :param to_state: the new state we want to transition into
+    :type to_state: str
+    :param from_state: optional, the current state the Issue is expected to be
+    :type from_state: str or None
+    :raise MismatchedState: If the argument from_state is being provided,
+        but the Issue is not currently in that state,
+        we should abort immediately and raise an Exception
+    :raise ForbiddenTransition: If the current Issue's workflow does not allow
+        a transition from its current state to the new one we want to move into,
+        we should raise an Exception
+    """
+    with JiraConnection(account) as conn:
+        issue = conn.issue(issue_key)
+
+        # 1. if needed, double check the current state is the one
+        #    passed as [optional] argument from_state
+        #    if that is not the case, raise an Exception
+        if from_state and issue.fields.status.name != from_state:
+            raise MismatchedState(issue_key, from_state)
+
+        # 2. To perform a transition, using method "transition_issue( )"
+        #    we have 2 options:
+        #    - pass the name of the transition
+        #    - pass the id of the transition
+        #    it doesn't seem to be possible to just pass the name of the
+        #    destination state.
+        #    Therefore, we first need to find the transition moving
+        #    from current state to the desired one, and use it as argument
+        #    to method conn.transition_issue.
+        #    If we are not able to find any transition performing that
+        #    desired change, we raise an Exception
+        allowed_transitions = conn.transitions(issue_key)
+        for transition in allowed_transitions:
+            if transition["to"]["name"] == to_state:
+                conn.transition_issue(issue_key, transition["id"])
+                break
+        else:
+            raise ForbiddenTransition(issue_key, to_state)
