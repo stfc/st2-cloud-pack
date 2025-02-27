6eeb3373e693189d2543a0a79c36869c7896bbe7
Nalin Gupta <nalin.gupta@stfc.ac.uk>
2025-02-27 11:41:42 +0000

    Changed id to identifier for clarity Changed image_id and project_id to image_identifier and project_identifier respectively for more clarity. Also added descriptions to the parameters in the action

--- lib/openstack_api/openstack_image.py
+++ lib/openstack_api/openstack_image.py
@@ -3,18 +3,20 @@ from openstack.connection import Connection
 
 def share_image_to_project(
     conn: Connection,
-    image_id: str,
-    project_id: str,
+    image_identifier: str,
+    project_identifier: str,
 ) -> None:
     """
     Shares given image with given project
 
     :param conn: openstack connection object
-    :param image_id: The image ID that is shared with project
-    :param project_id: Openstack ID for the project
+    :param image_identifier: The image name or ID that is shared with project
+    :param project_identifier: Project name or ID to share image to
     """
-    image = conn.image.find_image(image_id, ignore_missing=False)
-    destination_project = conn.identity.find_project(project_id, ignore_missing=False)
+    image = conn.image.find_image(image_identifier, ignore_missing=False)
+    destination_project = conn.identity.find_project(
+        project_identifier, ignore_missing=False
+    )
 
     conn.image.add_member(image["id"], member_id=destination_project["id"])
 
--- tests/lib/openstack_api/test_openstack_image.py
+++ tests/lib/openstack_api/test_openstack_image.py
@@ -7,8 +7,8 @@ def test_share_image_to_project():
     Test sharing image to project
     """
     mock_connection = MagicMock()
-    mock_image_id = "test-image-id"
-    mock_project_id = "test-project-id"
+    mock_image_identifier = "test-image-id"
+    mock_project_identifier = "test-project-id"
 
     mock_image = MagicMock()
     mock_destination_project = MagicMock()
@@ -16,7 +16,9 @@ def test_share_image_to_project():
     mock_connection.image.find_image.return_value = mock_image
     mock_connection.identity.find_project.return_value = mock_destination_project
 
-    share_image_to_project(mock_connection, mock_image_id, mock_project_id)
+    share_image_to_project(
+        mock_connection, mock_image_identifier, mock_project_identifier
+    )
 
     mock_connection.image.add_member.assert_called_once_with(
         mock_image["id"], member_id=mock_destination_project["id"]
