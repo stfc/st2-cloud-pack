017a62f3dcb140a93e34bc1b394de4095af32afe
Jose Caballero Bejar <jcaballero.hep@gmail.com>
2025-03-03 13:11:44 +0000

    Unit test the custom made Exceptions

---
+++ tests/lib/exceptions/test_jira.py
@@ -0,0 +1,26 @@
+"""
+Custom made Exceptions specific for failures in the interaction
+with the JIRA server
+"""
+
+from exceptions.jira import MismatchedState, ForbiddenTransition
+
+
+def test_mismatched_state_exception():
+    """
+    Tests that MismatchedState exception initializes and returns the expected message
+    """
+    exception = MismatchedState("ISSUE-123", "In Progress")
+
+    msg = 'The JIRA Issue "ISSUE-123" is currently in state "In Progress"'
+    assert str(exception) == repr(msg)
+
+
+def test_forbidden_transition_exception():
+    """
+    Tests that ForbiddenTransition exception initializes and returns the expected message
+    """
+    exception = ForbiddenTransition("ISSUE-456", "Closed")
+
+    msg = 'The JIRA Issue "ISSUE-456" is not in any state allowing a transition to "Closed"'
+    assert str(exception) == repr(msg)
