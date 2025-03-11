268519864c786dc4968e67c4f43d1ab56d737c5e
Jose Caballero Bejar <jcaballero.hep@gmail.com>
2025-03-11 08:23:20 +0000

    Add empty placeholder for the Action
    In this commit we just add an empty module for the automation, doing
    nothing for the time being.
    
    The actual Action implementation will be done in
    a separate future commit. This one is just to complete the creation
    of the skeleton for the entire automation.

---
+++ lib/workflows/jira_openstack_request_new_project.py
@@ -0,0 +1,11 @@
+from structs.jira.jira_account import JiraAccount
+
+
+def request_new_project(
+    jira_account: JiraAccount,  # pylint: disable=unused-argument
+    issue_key,  # pylint: disable=unused-argument
+):
+    """
+    to be implemented
+    """
+    raise NotImplementedError
