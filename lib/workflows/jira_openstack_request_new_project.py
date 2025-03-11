1792d80a43abbfc483d681862074d0320c9d085a
Jose Caballero Bejar <jcaballero.hep@gmail.com>
2025-03-11 08:57:54 +0000

    Create first draft of action unit test
    Create just a first draft unit test code for the Action.
    This unit test will be improved in separate commits as the entire
    automation code is implemented properly.

--- lib/workflows/jira_openstack_request_new_project.py
+++ lib/workflows/jira_openstack_request_new_project.py
@@ -1,9 +1,11 @@
 from structs.jira.jira_account import JiraAccount
 
+# pylint: disable=unused-argument
+
 
 def request_new_project(
-    jira_account: JiraAccount,  # pylint: disable=unused-argument
-    issue_key,  # pylint: disable=unused-argument
+    jira_account: JiraAccount,
+    issue_key,
 ):
     """
     to be implemented
