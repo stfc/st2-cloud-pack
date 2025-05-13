b042a5154595c4296bb9e0dbd9852ae2579a81df
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2025-05-13 15:43:43 +0100

    Schedule request

--- sensors/src/chatops_pr_reminder_sensor.py
+++ sensors/src/chatops_pr_reminder_sensor.py
@@ -1,6 +1,7 @@
 import eventlet
 from st2reactor.sensor.base import Sensor
 import requests
+import schedule
 
 
 class ChatOpsPRReminderSensor(Sensor):
@@ -21,17 +22,24 @@ class ChatOpsPRReminderSensor(Sensor):
 
     def setup(self):
         """Setup method"""
+        schedule.every().monday.at("09:00").do(self.make_request)
+        schedule.every().wednesday.at("09:00").do(self.make_request)
+        schedule.every().friday.at("09:00").do(self.make_request)
+
+    def make_request(self):
+        """Make the request to the ChatOps endpoint."""
+        self._logger.debug("ChatOpsPRReminderSensor making request...")
+        request = requests.post(
+            url=self.endpoint,
+            json={"reminder_type": self.reminder_type, "channel": self.channel},
+            headers={"Authorization": f"token {self.token}"}
+        )
+        self._logger.error(f"ChatOpsPRReminderSensor request result with {request.status_code} {request.text}")
 
     def run(self):
         """Poll the ChatOps endpoint."""
         while not self._stop:
-            self._logger.debug("ChatOpsPRReminderSensor making request...")
-            request = requests.post(
-                url=self.endpoint,
-                json={"reminder_type": self.reminder_type, "channel": self.channel},
-                headers={"Authorization": f"token {self.token}"}
-            )
-            request.raise_for_status()
+            schedule.run_pending()
             eventlet.sleep(60)
 
     def cleanup(self):
