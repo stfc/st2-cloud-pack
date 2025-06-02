1333a29d967e2311a3aea0da03c0d5a3d2190105
AlexCK-STFC <210735915+AlexCK-STFC@users.noreply.github.com>
2025-06-02 16:23:44 +0100

    ENH: Add filtering by available flavours

--- lib/workflows/find_reinstall_candidate_hypervisors.py
+++ lib/workflows/find_reinstall_candidate_hypervisors.py
@@ -1,9 +1,13 @@
 import re
 from typing import List, Optional
 
+from openstack.connection import Connection
+from openstack_api.openstack_connection import OpenstackConnection
+from openstack_api.openstack_hypervisor import get_available_flavors
 from openstackquery import HypervisorQuery
 
 # pylint:disable=too-many-arguments
+# pylint:disable=too-many-locals
 
 
 def find_reinstall_candidate_hypervisors(
@@ -12,6 +16,8 @@ def find_reinstall_candidate_hypervisors(
     max_vcpus: Optional[int] = None,
     max_vms: Optional[int] = None,
     exclude_hostnames: Optional[List[str]] = None,
+    include_flavours: Optional[List[str]] = None,
+    exclude_flavours: Optional[List[str]] = None,
     properties_to_select: Optional[List[str]] = None,
     output_type: Optional[str] = "to_string",
     sort_by: Optional[str] = None,
@@ -27,9 +33,11 @@ def find_reinstall_candidate_hypervisors(
     :param max_vcpus: Optional maximum threshold for used vCPUs.
     :param max_vms: Optional maximum threshold for # of hosted VMs.
     :param exclude_hostnames: Optional list of hypervisor hostname substrings to exclude.
+    :param include_flavours: Optional list of flavor names; only hypervisors that support at least one are included.
+    :param exclude_flavours: Optional list of flavor names; hypervisors that support any of these are excluded.
     :param properties_to_select: Optional list of strings representing which properties to select,
     else all are selected.
-    :param output_type: Format of the output (e.g., "to_string", "to_html", "to_props", to_objects").
+    :param output_type: string representing desired output type of the query
     :param sort_by: Optional property to sort the results by.
     :param sort_direction: Sort direction for the results; either "asc" or "desc" (default: "desc").
     :param kwargs: A set of optional meta params to pass to the query
@@ -62,17 +70,35 @@ def find_reinstall_candidate_hypervisors(
 
     # TODO: Add custom openstackquery query for NOT_MATCHES_REGEX, lookahead filters like this aren't performant
     if exclude_hostnames:
-        escaped_hostnames = [re.escape(h) for h in exclude_hostnames]
-        hostname_pattern = f"^(?!.*({'|'.join(escaped_hostnames)})).*$"
-
         query.where(
             preset="MATCHES_REGEX",
             prop="name",
-            value=hostname_pattern,
+            value=construct_hostname_exclude_regex(exclude_hostnames),
         )
 
     if sort_by is not None:
         query.sort_by((sort_by, sort_direction))
+
+    if include_flavours or exclude_flavours:
+        if "name" not in properties_to_select:
+            query.select("name", *properties_to_select)
+
+        query.run(cloud_account, **kwargs)
+        hvs = query.to_props(flatten=True)
+
+        with OpenstackConnection(cloud_account) as conn:
+            allowed_hv_names = filter_hypervisors_by_flavour(
+                conn, hvs["hypervisor_name"], include_flavours, exclude_flavours
+            )
+
+        query.where(
+            preset="ANY_IN",
+            prop="name",
+            value=allowed_hv_names,
+        )
+
+        if "name" not in properties_to_select:
+            query.select(*properties_to_select)
 
     query.run(cloud_account, **kwargs)
 
@@ -82,3 +108,32 @@ def find_reinstall_candidate_hypervisors(
         "to_objects": query.to_objects(),
         "to_props": query.to_props(),
     }[output_type]
+
+
+def filter_hypervisors_by_flavour(
+    conn: Connection,
+    hv_names: List[str],
+    include_flavours: Optional[List[str]],
+    exclude_flavours: Optional[List[str]],
+) -> List[str]:
+    def check_flavours_allowed(conn, hv_name):
+        flavours = set(get_available_flavors(conn, hv_name))
+
+        if include_flavours:
+            if not set(include_flavours).intersection(flavours):
+                return False
+
+        if exclude_flavours:
+            if set(exclude_flavours).intersection(flavours):
+                return False
+
+        return True
+
+    allowed_hv_names = [name for name in hv_names if check_flavours_allowed(conn, name)]
+
+    return allowed_hv_names
+
+
+def construct_hostname_exclude_regex(exclude_hostnames: List[str]) -> str:
+    escaped_hostnames = [re.escape(h) for h in exclude_hostnames]
+    return f"^(?!.*({'|'.join(escaped_hostnames)})).*$"
