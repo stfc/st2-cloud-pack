962c1c18054afca46f163f68422e9fffcf603bda
jcornall <james.cornall@stfc.ac.uk>
2025-09-04 14:27:48 +0100

    Add host aggregate sensor files

---
+++ sensors/src/host_aggregate_list_sensor.py
@@ -0,0 +1,68 @@
+from openstack_api.openstack_connection import OpenstackConnection
+from st2reactor.sensor.base import PollingSensor
+
+
+class HostAggregateSensor(PollingSensor):
+    """
+    * self.sensor_service
+        - provides utilities like
+            get_logger() for writing to logs.
+            dispatch() for dispatching triggers into the system.
+    * self._config
+        - contains configuration that was specified as
+          config.yaml in the pack.
+    * self._poll_interval
+        - indicates the interval between two successive poll() calls.
+    """
+
+    def __init__(self, sensor_service, config=None, poll_interval=None):
+        super().__init__(
+            sensor_service=sensor_service, config=config, poll_interval=poll_interval
+        )
+        self._log = self._sensor_service.get_logger(__name__)
+        self.dest_cloud_account = self.config["sensor_dest_cloud"]
+
+    def setup(self):
+        """
+        Stub
+        """
+
+    def poll(self):
+        """
+        Polls the dev cloud host aggregates and dispatches a payload containing
+        a list of aggregates.
+        """
+        with OpenstackConnection(self.dest_cloud_account) as conn:
+            self._log.info(f"Destination Cloud: {self.dest_cloud_account}")
+            self._log.info("Polling for destination aggregates.")
+
+            dest_aggregates = conn.compute.aggregates()
+
+            self._log.info("Dispatching trigger for aggregate list.")
+
+            payload = {"dest_aggregates": dest_aggregates}
+
+            self.sensor_service.dispatch(
+                trigger="stackstorm_openstack.aggregate.aggregate_change",
+                payload=payload,
+            )
+
+    def cleanup(self):
+        """
+        Stub
+        """
+
+    def add_trigger(self, trigger):
+        """
+        Stub
+        """
+
+    def update_trigger(self, trigger):
+        """
+        Stub
+        """
+
+    def remove_trigger(self, trigger):
+        """
+        Stub
+        """
---
+++ tests/sensors/test_host_aggregate_list_sensor.py
@@ -0,0 +1,40 @@
+from unittest.mock import MagicMock, patch
+import pytest
+from sensors.src.host_aggregate_list_sensor import HostAggregateSensor
+
+
+@pytest.fixture(name="sensor")
+def aggregate_sensor_fixture():
+    """
+    Fixture for sensor config.
+    """
+    return HostAggregateSensor(
+        sensor_service=MagicMock(),
+        config={
+            "sensor_dest_cloud": "dev",
+        },
+        poll_interval=10,
+    )
+
+
+@patch("sensors.src.host_aggregate_list_sensor.OpenstackConnection")
+def test_poll(mock_openstack_connection, sensor):
+    """
+    Test main function of sensor, polling the dev cloud aggregates and their properties.
+    """
+    mock_conn = MagicMock()
+    mock_openstack_connection.return_value.__enter__.return_value = mock_conn
+
+    mock_aggregate = MagicMock()
+    mock_conn.compute.aggregates.return_value = [mock_aggregate]
+
+    sensor.poll()
+
+    mock_conn.compute.aggregates.assert_called_once_with()
+
+    expected_payload = {"dest_aggregates": [mock_aggregate]}
+
+    sensor.sensor_service.dispatch.assert_called_once_with(
+        trigger="stackstorm_openstack.aggregate.aggregate_change",
+        payload=expected_payload,
+    )
