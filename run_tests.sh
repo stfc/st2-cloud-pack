16ec5a0dffd990e7fc52442750c82d9cd410082b
jcornall <james.cornall@stfc.ac.uk>
2025-09-18 16:26:30 +0100

    MAINT: Reset run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -4,10 +4,10 @@ set -e
 # set repo directory as st2-cloud-pack location
 REPO_DIR=$(cd -- "$(dirname -- "$0")" && pwd)
 
-export ST2_REPO_PATH="/home/pam62425/repositories/st2"
+export ST2_REPO_PATH="/tmp/st2"
 if [ ! -d "$ST2_REPO_PATH" ]; then
     git clone --depth=1 https://github.com/stackstorm/st2.git "$ST2_REPO_PATH"
 fi
 
 export PYTHONPATH="$PYTHONPATH:$REPO_DIR/lib"
-"$ST2_REPO_PATH/st2common/bin/st2-run-pack-tests" -p "$REPO_DIR" -c -x -v
+"$ST2_REPO_PATH/st2common/bin/st2-run-pack-tests" -p "$REPO_DIR" -c
