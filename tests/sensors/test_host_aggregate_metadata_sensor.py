65a6bdad51cb0438e009622716d6223d626fb4ce
George Matthews <george.matthews@stfc.ac.uk>
2025-09-26 14:40:09 +0100

    Fix tests
    Fix aggregate sensor tests

--- tests/sensors/test_host_aggregate_metadata_sensor.py
+++ tests/sensors/test_host_aggregate_metadata_sensor.py
@@ -23,7 +23,7 @@ def aggregate_sensor_fixture():
 @patch("sensors.src.host_aggregate_metadata_sensor.tabulate")
 @patch("sensors.src.host_aggregate_metadata_sensor.diff_to_tabulate_table")
 @patch("sensors.src.host_aggregate_metadata_sensor.OpenstackConnection")
-def test_poll(
+def test_poll_mismatch(
     mock_openstack_connection, mock_diff_to_tabulate_table, mock_tabulate, sensor
 ):
     """
@@ -38,26 +38,21 @@ def test_poll(
     ]
 
     mock_source_aggregate = MagicMock()
-    mock_source_aggregate.to_dict.return_value = {
-        "name": "aggregate1",
-        "availability_zone": "nova",
-    }
+    mock_source_aggregate.name = "aggregate1"
+    mock_source_aggregate.availability_zone = "nova"
+
     mock_target_aggregate = MagicMock()
-    mock_target_aggregate.to_dict.return_value = {
-        "name": "aggregate1",
-        "availability_zone": "nova",
-    }
+    mock_target_aggregate.name = "aggregate1"
+    mock_target_aggregate.availability_zone = "ceph"
+
     mock_source_conn.compute.aggregates.return_value = [mock_source_aggregate]
     mock_target_conn.compute.aggregates.return_value = [mock_target_aggregate]
-
     mock_diff = mock_diff_to_tabulate_table.return_value
 
     sensor.poll()
 
     mock_source_conn.compute.aggregates.assert_called_once_with()
     mock_target_conn.compute.aggregates.assert_called_once_with()
-
-    mock_diff_to_tabulate_table.assert_called_once()
 
     expected_payload = {
         "aggregate_name": mock_source_aggregate.name,
@@ -72,3 +67,63 @@ def test_poll(
         trigger="stackstorm_openstack.aggregate.metadata_mismatch",
         payload=expected_payload,
     )
+
+
+@patch("sensors.src.host_aggregate_metadata_sensor.OpenstackConnection")
+def test_poll_match(mock_openstack_connection, sensor):
+    """
+    Test main function of sensor, polling the dev cloud aggregates and their properties.
+    """
+    mock_source_conn = MagicMock()
+    mock_target_conn = MagicMock()
+
+    mock_openstack_connection.return_value.__enter__.side_effect = [
+        mock_source_conn,
+        mock_target_conn,
+    ]
+
+    mock_source_aggregate = MagicMock()
+    mock_source_aggregate.name = "aggregate1"
+    mock_source_aggregate.availability_zone = "nova"
+
+    mock_source_conn.compute.aggregates.return_value = [mock_source_aggregate]
+    mock_target_conn.compute.aggregates.return_value = [mock_source_aggregate]
+
+    sensor.poll()
+
+    mock_source_conn.compute.aggregates.assert_called_once_with()
+    mock_target_conn.compute.aggregates.assert_called_once_with()
+
+    sensor.sensor_service.dispatch.assert_not_called()
+
+
+@patch("sensors.src.host_aggregate_metadata_sensor.OpenstackConnection")
+def test_poll_not_exist_in_target(mock_openstack_connection, sensor):
+    """
+    Test main function of sensor, polling the dev cloud aggregates and their properties.
+    """
+    mock_source_conn = MagicMock()
+    mock_target_conn = MagicMock()
+
+    mock_openstack_connection.return_value.__enter__.side_effect = [
+        mock_source_conn,
+        mock_target_conn,
+    ]
+
+    mock_source_aggregate = MagicMock()
+    mock_source_aggregate.name = "aggregate1"
+    mock_source_aggregate.availability_zone = "nova"
+
+    mock_target_aggregate = MagicMock()
+    mock_target_aggregate.name = "aggregate2"
+    mock_target_aggregate.availability_zone = "ceph"
+
+    mock_source_conn.compute.aggregates.return_value = [mock_source_aggregate]
+    mock_target_conn.compute.aggregates.return_value = [mock_target_aggregate]
+
+    sensor.poll()
+
+    mock_source_conn.compute.aggregates.assert_called_once_with()
+    mock_target_conn.compute.aggregates.assert_called_once_with()
+
+    sensor.sensor_service.dispatch.assert_not_called()
