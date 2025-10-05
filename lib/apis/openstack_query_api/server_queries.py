88249762ef46bd93770a3029032c4b801073a1f1
Jose Caballero Bejar <jcaballero.hep@gmail.com>
2025-10-05 11:32:28 +0100

    Move function find_servers_with_decom_flavor
    Move the function to find servers with a given flavor name (or names) to
    the API directory for the query library:
    
          lib/apis/openstack_query_api
    
    The code to send emails shouldn't need to know about the internal
    details of the query library.
    Moreover when there is an API directory for that query library.
    All code that needs to use it should be implemented as part of the API.
    
    In this commit we implement the following changes:
    
    * move the function to find servers to the query library API
    * fix the docstrings after moving the code to the API directory, to
      remove explicit mentions to "decommissioning"
    * remove the function to find servers from the workflow
      send_decom_flavor_email.py
    * the block of code to raise an Exception if no server is found remains
      in the workflow, that part does not need to be moved to the API
    * implement a new unittest file for the new code in the API directory
    * fix some incorrect function names in the old unittest code (functions
      had string "user" where it should be "servers")
    * fix the unittest code for the workflow

---
+++ lib/apis/openstack_query_api/server_queries.py
@@ -0,0 +1,47 @@
+from typing import List, Optional
+from openstackquery import FlavorQuery
+
+
+def find_servers_with_flavors(
+    cloud_account: str,
+    flavor_name_list: List[str],
+    from_projects: Optional[List[str]] = None,
+):
+    """
+    Use QueryAPI to run the query to find flavors
+    :param cloud_account: string represents cloud account to use
+    :param flavor_name_list: A list of flavor names
+    :param from_projects: A list of project identifiers to limit search in
+    """
+
+    flavor_query = FlavorQuery()
+    flavor_query.where(
+        "any_in",
+        "flavor_name",
+        values=flavor_name_list,
+    )
+    flavor_query.run(cloud_account)
+    flavor_query.sort_by(("flavor_id", "asc"))
+
+    if not flavor_query.to_props():
+        raise RuntimeError(
+            f"None of the Flavors provided {', '.join(flavor_name_list)} were found"
+        )
+
+    # find the VMs using flavors we found from the flavor query
+    server_query = flavor_query.then("SERVER_QUERY", keep_previous_results=True)
+    server_query.run(
+        cloud_account,
+        as_admin=True,
+        from_projects=from_projects if from_projects else None,
+        all_projects=not from_projects,
+    )
+    server_query.select(
+        "server_id",
+        "server_name",
+        "addresses",
+    )
+    server_query.append_from("PROJECT_QUERY", cloud_account, ["project_name"])
+    server_query.group_by("user_id")
+
+    return server_query
