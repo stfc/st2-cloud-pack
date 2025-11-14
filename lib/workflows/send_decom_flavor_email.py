09bd968355e6510e7f99c851feabedd0857f120e
Aziz Ahmad <abdul.aziz@stfc.ac.uk>
2025-11-14 16:04:07 +0000

    ENH: Refactor send_decom_flavor_email workflow
     - Change the name of find_servers_with_decom_flavors to find_servers_with_flavors and move it from workflows to server_queries module in openstack_query_api.

--- lib/workflows/send_decom_flavor_email.py
+++ lib/workflows/send_decom_flavor_email.py
@@ -9,6 +9,8 @@ from apis.openstack_api.enums.cloud_domains import CloudDomains
 from openstackquery import FlavorQuery, UserQuery
 from tabulate import tabulate
 
+from apis.openstack_query_api.server_queries import find_servers_with_flavors
+
 
 def get_affected_flavors_html(flavor_list: List[str], eol_list: List[str]):
     """
@@ -71,58 +73,6 @@ def validate_flavor_input(
         raise RuntimeError(
             "please provide either a list of project identifiers or the 'all_projects' flag"
         )
-
-
-def find_servers_with_decom_flavors(
-    cloud_account: str,
-    flavor_name_list: List[str],
-    from_projects: Optional[List[str]] = None,
-):
-    """
-    Use QueryAPI to run the query to find decom flavors
-    :param cloud_account: string represents cloud account to use
-    :param flavor_name_list: A list of flavor names to be decommissioned
-    :param from_projects: A list of project identifiers to limit search in
-    """
-
-    flavor_query = FlavorQuery()
-    flavor_query.where(
-        "any_in",
-        "flavor_name",
-        values=flavor_name_list,
-    )
-    flavor_query.run(cloud_account)
-    flavor_query.sort_by(("flavor_id", "asc"))
-
-    if not flavor_query.to_props():
-        raise RuntimeError(
-            f"None of the Flavors provided {', '.join(flavor_name_list)} were found"
-        )
-
-    # find the VMs using flavors we found from the flavor query
-    server_query = flavor_query.then("SERVER_QUERY", keep_previous_results=True)
-    server_query.run(
-        cloud_account,
-        as_admin=True,
-        from_projects=from_projects if from_projects else None,
-        all_projects=not from_projects,
-    )
-    server_query.select(
-        "server_id",
-        "server_name",
-        "addresses",
-    )
-
-    if not server_query.to_props():
-        raise RuntimeError(
-            f"No servers found with flavors {', '.join(flavor_name_list)} on projects "
-            f"{','.join(from_projects) if from_projects else '[all projects]'}"
-        )
-
-    server_query.append_from("PROJECT_QUERY", cloud_account, ["project_name"])
-    server_query.group_by("user_id")
-
-    return server_query
 
 
 def print_email_params(
@@ -223,7 +173,7 @@ def send_decom_flavor_email(
         flavor_name_list, flavor_eol_list, limit_by_projects, all_projects
     )
 
-    server_query = find_servers_with_decom_flavors(
+    server_query = find_servers_with_flavors(
         cloud_account, flavor_name_list, limit_by_projects
     )
 
