1c250d2b41ea31b126ceba9e7f56af0277bae62d
Jose Caballero Bejar <jcaballero.hep@gmail.com>
2026-08-06 16:20:13 +0100

    fix name of call to function

--- lib/apis/wazuh_api/wazuh_query_agents.py
+++ lib/apis/wazuh_api/wazuh_query_agents.py
@@ -83,7 +83,7 @@ def wazuh_list_servers_by_os(
     # by adding conditions
     #   group!=kolla
     # to the query string
-    data = query_agents(wazuh_token, wazuh_account.url, values=["name"], query=query)
+    data = _query_agents(wazuh_token, wazuh_account.url, values=["name"], query=query)
 
     # extract the Server ID from the fetched data
     # the variable "name" for OpenStack Servers looks like this:
