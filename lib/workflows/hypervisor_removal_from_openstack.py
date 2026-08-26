6d6b6a1e0a597db6b76bba61cf037a4cbe08308a
RWhiting78 <r_whiting@live.com>
2026-08-26 14:42:44 +0000

    Initial commit

---
+++ lib/workflows/hypervisor_removal_from_openstack.py
@@ -0,0 +1,7 @@
+from openstack.connection import Connection
+from apis.openstack_api.openstack_hypervisor import 
+
+def hypervisor_removal_from_openstack(
+    conn: Connection,
+    hypervisor_name: str,
+)
