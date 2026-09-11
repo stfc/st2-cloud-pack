f0681854c24a8e7088a6957be2b36eecd5f5d954
George Matthews <george.matthews@stfc.ac.uk>
2026-09-11 15:37:45 +0100

    Update patching hv unit tests
    Now test using a fixed start time to test a fixed end time

--- tests/lib/workflows/test_hv_patch_and_reboot.py
+++ tests/lib/workflows/test_hv_patch_and_reboot.py
@@ -1,4 +1,4 @@
-import datetime
+import datetime as dt
 from unittest.mock import MagicMock, patch, call
 from paramiko import SSHException
 
@@ -10,7 +10,7 @@ import pytest
 
 
 # pylint:disable=too-many-locals
-@pytest.mark.freeze_time
+@pytest.mark.freeze_time("2026-09-11 15:00:01")
 @patch("workflows.hv_patch_and_reboot.schedule_silence")
 @patch("workflows.hv_patch_and_reboot.SSHConnection")
 def test_successful_patch_and_reboot(
@@ -32,15 +32,15 @@ def test_successful_patch_and_reboot(
     )
     mock_silence_details_instance = SilenceDetails(
         matchers=[AlertMatcherDetails(name="instance", value="test_host")],
-        start_time_dt=datetime.datetime.utcnow(),
-        duration_hours=6,
+        start_time_dt=dt.datetime(2026, 9, 11, 15, 0, 1, tzinfo=dt.timezone.utc),
+        end_time_dt=dt.datetime(2026, 9, 14, 10, 10, 1, tzinfo=dt.timezone.utc),
         author="stackstorm",
         comment="Stackstorm: HV Patching",
     )
     mock_silence_details_hostname = SilenceDetails(
         matchers=[AlertMatcherDetails(name="hostname", value="test_host")],
-        start_time_dt=datetime.datetime.utcnow(),
-        duration_hours=6,
+        start_time_dt=dt.datetime(2026, 9, 11, 15, 0, 1, tzinfo=dt.timezone.utc),
+        end_time_dt=dt.datetime(2026, 9, 14, 10, 10, 1, tzinfo=dt.timezone.utc),
         author="stackstorm",
         comment="Stackstorm: HV Patching",
     )
@@ -61,7 +61,7 @@ def test_successful_patch_and_reboot(
     mock_ssh_conn.return_value.run_command_on_host.assert_any_call("reboot")
 
 
-@pytest.mark.freeze_time
+@pytest.mark.freeze_time("2026-09-10 15:00:01")
 @patch("workflows.hv_patch_and_reboot.schedule_silence")
 @patch("workflows.hv_patch_and_reboot.remove_silence")
 @patch("workflows.hv_patch_and_reboot.SSHConnection")
@@ -97,15 +97,15 @@ def test_failed_ssh(
     )
     mock_silence_details_instance = SilenceDetails(
         matchers=[AlertMatcherDetails(name="instance", value="test_host")],
-        start_time_dt=datetime.datetime.utcnow(),
-        duration_hours=6,
+        start_time_dt=dt.datetime(2026, 9, 10, 15, 0, 1, tzinfo=dt.timezone.utc),
+        end_time_dt=dt.datetime(2026, 9, 11, 10, 10, 1, tzinfo=dt.timezone.utc),
         author="stackstorm",
         comment="Stackstorm: HV Patching",
     )
     mock_silence_details_hostname = SilenceDetails(
         matchers=[AlertMatcherDetails(name="hostname", value="test_host")],
-        start_time_dt=datetime.datetime.utcnow(),
-        duration_hours=6,
+        start_time_dt=dt.datetime(2026, 9, 10, 15, 0, 1, tzinfo=dt.timezone.utc),
+        end_time_dt=dt.datetime(2026, 9, 11, 10, 10, 1, tzinfo=dt.timezone.utc),
         author="stackstorm",
         comment="Stackstorm: HV Patching",
     )
