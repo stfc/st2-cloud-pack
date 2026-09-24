4d3d20d528646fe3b57acba27059625d589fb92b
Jose Caballero Bejar <jcaballero.hep@gmail.com>
2026-09-24 11:17:37 +0100

    fixed

--- lib/workflows/hello_world.py
+++ lib/workflows/hello_world.py
@@ -1,14 +1,12 @@
 import logging
+from pathlib import Path
 
 logger = logging.getLogger(__name__)
 
 
 def salute():
-    from pathlib import Path
     BASE_DIR = Path(__file__).resolve().parent
     conf_path = BASE_DIR / "etc/config"
-    print(conf_path)
-    #conf = open("etc/config")
     conf = open(conf_path)
     logger.info("Hello!!")
     logger.info(conf.readlines())
